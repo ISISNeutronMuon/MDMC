be2c9d622bfd27309d442cf6507d6ec741faeeb1
Thomas Farmer <thomas.farmer27@hotmail.co.uk>
2018-05-16 11:29:26 +0100

    Added harmonic, LJ and Coulomb interactions functions

--- MDMC/src/MD/interaction_functions.py
+++ MDMC/src/MD/interaction_functions.py
@@ -1,8 +1,10 @@
-"""A module for defining and storing atomic interaction functions
+"""A module for storing atomic interaction functions
 
 Contains abstract class InteractionFunction for defining the form interaction
 function classes must take.  All functions describing atomic interactions must
-be added to this module in order to be called by a universe.
+be added to this module in order to be called by a universe.  If needed, the
+interaction function classes can be extended to contain actual function
+definitions.
 
 AUTHOR :    Thomas Farmer        START DATE :    2018-5-1 10:15:10"""
 
@@ -13,13 +15,27 @@ class interaction_function(ABC):
     supplied
 
     Attributes:
-    n_params - number of parameters"""
+    """
+
+    def function(self):
+        pass
+
 
 class harmonic_potential(interaction_function):
     """Harmonic potential for bond stretching and angular vibration"""
 
+    def __init__(self,equilibrium_state,potential_strength):
+        self.params = {'equilibrium_state':equilibrium_state,
+                    'potential_strength':potential_strength}
+
 class lennard_jones(interaction_function):
-    """Non-bonded Lennard-Jones interaction"""
+    """Dispersive Lennard-Jones interaction"""
+
+    def __init__(self,sigma,eta):
+        self.params = {'sigma':sigma,'eta':eta}
 
 class coulomb(interaction_function):
-    """Non-bonded Coulomb interaction for charged particles"""
+    """Coulomb interaction for charged particles"""
+
+    def __init__(self,charge):
+        self.params = {'charge':charge}
