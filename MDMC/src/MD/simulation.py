710dcd6df39c32d1d137a8c607a633c5bf4574c6
Thomas Farmer <thomas.farmer27@hotmail.co.uk>
2018-05-29 16:24:48 +0100

    Changed fill to use np.linspace, parameter passing to NVE ensemble

--- MDMC/src/MD/simulation.py
+++ MDMC/src/MD/simulation.py
@@ -4,7 +4,8 @@
 
  AUTHOR :    Thomas Farmer        START DATE :    2018-4-30 13:01:04"""
 
-from MDMC.src.MD.MMTK.MMTK_wrapper import MMTK
+from MDMC.src.MD.engine_facades.facade_factory import MDEngineFacadeFactory
+from MDMC.src.MD.structural_units import Molecule
 
 from enum import Enum
 import itertools
@@ -12,8 +13,8 @@ from copy import deepcopy
 import numpy as np
 import weakref
 
-Shape = Enum('Shape','orthorhombic')
-Boundary = Enum('Boundary', 'infinite cubic orthorhombic')
+Shape = Enum('Shape',['cubic','orthorhombic','infinite','rhombic_dodecahedron',
+                'truncated_octahedron'])
 
 # TODO: Extract out atomic structure generation from Universe class, so that more structures can be easily added
 # TODO: Atomic Structures should be factory
@@ -25,20 +26,22 @@ def _liquid_structure():
     pass
 
 class Universe(object):
-    """Class where configuration and topology are defined
+
+    """
+    Class where configuration and topology are defined
 
     DESCRIPTION
 
     Attributes:
     shape
     dims - dimensions
-    pbc - Periodic Boundary Conditions"""
+    pbc - Periodic Boundary Conditions
+    """
 
-    def __init__(self,dimensions,shape=Shape.orthorhombic,pbc=Boundary.cubic):
+    def __init__(self,dimensions,shape=Shape.cubic):
         # TODO: Change interactions so that it maintains an ordered set, so that searching is optimized
         self.dims = np.array(dimensions)
         self.shape = shape
-        self.pbc = pbc
         self.configuration = []
         self._interactions = set()
         self.force_field = None
@@ -56,7 +59,7 @@ class Universe(object):
                             structure='liquid',force_field=None):
         self._add_single_structural_unit(structural_unit)
         if force_field:
-            add_force_field(force_field,structural_unit.interaction_set())
+            self.add_force_field(force_field,structural_unit.interaction_set())
         # TODO: Extract below into fill method
         # Calculate what bounding radius factor needs to be so structural units
         # are approximately equidistant and pass this to fill.
@@ -72,7 +75,9 @@ class Universe(object):
     # TODO: Add in option to tessellate a configuration to fill universe (a la GROMACS)
     def fill(self,structural_unit,force_field=None,
                 structural_motif=_liquid_structure(),**kwargs):
-        """A fluid-like filling of the universe independent of existing atoms
+
+        """
+        A fluid-like filling of the universe independent of existing atoms
 
         Adds copies of structural_unit to existing configuration until universe
         is full.  As exclusion region is defined by the size of a bounding
@@ -89,31 +94,32 @@ class Universe(object):
         structural_motif - The arrangement of structural units
 
         Raises:
-        EXCEPTIONS"""
-
-        self._add_single_structural_unit(structural_unit)
-        structural_unit.position = [0,0,0]
-        if force_field:
-            self.add_force_field(force_field,structural_unit.interaction_set())
-
-        # Calculate number of structural units required to achieve number_density
-        # Create a generator to determine the position of each structural_unit in the given structural_motif
-        # Create each structural_unit with a deepcopy and then set position
+        EXCEPTIONS
+        """
 
         # TODO: implement method for specifying number of molecules and number density, rather than box size
-        num_units = int(self.volume / kwargs['num_density']) - 1
-        unit_separation = (self.dims / int((num_units**(1./3.))))
-        '''positions is sliced so that [0,0,0] is excluded'''
-        # TODO: Replace arange with linspace as arange is unreliable with non-integer steps
-        positions = sorted(list(itertools.product(
-                            np.arange(0,self.dims[0],unit_separation[0]),
-                            np.arange(0,self.dims[1],unit_separation[1]),
-                            np.arange(0,self.dims[2],unit_separation[2]))))[1:]
+        n_units_xyz = self.dims/(1./kwargs.get('num_density'))**(1./3.)
+        n_units_xyz = n_units_xyz.astype(int)
+
+        positions = []
+        for i in range(len(self.dims)):
+            positions.append(np.linspace(0,self.dims[i],n_units_xyz[i],
+                endpoint = False))
+
+        positions = sorted(list(itertools.product(*positions)))
+
         # TODO: See if changing from appending to extending improves performance
         for position in positions:
-            new_unit = deepcopy(structural_unit)
-            new_unit.position = position
-            self.configuration.append(new_unit)
+            if position is positions[0]:
+                self._add_single_structural_unit(structural_unit)
+                offset = (structural_unit.position - structural_unit.bounding_box.min)
+                structural_unit.position = position + offset
+                if force_field:
+                    self.add_force_field(force_field,structural_unit.interaction_set())
+            else:
+                new_unit = deepcopy(structural_unit)
+                new_unit.position = position + offset
+                self.configuration.append(new_unit)
 
     # TODO: Add duplicate structural unit method
 
@@ -129,36 +135,87 @@ class Universe(object):
             interactions = (self.interactions,)
         self.force_field = force_field(*interactions)
 
+    @property
+    def element_list(self):
+        return [atom.element for atom in self.atom_list()]
+
+    @property
+    def element_dict(self):
+
+        """
+        Returns a dictionary of all elements and a single atom of that
+        element type. This is required for MD engines which assign the same
+        potential parameters for all identical element types.
+        """
+
+        return {atom.element:atom for atom in self.atom_list()}
+
+    # TODO: Determine method of getting molecule list with looser coupling
+    @property
+    def molecule_list(self):
+        return [unit for unit in self.configuration
+            if isinstance(unit,Molecule)]
+
 
 class EnergyMinimizer(object):
-    """Define the MD energy minimizer
 
-     Attributes:
-     n_steps - number of steps
-     sz_steps - size of steps
-     algorithm - minimization algorithm
-     stop - condition for minimization to stop
-     minimize() - uses MD engine API to minimize energy
-     """
+    """
+    Define the MD energy minimizer
+
+    Attributes:
+    n_steps - number of steps
+    sz_steps - size of steps
+    algorithm - minimization algorithm
+    stop - condition for minimization to stop
+    minimize() - uses MD engine API to minimize energy
+    """
 
 # TODO: Create other ensembles by decorating NVESimulation with thermo/barostats
 class NVESimulation(object):
-    """Molecular dynamics engine for NVE ensemble
+
+    """
+    Molecular dynamics engine for NVE ensemble
 
     Attributes:
-    time_step - time per step
-    integrator - time integrators
-    temperature - simulation temperature in K
+    universe - an MDMC universe with force field and atomic configuration
+    engine - an instance of an external MD engine
+    settings - a list of settings passed to MD engine, see __init__ for details
+
+    Methods:
     run() - uses MD engine API to run simulation
     """
 
-    def __init__(self,time_step,temperature,
-                    integrator="velocity_verlet",**settings):
-        self.time_step = time_step
-        self.temperature = temperature
-        self.integrator = integrator
-        self.settings = settings
+    # TODO: Potentially separate out universe and simulation setup
+    def __init__(self, universe, engine="mmtk", **settings):
 
-    def run(self,engine=MMTK,n_steps):
-        engine(self.time_step,self.temperature,self.integrator,
-                self.settings)
+        """
+        Initializes universe, engine and settings
+
+        Engine independent settings:
+        temperature - simulation temperature in K
+        time_step - simulation timestep size in fs
+        integrator - MD time integrator
+
+        MMTK specific settings:
+        lj_options - Options for Lennard-Jones interactions
+        es_options - Options for electrostatic interactions
+        """
+
+        self.universe = universe
+        self.settings = settings
+        self.engine = MDEngineFacadeFactory.create_facade(engine)
+        self._setup()
+
+    def _setup(self):
+
+        """
+        Creates a universe within the MD engine with the equivalent
+        configuration and topology to self.universe and defines the simulation
+        conditions
+        """
+
+        self.engine.setup_universe(self.universe,**self.settings)
+        self.engine.setup_simulation(self.universe,**self.settings)
+
+    def run(self,n_steps):
+        self.engine.run(n_steps)
