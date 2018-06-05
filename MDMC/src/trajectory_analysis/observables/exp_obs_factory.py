a18ff5b403f625d43107d2a59a2263cce8a177ea
Thomas Farmer <thomas.farmer27@hotmail.co.uk>
2018-06-05 14:44:24 +0100

    Created factory for instantiating experimental observables

---
+++ MDMC/src/trajectory_analysis/observables/exp_obs_factory.py
@@ -0,0 +1,23 @@
+"""Factory class for generating experimental observables
+
+AUTHOR :    Thomas Farmer        START DATE :    2018-6-5 14:18:49"""
+
+from importlib import import_module
+from inspect import isclass, isabstract, getmembers
+
+from MDMC.src.trajectory_analysis.observables.exp_obs import \
+    ExperimentalObservable
+
+class ExperimentalObservableFactory(object):
+
+    @staticmethod
+    def create_observable(module_name):
+        module = import_module('.' + module_name, __package__)
+
+        classes = getmembers(module, lambda m: (
+                                        isclass(m)
+                                        and not isabstract(m)
+                                        and issubclass(m, ExperimentalObservable
+                                        )))
+
+        return classes[0][1]()
