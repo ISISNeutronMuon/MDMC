df0f174d279e4ac94d2abf98b2a54017ef9801e4
Thomas Farmer <thomas.farmer27@hotmail.co.uk>
2018-06-05 14:43:48 +0100

    Created abstract class and methods for experimental observables

---
+++ MDMC/src/trajectory_analysis/observables/exp_obs.py
@@ -0,0 +1,55 @@
+"""Module defining a class for storing, calculating and reading in experimental
+observables from molecular dynamics trajectories.
+
+AUTHOR :    Thomas Farmer        START DATE :    2018-4-26 10:14:51"""
+
+from abc import ABCMeta, abstractmethod, abstractproperty
+
+class ExperimentalObservable:
+
+    """
+    Abstract class that defines methods common to all experimental observable
+    data containers
+
+    Experimental observable data can either be from a file or calculated from
+    MD and stored in the data property, along with the associated uncertainty.
+    The boolean property from_MD states the source of the information.
+    """
+
+    __metaclass__ = ABCMeta
+
+    @abstractproperty
+    def from_MD(self):
+
+        pass
+
+    @abstractproperty
+    def data(self):
+
+        pass
+
+    # TODO: Determine if this can be concrete rather than abstract
+    @abstractproperty
+    def dependent_variable(self):
+
+        pass
+
+    @abstractmethod
+    def read_from_file(self, reader, file):
+
+        """
+        Reads in experimental data from a file using a specified reader
+        """
+
+        pass
+
+    # TODO: Currently uses the generic parameter MD_input - if this is only histograms then change this
+    @abstractmethod
+    def calculate_from_MD(self, MD_input):
+
+        """
+        Calculates the experimental obseravable using input from an MD
+        simulation
+        """
+
+        pass
