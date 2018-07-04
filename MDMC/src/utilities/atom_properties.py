93b530c406199a9beeb111e9831b43548451c4ae
Thomas Farmer <thomas.farmer27@hotmail.co.uk>
2018-07-04 15:09:10 +0100

    Created atom properties modyle with lookup dictionary for mass and b

---
+++ MDMC/src/utilities/atom_properties.py
@@ -0,0 +1,34 @@
+"""Module for storing properties related to atoms
+
+AUTHOR :    Thomas Farmer        START DATE :    04/07/2018, 11:04:45"""
+
+"""
+Atomic masses are taken from IUPAC 2013.  In instances where IUPAC specifies a
+range of accepted values rather than a single value, the mean is taken.
+"""
+
+MASS = {
+    'H':1.00798,
+
+
+
+
+
+
+    'O':15.99903
+
+}
+
+
+"""
+Neutron scattering lengths are taken from Neutron News, Vol. 3, No. 3, 1992,
+pp. 29-37.
+"""
+
+B_COH = {
+
+}
+
+B_INCOH = {
+
+}
