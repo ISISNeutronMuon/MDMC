551b6e3daefdb9d7c0622483a7dc62cb72e881c2
Thomas Farmer <thomas.farmer27@hotmail.co.uk>
2018-07-04 22:06:17 +0100

    Added documentation

--- MDMC/src/MD/engine_facades/facade_factory.py
+++ MDMC/src/MD/engine_facades/facade_factory.py
@@ -9,6 +9,12 @@ from MDMC.src.MD.engine_facades.facade import MDEngine
 
 class MDEngineFacadeFactory(object):
 
+    """
+    Provides a factory for creating facades to MD engines.  Any facade within
+    the engine_facades folder can be created with a string of the class name, as
+    long as it is a subclass of MDEngine.
+    """
+
     @staticmethod
     def create_facade(module_name):
         module = import_module('.' + module_name, __package__)
