ea54c7ced49d99ba7cf55cd8856ed75b96feab5d
Thomas Farmer <thomas.farmer27@hotmail.co.uk>
2018-07-09 18:18:23 +0100

    Changed how test data filenames are stored

--- MDMC/tests/test_data/data.py
+++ MDMC/tests/test_data/data.py
@@ -1,6 +1,7 @@
 """Stores all filenames and additional data that is required for testing
 
-Origins for data are also supplied
+Test data must have the same name as the reader. Origins for data are also
+supplied.
 
 AUTHOR :    Thomas Farmer        START DATE :    2018-6-7 14:10:35"""
 
@@ -9,5 +10,7 @@ from os import path
 _ABS_DIR_PATH = path.split(path.abspath(__file__))[0]
 
 # TODO: introspective method to add abs_dir_path (use dir() to get all var names)
-# From Bertil Halle QENS water data on in5
-LAMP_SQW_FILE = _ABS_DIR_PATH + '/263K05Awat_LAMP'
+
+# LAMPSQw - From Bertil Halle QENS water data on in5
+
+data = {'LAMPSQw':_ABS_DIR_PATH + '/263K05Awat_LAMP'}
