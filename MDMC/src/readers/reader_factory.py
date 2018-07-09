6fbb1ec913365be4384faea2191bba71388de14a
Thomas Farmer <thomas.farmer27@hotmail.co.uk>
2018-07-09 16:54:09 +0100

    Added dosctring

--- MDMC/src/readers/reader_factory.py
+++ MDMC/src/readers/reader_factory.py
@@ -9,6 +9,12 @@ from MDMC.src.readers.readers import Reader
 
 class ReaderFactory(object):
 
+    """
+    Provides a factory for creating readers.  Any facade within the readers
+    folder can be created with a string of the class name, as
+    long as it is a subclass of Reader.
+    """
+
     @staticmethod
     def create_reader(module_name):
         module = import_module('.' + module_name, __package__)
