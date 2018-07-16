ec7a465d5553776994ce1117b32f5c03dab4197d
Thomas Farmer <thomas.farmer27@hotmail.co.uk>
2018-07-16 16:01:48 +0100

    Moved experimental data to specific subfolder

--- MDMC/tests/test_data/data.py
+++ MDMC/tests/test_data/data.py
@@ -8,9 +8,11 @@ AUTHOR :    Thomas Farmer        START DATE :    2018-6-7 14:10:35"""
 from os import path
 
 _ABS_DIR_PATH = path.split(path.abspath(__file__))[0]
+_EXP_DATA_PATH = '/experimental_data'
+_CALC_OBS_PATH = '/calculated_observables'
 
 # TODO: introspective method to add abs_dir_path (use dir() to get all var names)
 
 # LAMPSQw - From Bertil Halle QENS water data on in5
 
-data = {'LAMPSQw':_ABS_DIR_PATH + '/263K05Awat_LAMP'}
+data = {'LAMPSQw':_ABS_DIR_PATH + _EXP_DATA_PATH + '/263K05Awat_LAMP'}
