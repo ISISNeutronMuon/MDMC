48f0a5503ccf3b0710195f3939c39a7c114f6e26
Thomas Farmer <thomas.farmer27@hotmail.co.uk>
2018-07-19 15:52:26 +0100

    Created module for mathematical functions. Implemented correlation.

---
+++ MDMC/src/common/mathematics.py
@@ -0,0 +1,40 @@
+"""A module containing mathematical functions
+
+AUTHOR :    Thomas Farmer        START DATE :    18/07/2018, 16:42:10"""
+
+from numpy import conjugate, arange, real
+from numpy.fft import fft, ifft
+
+def correlation(input1, input2=None, normalise=False):
+
+    """
+    The correlation of two vector
+
+    The Fast Correlation Algorithm (FCA) is utilised.  If only a single input is
+    provided, the autocorrelation is calculated.
+    """
+
+    N = len(input1)
+
+    fft1 = fft(input1, n=(N * 2))
+
+    if input2 is None:
+        fft2 = fft1
+    else:
+        fft2 = fft(input2, n=(N * 2))
+
+    # Calculate the cyclic correlation function
+    cyclic_corr = ifft(conjugate(fft1) * fft2)
+
+    # Normalise for variable number of contributions to each timestep:
+    # 1 / (N - m)
+    # where m is the number of each individual step
+    if normalise:
+        prefactor = 1. / (N - arange(N))
+    else:
+        prefactor = 1.
+
+    corr = prefactor * real(cyclic_corr[0:N])
+
+    return corr
+
