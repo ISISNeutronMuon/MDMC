f7d9857c4cee76e2f3ab8e2ef86661d5208ca5cd
Thomas Farmer <thomas.farmer27@hotmail.co.uk>
2018-07-20 18:55:58 +0100

    Created module for coherent SQw class

---
+++ MDMC/src/trajectory_analysis/observables/SQw_coh.py
@@ -0,0 +1,8 @@
+"""Module for coherent SQw class
+
+AUTHOR :    Thomas Farmer        START DATE :    20/07/2018, 16:28:02"""
+
+import numpy as np
+
+from MDMC.src.common.mathematics import correlation
+from MDMC.src.trajectory_analysis.observables.SQw import AbstractSQw
