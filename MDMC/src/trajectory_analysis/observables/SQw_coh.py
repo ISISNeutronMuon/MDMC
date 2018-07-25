357ebec3e8017c98fc3c843ab55a212fec27928c
Thomas Farmer <thomas.farmer27@hotmail.co.uk>
2018-07-25 15:35:29 +0100

    Added SQwCoherent, _calculate_FQt_single_Q, _calculate_rho placeholders

--- MDMC/src/trajectory_analysis/observables/SQw_coh.py
+++ MDMC/src/trajectory_analysis/observables/SQw_coh.py
@@ -6,3 +6,19 @@ import numpy as np
 
 from MDMC.src.common.mathematics import correlation
 from MDMC.src.trajectory_analysis.observables.SQw import AbstractSQw
+
+
+class SQwCoherent(AbstractSQw):
+
+    """
+    A class for containing, calculating and reading the coherent dynamic
+    structure factor
+    """
+
+    def _calculate_FQt_single_Q(self, Q_vector):
+
+        raise NotImplementedError
+
+    def _calculate_rho(self, positions, Q_vector):
+
+        raise NotImplementedError
