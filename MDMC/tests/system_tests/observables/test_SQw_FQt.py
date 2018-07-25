f92357a9dbfe97ac5c75c3ee762e35efd6f0b17a
Thomas Farmer <thomas.farmer27@hotmail.co.uk>
2018-07-25 09:33:08 +0100

    Added placeholders for FQt and SQw fixtures and tests

--- MDMC/tests/system_tests/observables/test_SQw_FQt.py
+++ MDMC/tests/system_tests/observables/test_SQw_FQt.py
@@ -13,3 +13,101 @@ from netCDF4 import Dataset
 import MDMC.src.trajectory_analysis.observables.obs_factory as eof
 
 from MDMC.tests.test_data import data
+
+@pytest.fixture
+def incoh_file():
+    return Dataset(data.obs_data['SQw_incoh'],'r')
+
+@pytest.fixture
+def coh_file():
+    return Dataset(data.obs_data['SQw_coh'],'r')
+
+@pytest.fixture
+def Q(incoh_file):
+    return np.array(incoh_file.variables['q'][:])
+
+@pytest.fixture
+def time(incoh_file):
+    return np.array(incoh_file.variables['time'][:])
+
+@pytest.fixture
+def w(incoh_file):
+    return np.array(incoh_file.variables['frequency'][:])
+
+@pytest.fixture
+def FQt_incoh(incoh_file):
+    return np.array(incoh_file.variables['Fqt-total'][:])
+
+@pytest.fixture
+def SQw_incoh(incoh_file):
+    return np.array(incoh_file.variables['Sqw-total'][:])
+
+@pytest.fixture
+def FQt_coh(coh_file):
+    return np.array(coh_file.variables['Fqt-total'][:])
+
+@pytest.fixture
+def SQw_coh(coh_file):
+    return np.array(coh_file.variables['Sqw-total'][:])
+
+
+def test_FQt_incoh(Q, time, FQt_incoh):
+
+    """
+    Validate the calculation of the intermediate incoherent structure factor
+    against nMOLDYN
+    """
+
+    raise NotImplementedError
+
+
+def test_FQt_coh(Q, time, FQt_coh):
+
+    """
+    Validate the calculation of the intermediate coherent structure factor
+    against nMOLDYN
+    """
+
+    raise NotImplementedError
+
+
+def test_FQt_total(Q, time, FQt_incoh, FQt_coh):
+
+    """
+    Validate the calculation of the intermediate total structure factor against
+    the sum of the intermediate incoherent and coherent structure factors
+    calculated by MOLDYN
+    """
+
+    raise NotImplementedError
+
+
+def test_SQw_incoh(Q, w, SQw_incoh):
+
+    """
+    Validate the calculation of the dynamic incoherent structure factor against
+    nMOLDYN
+    """
+
+    raise NotImplementedError
+
+
+def test_SQw_coh(Q, w, SQw_coh):
+
+    """
+    Validate the calculation of the dynamic coherent structure factor against
+    nMOLDYN
+    """
+
+    raise NotImplementedError
+
+
+def test_SQw_total(Q, w, SQw_incoh, SQw_coh):
+
+    """
+    Validate the calculation of the dynamic total structure factor against the
+    sum of the dynamic incoherent and coherent structure factors calculated by
+    nMOLDYN
+    """
+
+    raise NotImplementedError
