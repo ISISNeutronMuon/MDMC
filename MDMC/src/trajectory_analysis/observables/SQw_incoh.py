Corrected when normalization occurs so that max(FQt)=1

MDMC/src/trajectory_analysis/observables/SQw_incoh.py
@@ -26,12 +26,12 @@ class SQwIncoherent(AbstractSQw):
                               in self.trajectory.configurations]
 
             # Normalise to the number of orthogonal vectors
-            rho = self._calculate_rho(atom_positions, Q_vector) \
+            rho = self._calculate_rho(atom_positions, Q_vector)
+            FQt_single_Q_atom = correlation(rho, normalise=True) \
                   / np.shape(Q_vector)[1]
-            FQt_single_Q_atom = correlation(rho, normalise=True)
             FQt_single_Q += FQt_single_Q_atom
 
-        return FQt_single_Q
+        return FQt_single_Q / n_atoms
 
     def _calculate_rho(self, positions, Q_vector):
 