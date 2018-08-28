783c70084e97cf26ab6b2b9227fcfb764c11b81f
Thomas Farmer <thomas.farmer27@hotmail.co.uk>
2018-08-28 13:08:07 +0100

    Added scipy dependency

--- setup.py
+++ setup.py
@@ -6,5 +6,5 @@ setup(
     packages=find_packages(),
     author="Thomas Farmer",
     url="https://github.com/MDMCproject",
-    install_requires=["numpy==1.8.0"]
+    install_requires=["numpy==1.8.0","scipy==0.13.0"]
 )
