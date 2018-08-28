8561bd33bff26bf2db5e75789187a4e6795890c8
Thomas Farmer <thomas.farmer27@hotmail.co.uk>
2018-08-28 11:21:18 +0100

    Include tests

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="MDMC",
     version="0.2pilot",
-    packages=find_packages(exclude=('tests')),
+    packages=find_packages(),
     author="Thomas Farmer",
     url="https://github.com/MDMCproject",
     install_requires=["numpy==1.8.0"]
