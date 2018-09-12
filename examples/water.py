fb009bcf987ecb909467d9191da2094933963536
Thomas Farmer <thomas.farmer27@hotmail.co.uk>
2018-09-12 10:58:10 +0100

    Corrected style

--- examples/water.py
+++ examples/water.py
@@ -16,8 +16,8 @@ from tests.test_data import data
 # Build universe
 universe = sim.Universe(dimensions=(10., 10., 10.), shape=sim.Shape.orthorhombic)
 H1 = su.Atom('H', mass=1.008)
-H2 = su.Atom('H', position=(0.151390, 0., 0.), mass=1.008)
-O = su.Atom('O', position=(0.075695, 0., 0.058588), mass=16.000)
+H2 = su.Atom('H', position=(1.51390, 0., 0.), mass=1.008)
+O = su.Atom('O', position=(0.75695, 0., 0.58588), mass=16.000)
 water_mol = su.Molecule(position=(0, 0, 0),
                         velocity=(0, 0, 0),
                         atoms=[H1, H2, O],
@@ -26,15 +26,26 @@ water_mol = su.Molecule(position=(0, 0, 0),
                                       su.Dispersion(O),
                                       su.BondAngle(atoms=[H1, O, H2])],
                         name='water')
-universe.fill(water_mol,force_field=ff.SPCE,num_density=0.0333679)
+universe.fill(water_mol, force_field=ff.SPCE, num_density=0.0333679)
 
 # MD Engine setup
-md_engine = sim.NVESimulation(universe, engine="mmtk", time_step=1, temperature=263,
-    integrator='velocity_verlet', lj_options=1.2, es_options={'method':'ewald'})
+md_engine = sim.NVESimulation(universe,
+                              engine="mmtk",
+                              time_step=1,
+                              temperature=263.,
+                              integrator='velocity_verlet',
+                              lj_options=1.2,
+                              es_options={'method':'ewald'},
+                              minimizer='steepest_descent')
 
-# Setup and run refinement
+# Setup refinement
+
 # exp_datasets is a list of dictionaries with one dictionary per experimental dataset
-exp_datasets = [{'file_name':data.LAMP_SQW_FILE, 'type':'SQw', 'reader':'LAMPSQw', 'weight':1.}]
+exp_datasets = [{'file_name':data.READER_DATA['LAMPSQw'],
+                 'type':'SQw',
+                 'reader':'LAMPSQw',
+                 'weight':1.}]
+
 # Fit parameters is a set(?) of all unique fit parameters in the universe which can then be filtered.
 fit_params = universe.parameters
 control = MDMCControl(MD_engine=md_engine,
