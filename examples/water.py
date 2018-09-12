1cb4b934b8b6da28cd0b5d6a00baf3bfe3c49827
Thomas Farmer <thomas.farmer27@hotmail.co.uk>
2018-09-12 10:56:48 +0100

    Added interpolation of experimental SQw for regular grid

--- examples/water.py
+++ examples/water.py
@@ -2,6 +2,9 @@
 An example MDMC script for optimizing spce parameters for water at 263 K
 """
 
+import numpy as np
+from scipy.interpolate import interp2d
+
 import MDMC.MD.simulation as sim
 import MDMC.MD.structural_units as su
 import MDMC.MD.force_fields as ff
@@ -34,6 +37,44 @@ md_engine = sim.NVESimulation(universe, engine="mmtk", time_step=1, temperature=
 exp_datasets = [{'file_name':data.LAMP_SQW_FILE, 'type':'SQw', 'reader':'LAMPSQw', 'weight':1.}]
 # Fit parameters is a set(?) of all unique fit parameters in the universe which can then be filtered.
 fit_params = universe.parameters
-control = MDMCControl(MD_engine=md_engine, exp_datasets=exp_datasets,
-    fit_params=fit_params, minimizer_type = "MMC")
-control.refine(n_steps=100)
+control = MDMCControl(MD_engine=md_engine,
+                      exp_datasets=exp_datasets,
+                      fit_params=fit_params,
+                      MC_norm=1,
+                      minimizer_type="MMC",
+                      MD_steps=103,
+                      t_resolution=30.)
+
+# Bertil Halle water data is non-symmetric, and has a non-rectangular grid with
+# a non-uniform E step.
+# To account for this, a limited E is used and undefined errors are set to zero
+# for the purposes of interpolation.
+# This should really be performed before the data is read into control - the
+# final step is a reflection of this as the MD observable is changed to match
+# the new independent variables of the experimental observable
+exp_obs = control.observable_pairs[0].exp_obs
+Q = exp_obs.Q
+E_range = (exp_obs.E >=0)
+E = exp_obs.E[E_range]
+SQw = np.array([Sw[E_range] for Sw in exp_obs.SQw])
+SQw_err = np.array([Sw_err[E_range] for Sw_err
+                          in exp_obs.SQw_err])
+SQw_fun = interp2d(E, Q, SQw)
+SQw_err_zero = SQw_err
+SQw_err_zero[SQw_err == np.float('inf')] = 0
+SQw_err_fun = interp2d(E, Q, SQw_err_zero)
+# Use the largest step size from the E data for the uniform step size
+E_step = max([E[i] - E[i-1] for i in np.arange(len(E) - 1) + 1])
+E_uniform = np.arange(E[0], E[-1], E_step)
+SQw_uniform = SQw_fun(E_uniform, Q)
+SQw_err_uniform = SQw_err_fun(E_uniform, Q)
+SQw_err_uniform[SQw_err_uniform == 0.] = np.float('inf')
+control.observable_pairs[0].exp_obs.independent_variables = {'E':E_uniform,
+                                                             'Q':Q}
+control.observable_pairs[0].exp_obs._dependent_variables = {'SQw':SQw_uniform}
+control.observable_pairs[0].exp_obs._errors = {'SQw':SQw_err_uniform}
+control.observable_pairs[0].MD_obs.independent_variables = {'E':E_uniform,
+                                                            'Q':Q}
+
+# Run refinement
+control.refine(n_steps=0)
