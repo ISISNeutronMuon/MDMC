a045ceaacd8d306c291b4c7807b001fa4f9e0307
Thomas Farmer <thomas.farmer27@hotmail.co.uk>
2018-10-19 09:51:52 +0100

    Corrected for change of argument name

--- tests/trajectory_analysis/test_SQw.py
+++ tests/trajectory_analysis/test_SQw.py
@@ -23,11 +23,13 @@ def SQw_from_data():
 @pytest.fixture
 def SQw_from_MD(trajectory, universe):
     SQw = of.ObservableFactory.create_observable('SQw')
-    cell = universe.dims
+    dims = universe.dims
     n_Q = 10
     t_resolution = 30.999425
-    Q_values = [2 * np.pi * i / cell[0] for i in range(1,n_Q+1)]
-    SQw.calculate_from_MD(trajectory, Q_values = Q_values, cell = cell,
+    Q_values = [2 * np.pi * i / dims[0] for i in range(1, n_Q+1)]
+    SQw.calculate_from_MD(trajectory,
+                          Q_values=Q_values,
+                          dims=dims,
                           t_resolution=t_resolution)
     return SQw
 
