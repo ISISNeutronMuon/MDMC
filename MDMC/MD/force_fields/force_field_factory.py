49237de254aa51248be04d2b95a096db1e8623d3
Thomas Farmer <thomas.farmer27@hotmail.co.uk>
2018-11-02 13:43:59 +0000

    Created a factory to generate forcefields

---
+++ MDMC/MD/force_fields/force_field_factory.py
@@ -0,0 +1,26 @@
+"""Factory class for generating force fields
+
+AUTHOR :    Thomas Farmer        START DATE :    02/11/2018, 13:17:31"""
+
+from importlib import import_module
+from inspect import isclass, isabstract, getmembers
+
+from MDMC.MD.force_fields.ff import ForceField
+
+class ForceFieldFactory(object):
+
+    """
+    Provides a factory for creating force fields.  Any facade within
+    the force fields folder can be created with a string of the class name, as
+    long as it is a subclass of ForceField.
+    """
+
+    @staticmethod
+    def create_facade(module_name):
+        module = import_module('.' + module_name, __package__)
+
+        classes = getmembers(module, lambda m: (isclass(m)
+                                                and not isabstract(m)
+                                                and issubclass(m, ForceField)))
+
+        return classes[0][1]()
