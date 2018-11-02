e4b8d9128225b7c59070236355201810894c00d6
Thomas Farmer <thomas.farmer27@hotmail.co.uk>
2018-11-02 13:47:43 +0000

    Renamed force_fields to ff, moved to force_fields dir, removed SPC/SPCE

---
+++ MDMC/MD/force_fields/ff.py
@@ -0,0 +1,41 @@
+"""A module for defining force fields that can be applied to a universe
+
+Each force field consists of a combination of interaction functions, and also
+the values of the parameters within these functions.  In this instance water
+models (such as SPCE and TIP3P) are also defined as force fields, even though
+the parameter sets are restricted to describing water.
+
+AUTHOR :    Thomas Farmer        START DATE :    2018-5-4 17:38:48"""
+
+from abc import ABCMeta,abstractmethod
+
+
+class ForceField:
+
+    """
+    Abstract class defining a force field
+
+    For each interaction type that it uses (non-bonded, bonds, bond angles etc),
+    a force field must define the interaction function (LJ, harmonic etc).  It
+    must also define the parameters for each of these functions.
+    """
+
+    __metaclass__ = ABCMeta
+
+    def __init__(self, interactions):
+
+        for interaction in interactions:
+            self.parameterize_interaction(interaction)
+
+    def parameterize_interaction(self, interaction):
+
+        int_type = type(interaction)
+        elements = interaction._element_tuple()
+        try:
+            interaction.function = self.interaction_dictionary[
+                (int_type, elements)]
+            interaction.function_name = interaction.function.name
+            interaction.function.set_params_interactions(interaction)
+        except KeyError:
+            raise KeyError("This force field does not have defined interactions"
+                           " for these element types")
