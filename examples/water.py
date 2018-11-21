560b8d0aff81e51ebc4b16213a4f93c9efc73e5c
Thomas Farmer <thomas.farmer27@hotmail.co.uk>
2018-11-21 20:40:33 +0000

    Changed trajectory steps to 1000

--- examples/water.py
+++ examples/water.py
@@ -41,7 +41,7 @@ md_engine = sim.Simulation(universe,
                            lj_options=12.,
                            es_options='ewald',
                            minimizer='steepest_descent',
-                           traj_step=500,
+                           traj_step=1000,
                            rigid=True,
                            threads=4)
 
