7ee495ad18f72e8fbf7096f340e495551b31631b
Thomas Farmer <thomas.farmer27@hotmail.co.uk>
2018-12-14 11:33:07 +0000

    Corrected style

--- MDMC/MD/engine_facades/mmtk.py
+++ MDMC/MD/engine_facades/mmtk.py
@@ -100,7 +100,7 @@ class MMTKEngine(MDEngine):
 
     def minimize(self, n_steps):
 
-        self.minimizer(steps = n_steps, step_size = 0.05*Units.Ang)
+        self.minimizer(steps=n_steps, step_size=0.05*Units.Ang)
         self.universe.initializeVelocitiesToTemperature(self.temperature)
 
     def run(self, n_steps, equilibration=False):
