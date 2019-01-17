75a6c37666541afed69e2b9fbddf3f7889d72065
Thomas Farmer <thomas.farmer27@hotmail.co.uk>
2019-01-17 16:23:10 +0000

    Changed test_copy_interaction implementation

--- tests/MD/test_simulation.py
+++ tests/MD/test_simulation.py
@@ -4,7 +4,6 @@
 
 from collections import Counter
 from copy import deepcopy
-from inspect import getargspec
 from itertools import permutations
 
 import numpy as np
@@ -404,64 +403,48 @@ def test_molecule_subunit_positions(water_molecule):
         assert all(atom.position == water_molecule.position + rel_pos[atom])
 
 
-def test_copy_interaction(water_SPCE_universe):
-
-    """
-    As __deepcopy__ has been implemented for Interaction, this tests that the
-    correct attributes are copied and others are modified
-
-    This includes when the interaction is copied when an atom is deepcopied
-    """
-
-    inter = list(water_SPCE_universe.interactions)[0]
-    cpy_inter = deepcopy(inter)
-    changed_attr = ['_atom_list']
-    for attr in inter.__dict__:
-        if attr in changed_attr:
-            assert getattr(cpy_inter, attr) != getattr(inter, attr)
-        else:
-            assert np.all(getattr(cpy_inter, attr) == getattr(inter, attr))
-
-    # Test interaction has references to the correct atom if the atom is copied
-    atom = deepcopy(water_SPCE_universe.atom_list[0])
-    for inter in atom.interactions:
-        assert atom in inter.atom_list
-
-
-@pytest.mark.parametrize("BondInt", [su.Bond, su.BondAngle])
-def test_bonded_interaction(BondInt, atom):
+@pytest.mark.parametrize("Int, n_atoms", [(su.Coulombic, [1]),
+                                          (su.Dispersion, [1]),
+                                          (su.Bond, [2]),
+                                          (su.BondAngle, [3, 4])])
+def test_interactions(Int, n_atoms, atom):
 
     """
     Tests that only the correct number of atoms can be used for the interaction
 
-    Tests that atoms added to bonded interactions are unique i.e. there are no
+    Tests that atoms added to interactions are unique i.e. there are no
     duplicates
     """
 
-    # -1 to account for self
-    argspec = getargspec(BondInt.__init__)
-    n_atoms = len(argspec.args) - 1
-    atoms = [atom]
-    for i in range(n_atoms-1):
-        atoms.append(deepcopy(atom))
+    bonded = issubclass(Int, su.BondedInteraction)
 
-    # Test correct number and values of atoms (i.e. no duplicates)
-    valid_bond = BondInt(*atoms)
-
-    # Test incorrect number of atoms
-    with pytest.raises(TypeError):
-        if argspec.varargs:
-            additional_atoms = 2
-        else:
-            additional_atoms = 1
-        for i in range(additional_atoms):
+    for n in n_atoms:
+        atoms = []
+        for _ in range(n):
             atoms.append(deepcopy(atom))
-        invalid_bond = BondInt(*atoms)
 
-    # Test duplicates
+        # Test correct number and values of atoms (i.e. no duplicates)
+        valid_bond = Int(*atoms)
+        assert len(valid_bond.atoms[-1]) == n
+
+    # Test zero atoms
+    empty_bond = Int()
+    assert len(empty_bond.atoms) == 0
+
+    # Test duplicates for bonded interactions
     atoms_duplicate = []
-    for i in range(n_atoms):
+    n_duplicates = max(n_atoms) if bonded else max(n_atoms) + 1
+    for _ in range(n_duplicates):
         atoms_duplicate.append(atom)
 
     with pytest.raises(ValueError):
-        invalid_bond = BondInt(*atoms_duplicate)
+        invalid_bond = Int(*atoms_duplicate)
+
+    # Test incorrect number of atoms for bonded interactions
+    if bonded:
+        for n in [min(n_atoms) - 1, max(n_atoms) + 1]:
+            atoms = []
+            for _ in range(n):
+                atoms.append(deepcopy(atom))
+            with pytest.raises(TypeError):
+                invalid_bond = Int(*atoms)
