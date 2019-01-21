6fdc579ef8f6312d8671b28eca21164f869d58c1
Thomas Farmer <thomas.farmer27@hotmail.co.uk>
2019-01-21 14:11:34 +0000

    Changed module name to LAMMPS

--- MDMC/MD/engine_facades/lammps.py
+++ MDMC/MD/engine_facades/lammps.py
@@ -11,7 +11,7 @@ steps, to be int.  Therefore it is always a good idea to be cast these variables
 when they are read from PyLammps e.g. int(lmp.variables['steps'].value).
 
 Note: A minor bug in LAMMPS (Dec 2018 version) means that nangletypes returned
-by PyLammps is incorrectly set to ndihedraltypes 
+by PyLammps is incorrectly set to ndihedraltypes
 
 AUTHOR :    Thomas Farmer        START DATE :    11/01/2019, 13:45:29"""
 
