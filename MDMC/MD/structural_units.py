76ce99390ce159d0da5a568ed1a13472cce33725
Thomas Farmer <thomas.farmer27@hotmail.co.uk>
2019-02-04 16:43:35 +0000

    Corrected __repr__ output style

--- MDMC/MD/structural_units.py
+++ MDMC/MD/structural_units.py
@@ -929,11 +929,11 @@ class Interaction:
                 '  function: {1},'
                 '  parameters: {2},'
                 '  universe: {3},'
-                '  elements: {4},'.format(self.name,
-                                          self.function,
-                                          params,
-                                          self.universe,
-                                          self.element_list()))
+                '  elements: {4}'.format(self.name,
+                                         self.function,
+                                         params,
+                                         self.universe,
+                                         self.element_list()))
 
     def __str__(self):
 
