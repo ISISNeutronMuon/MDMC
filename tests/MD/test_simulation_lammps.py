1e42163e1772b45afa42610a6a6fb8528da703c1
Thomas Farmer <thomas.farmer27@hotmail.co.uk>
2019-02-11 17:12:20 +0000

    Added placeholders for tests for universe creation

---
+++ tests/MD/test_simulation_lammps.py
@@ -0,0 +1,68 @@
+"""Tests for setting up and running MDMC using LAMMPS
+
+AUTHOR :    Thomas Farmer        START DATE :    11/02/2019, 16:21:38"""
+
+
+def test_universe_dims():
+
+    """
+    Tests that creating a simulation box from an MDMC universe results in the
+    correct universe dimensions
+    """
+
+    pass
+
+
+def test_universe_shape():
+
+    """
+    Tests that creating a simulation box from MDMC universe results in the
+    correct universe shape
+    """
+
+    pass
+
+
+def test_number_elements():
+
+    """
+    Tests that creating a simulation box from an MDMC universe results in the
+    correct number of elements
+    """
+
+    pass
+
+
+def test_number_interaction_types():
+
+    """
+    Tests that creating a simulation box from an MDMC universe results in the
+    correct number of each interaction type:
+
+    - bond
+    - angle
+    - dihedral
+    - improper
+
+    DIHEDRAL AND IMPROPER ARE NOT CURRENTLY IMPLEMENTED
+    """
+
+    pass
+
+
+def test_number_interactions_per_atom():
+
+    """
+    Tests that creating a simulation box from an MDMC universe results in the
+    correct allowed number of interactions per atom for each interaction type:
+
+    - bond
+    - angle
+    - dihedral
+    - improper
+
+    DIHEDRAL AND IMPROPER ARE NOT CURRENTLY IMPLEMENTED
+    """
+
+    pass
+
