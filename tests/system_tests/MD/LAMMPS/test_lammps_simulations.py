b2c38f7f5d851314dce32fbf7aa8109dfe2e0094
Thomas Farmer <thomas.farmer27@hotmail.co.uk>
2019-02-25 18:03:11 +0000

    Added imports and simulation constants

--- tests/system_tests/MD/LAMMPS/test_lammps_simulations.py
+++ tests/system_tests/MD/LAMMPS/test_lammps_simulations.py
@@ -7,3 +7,18 @@ calculations of the properties in both cases are performed by LAMMPS, the only
 difference is whether the LAMMPS simulation was run through MDMC.
 
 AUTHOR :    Thomas Farmer        START DATE :    22/02/2019, 13:50:29"""
+
+import numpy as np
+import pytest
+
+from MDMC.MD.simulation import Universe, Simulation, Shake, PPPM
+from MDMC.MD.structural_units import Atom, Bond, BondAngle, Coulombic, \
+    Dispersion, Molecule
+
+N_MOLECULES = 216
+DIMENSION = 18.63
+TEMPERATURE = 300.
+# Number of steps between logging of thermo_style variables
+THERMO_STEPS = 100
+MD_STEPS = 1000
+
