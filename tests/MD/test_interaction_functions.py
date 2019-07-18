ef8990aed4fce3d119a1a76e2f3ef7b78d8b5253
Thomas Farmer <thomas.farmer27@hotmail.co.uk>
2019-07-19 00:24:02 +0100

    Added tests of parameter tying

--- tests/MD/test_interaction_functions.py
+++ tests/MD/test_interaction_functions.py
@@ -8,6 +8,31 @@ VALUE = 1.0
 NAME = 'length'
 UNIT = Unit('Ang')
 
+@pytest.fixture
+def parameter():
+
+    """
+    Returns
+    -------
+    Parameter
+        A Parameter with a value and a name
+    """
+
+    return Parameter(UnitFloat(VALUE, UNIT), NAME)
+
+
+@pytest.fixture
+def scaled_parameter():
+
+    """
+    Returns
+    -------
+    Parameter
+        A Parameter with a scaled value and a name
+    """
+
+    return Parameter(UnitFloat(5 * VALUE, UNIT), NAME)
+
 
 @pytest.mark.parametrize('value, unit', [(VALUE, UNIT),
                                          (UnitFloat(VALUE, UNIT), None)])
@@ -23,3 +48,42 @@ def test_parameter_value_init(value, unit):
     assert param.value == VALUE
     assert param.unit == UNIT
 
+
+def test_tied_parameters(parameter, scaled_parameter):
+
+    """
+    Tests that parameters that are tied with a scale of 1 have the same value,
+    including after the value of the tie parameter is changed
+    """
+
+    assert scaled_parameter.value != parameter.value
+
+    scaled_parameter.set_tie(parameter, "* 1")
+    assert scaled_parameter.value == parameter.value
+
+    parameter.value *= 2.
+    assert scaled_parameter.value == parameter.value
+
+
+def test_tied_parameter_change_warning(parameter, scaled_parameter):
+
+    """
+    Tests that parameters that are tied issue a warning when the value is set,
+    and that the value does not change
+    """
+
+    scaled_parameter.set_tie(parameter, "* 1")
+    with pytest.warns(UserWarning):
+        scaled_parameter.value = parameter.value * 10.
+    assert scaled_parameter.value == parameter.value
+
+
+def test_parameter_tied(parameter, scaled_parameter):
+
+    """
+    Tests that tying a Parameter changes tied attribute from False to True
+    """
+
+    assert not scaled_parameter.tied
+    scaled_parameter.set_tie(parameter, "* 1")
+    assert scaled_parameter.tied
