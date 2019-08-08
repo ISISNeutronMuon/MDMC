3dd83ec1a85305e78c2c6642b38fe8e261f52d3e
Thomas Farmer <thomas.farmer27@hotmail.co.uk>
2019-08-08 23:07:01 +0100

    Changed configuration imports to coordinates

--- MDMC/MD/simulation.py
+++ MDMC/MD/simulation.py
@@ -10,8 +10,7 @@ import numpy as np
 
 from MDMC.common.decorators import unit_decorator, unit_decorator_getter
 from MDMC.common import units
-from MDMC.common.configurations import (molec_from_dict, molec_list_from_config,
-                                        SPCE)
+from MDMC.common import coordinates
 from MDMC.MD.engine_facades.facade_factory import MDEngineFacadeFactory
 from MDMC.MD.force_fields.force_field_factory import ForceFieldFactory
 from MDMC.MD.structural_units import Coulombic, Dispersion
