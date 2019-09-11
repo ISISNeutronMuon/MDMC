96fb168bca996e75b215d7796d7bc94df75d005c
Thomas Farmer <thomas.farmer27@hotmail.co.uk>
2019-09-11 17:26:16 +0100

    Changed np.tile to np.broadcast_to

--- MDMC/trajectory_analysis/observables/SQw.py
+++ MDMC/trajectory_analysis/observables/SQw.py
@@ -600,8 +600,8 @@ class AbstractSQw(Observable):
         window = function(self.t[:len(self.E)] / 1000., params['sigma'],
                           norm=False)
 
-        # Tile the window so that it is applied for all Q values
-        return np.tile(window, [N_Q, 1]) * FQt
+        # Broadcast the window so that it is applied for all Q values
+        return np.broadcast_to(window, (N_Q, ) + np.shape(window)) * FQt
 
 
 class SQw(AbstractSQw):
