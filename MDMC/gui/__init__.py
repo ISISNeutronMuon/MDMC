9496b980677408e99dce7c3ebbcdb831829bcca2
TOFarmer <thomas.farmer27@hotmail.co.uk>
2020-01-08 15:39:57 +0000

    Changed atom_collection to atom_container

--- MDMC/gui/__init__.py
+++ MDMC/gui/__init__.py
@@ -4,29 +4,29 @@
 from MDMC.MD.ase import viewer
 
 
-def view(atom_collection):
+def view(atom_container):
 
     """
     Launches a GUI for viewing collections of Atoms
 
     Parameters
     ----------
-    atom_collection : list of Atoms, Molecule, Universe
+    atom_container : list of Atoms or an AtomContainer
         An object which contains some atoms. This can either be in the sense
         that it is a list of ``Atom`` objects, or it could be an object which
-        has the ``atom_list`` attribute, such as ``Molecule`` or ``Universe``.
-        If ``atom_collection`` also has a ``dimensions`` attribute (such as
+        has the ``atom_list`` attribute (e.g. an ``AtomContainer``). If
+        ``atom_collection`` also has a ``dimensions`` attribute (such as
         ``Universe``), then this is used to set the volume displayed; otherwise
         the volume is determined by the extents of the atoms.
     """
 
     try:
-        dimensions = atom_collection.dimensions
+        dimensions = atom_container.dimensions
     except AttributeError:
         dimensions = None
     try:
-        atoms = atom_collection.atom_list
+        atoms = atom_container.atom_list
     except AttributeError:
-        atoms = atom_collection
+        atoms = atom_container
 
     viewer.view(atoms, cell=dimensions)
