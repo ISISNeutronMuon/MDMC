6cf8f10e6a2e16affa628c2f45142bfc5cc81b8e
TOFarmer <thomas.farmer27@hotmail.co.uk>
2020-01-20 12:20:52 +0000

    Add test that extension must be overridden in ConfigurationReader subclasses

--- tests/readers/test_configuration_reader.py
+++ tests/readers/test_configuration_reader.py
@@ -5,8 +5,32 @@ ConfigurationReaderFactory
 import pytest
 
 from MDMC.readers.configurations.cif import CIF
+from MDMC.readers.configurations.conf_reader import ConfigurationReader
 from MDMC.readers.configurations.conf_reader_factory import \
     ConfigurationReaderFactory
+
+
+def test_configuration_reader_extension_error():
+
+    """
+    Tests that subclasses of ConfigurationReader require an extension to be
+    defined
+    """
+
+    class DummyConfig(ConfigurationReader):
+
+        def parse(self, **settings):
+
+            self._atoms = None
+
+        @property
+        def atoms(self):
+
+            return self._atoms
+
+    with pytest.raises(TypeError):
+        #pylint: disable=abstract-class-instantiated
+        DummyConfig()
 
 
 def test_create_reader_from_ext():
