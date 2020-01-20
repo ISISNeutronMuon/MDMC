9a88151fb23919df8efc0eed9a22eedcd7759097
TOFarmer <thomas.farmer27@hotmail.co.uk>
2020-01-20 11:52:19 +0000

    Add create_reader_from_ext

--- MDMC/readers/configurations/conf_reader_factory.py
+++ MDMC/readers/configurations/conf_reader_factory.py
@@ -1,18 +1,62 @@
 """Factory class for generating readers for configurations"""
 
+from importlib import import_module
+from inspect import getmembers, isabstract, isclass
+from os.path import dirname
+from pkgutil import iter_modules
+
 from MDMC.readers.reader_factory import ReaderFactory
 from MDMC.readers.configurations.conf_reader import ConfigurationReader
 
 
-class ObservableReaderFactory(ReaderFactory):
+class ConfigurationReaderFactory(ReaderFactory):
 
     """
     Provides a factory for creating readers.  Any module within the readers
     submodule can be created with a string of the class name, as long as it is a
-    subclass of Reader.
+    subclass of `ConfigurationReader`.
     """
 
     @staticmethod
     def base_class():
 
         return ConfigurationReader
+
+    @classmethod
+    def create_reader_from_ext(cls, extension):
+
+        """
+        Parameters
+        ----------
+        extension : str
+            The file extension from which to initialize a subclass of
+            `ConfigurationReader`
+
+        Returns
+        -------
+        ConfigurationReader subclass
+            An initialized configuration reader which has the extension
+            specified by `extension`
+
+        Raises
+        ------
+        NotImplementedError
+            If `extension` does not match the `extension` property of any
+            subclass of `ConfigurationReader`
+        """
+
+        for module_info in iter_modules([dirname(__file__)]):
+            name = module_info.name
+            if not name.startswith('_'):
+                module = import_module('.' + name, __package__)
+                classes = getmembers(module,
+                                     lambda m: (isclass(m)
+                                                and not isabstract(m)
+                                                and issubclass(m,
+                                                               cls.base_class)
+                                               ))
+                if classes[0][1].extension == extension:
+                    return name()
+
+        raise NotImplementedError('No implemented reader is compatible with {}'
+                                  ' extension'.format(extension))
