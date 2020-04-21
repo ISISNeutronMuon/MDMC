ec63aff593e8e16b22907af16f97310e97b7dcc1
TOFarmer <thomas.farmer27@hotmail.co.uk>
2020-04-21 12:13:14 +0100

    Removed `data` from excluded list

--- MDMC/MD/__init__.py
+++ MDMC/MD/__init__.py
@@ -27,7 +27,7 @@ from .structural_units import *
 # Get the class of each force field
 for _, name, _ in iter_modules(force_fields.__path__,
                                force_fields.__name__ +'.'):
-    if name.split('.')[-1] not in ['ff', 'force_field_factory', 'data']:
+    if name.split('.')[-1] not in ['ff', 'force_field_factory']:
         module = import_module(name)
         cls = getmembers(module, lambda m: (isclass(m)
                                             and not isabstract(m)))[0][1]
