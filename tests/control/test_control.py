4b4c6f926a4d67532a7f5fc213063f4b79193b87
TOFarmer <thomas.farmer27@hotmail.co.uk>
2020-07-22 10:10:00 +0100

    Added test for stdout from Control.refine

--- tests/control/test_control.py
+++ tests/control/test_control.py
@@ -36,10 +36,11 @@ def mock_update_engine_parameters(self):
     pass
 
 
-def test_control_refine(monkeypatch, capsys):
+def test_control_refine_stdout(monkeypatch, capsys):
 
     """
-
+    Tests that the stdout from Control.refine is in the expected format. Test
+    considers float, str, int all of variable lengths.
     """
 
     monkeypatch.setattr(control.Control, "_generate_FoM", mock_generate_FoM)
@@ -51,9 +52,20 @@ def test_control_refine(monkeypatch, capsys):
                                   'str': ['str1', 'test', 'Accepted'],
                                   'int': [10, 100, 1000]})
 
+    minim.history = pd.concat([minim.history]*4, ignore_index=True)
     cont = control.Control(None, [], [], reset_config=False)
     cont.minimizer = minim
-    import pdb; pdb.set_trace()
-    cont.refine(2)
+    cont.refine(10)
     stdout = capsys.readouterr().out
-    # assert stdout == 'This'
+    assert stdout == ('Step     float        str        int\n'
+                      '   0     1.657       str1         10\n'
+                      '   1       2.0       test        100\n'
+                      '   2   3.87346   Accepted       1000\n'
+                      '   3     1.657       str1         10\n'
+                      '   4       2.0       test        100\n'
+                      '   5   3.87346   Accepted       1000\n'
+                      '   6     1.657       str1         10\n'
+                      '   7       2.0       test        100\n'
+                      '   8   3.87346   Accepted       1000\n'
+                      '   9     1.657       str1         10\n'
+                      '  10       2.0       test        100\n')
