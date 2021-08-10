a1398b576dcb5daeeb555a8f1892e6211d29f598
alexhroom <alex.room@btinternet.com>
2021-08-10 14:32:54 +0100

    Better integrates Docker rebuild check into travis workflow

--- .travis/require_rebuild_check.sh
+++ .travis/require_rebuild_check.sh
@@ -1,11 +1,15 @@
 ####### This script tests whether the Docker image needs to be rebuilt
 ####### i.e. if there are changes to requirements.txt or the Dockerfile
 
-#!/bin/bash -ex
+#!/bin/bash 
 
-if ! git diff remotes/origin/master master --name-only -- requirements.txt | read REPLY && \
-  ! git diff remotes/origin/master master --name-only -- ./build/Docker | read REPLY
+if ! git diff ${TRAVIS_BRANCH} ${TRAVIS_PULL_REQUEST_BRANCH} --name-only -- requirements.txt | read REPLY && \
+  ! git diff ${TRAVIS_BRANCH} ${TRAVIS_PULL_REQUEST_BRANCH} --name-only -- ./build/Docker | read REPLY
 then
   echo; echo "Docker file does not require rebuilding." 
-  exit 0;
+  exit 1; # fails harmlessly
+else
+  echo; echo "Docker file requires rebuilding."
+  REBUILD=true # sets rebuild variable to true so other docker test does not run
+  exit 0; # continues docker rebuild script 
 fi
