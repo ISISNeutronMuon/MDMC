763f7c4544eee6c894251f4cd475ff648a2ecb01
alexhroom <alex.room@btinternet.com>
2021-09-01 08:12:06 +0100

    Better implementation of bash if statement

--- .travis/documentation_testing.sh
+++ .travis/documentation_testing.sh
@@ -12,9 +12,9 @@
 #!/bin/bash 
 
 ### PR testing here
-if ${TRAVIS_EVENT_TYPE}!=cron
+if [ ${TRAVIS_EVENT_TYPE} != cron ];
 then  
-  if git diff remotes/origin/${TRAVIS_BRANCH} remotes/origin/${TRAVIS_PULL_REQUEST_BRANCH} --name-only -- ./doc | read REPLY
+  if git diff remotes/origin/${TRAVIS_BRANCH} remotes/origin/${TRAVIS_PULL_REQUEST_BRANCH} --name-only -- ./doc | read REPLY;
   then
     echo; echo "Documentation requires testing."
     docker pull mdmc/mdmc:latest
