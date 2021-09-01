c5de4d2d1b78c27c4331fb84639771d8e43ef88c
alexhroom <alex.room@btinternet.com>
2021-09-01 08:01:21 +0100

    Created documentation test and deployment script

--- .travis/documentation_testing.sh
+++ .travis/documentation_testing.sh
@@ -2,28 +2,26 @@
 ####### i.e. if there are changes to requirements.txt or the Dockerfile
 
 # the workflow of this script is the following:
-# 1a. if the job is a PR, and not a cron job, test whether changes have been made to the doc folder of the PR branch.
-# 1b. if so, test the documentation builds correctly.
-# 1c. if not, do not test and return a success.
+# 1a. if the job is a PR, and not a cron job, test whether changes have been made to the doc folder of the PR branch. (line 18)
+# 1b. if so, test the documentation builds correctly. (lines 20-22)
+# 1c. if not, do not test and return a success. (lines 24-25)
 
-# 2a. if the job is a cron job, test the documentation.
-# 2b. if the documentation test is successful, deploy it as a PR to our Github Pages repository.
-# 2c. if not successful, return an error.
+# 2a. if the job is a cron job, test the documentation. (lines 31-32)
+# 2b. if the documentation test is successful, deploy it as a PR to our Github Pages repository. (lines 33-39)
 
 #!/bin/bash 
 
-# the type != cron line is to avoid giving unnecessary and contradictory output messages.
-if ${TRAVIS_EVENT_TYPE}!=cron
 ### PR testing here
+if ${TRAVIS_EVENT_TYPE}!=cron
 then  
-  if ! git diff remotes/origin/${TRAVIS_BRANCH} remotes/origin/${TRAVIS_PULL_REQUEST_BRANCH} --name-only -- ./doc | read REPLY
+  if git diff remotes/origin/${TRAVIS_BRANCH} remotes/origin/${TRAVIS_PULL_REQUEST_BRANCH} --name-only -- ./doc | read REPLY
   then
-    echo; echo "Documentation does not require testing." 
-    exit 0;
-  else
     echo; echo "Documentation requires testing."
     docker pull mdmc/mdmc:latest
     docker run -t --mount type=bind,source=$(pwd),target=$(pwd) mdmc/mdmc:latest /bin/bash -c  "cd $(pwd) && apt-get update &&  apt-get install pandoc -y && pip3 install sphinx nbsphinx sphinx_rtd_theme docutils==0.16 . && make -d -C $(pwd)/doc html"
+  else
+    echo; echo "Documentation does not require testing."
+	exit 0;
   fi
   
   ### cron job testing & deployment here
@@ -39,5 +37,4 @@ then
     cd ./MDMCproject.github.io
     git checkout -B travis --track origin/travis 
     git add * && git commit -m "Travis weekly documentation update" && git push
-
- 
+fi
