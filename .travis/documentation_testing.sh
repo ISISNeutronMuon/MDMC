85710a23fb7b5a3d697a40e8964922fab8045c54
alexhroom <alex.room@btinternet.com>
2021-10-01 14:12:56 +0100

    Fixed another typo [no ci]

--- .travis/documentation_testing.sh
+++ .travis/documentation_testing.sh
@@ -15,12 +15,12 @@
 if [ ${TRAVIS_EVENT_TYPE} != cron ]
 then  
   if git diff remotes/origin/${TRAVIS_BRANCH} remotes/origin/${TRAVIS_PULL_REQUEST_BRANCH} --name-only -- ./doc | read REPLY && \
-  ! git diff diff remotes/origin/${TRAVIS_BRANCH} remotes/origin/${TRAVIS_PULL_REQUEST_BRANCH} -- requirements.txt | grep  '+ipython\|+ipykernel' # if there is a change to doc but not to ipython
+  ! git diff remotes/origin/${TRAVIS_BRANCH} remotes/origin/${TRAVIS_PULL_REQUEST_BRANCH} -- requirements.txt | grep  '+ipython\|+ipykernel' # if there is a change to doc but not to ipython
   then
     echo; echo "Documentation requires testing."
     docker pull mdmc/mdmc:latest
     docker run -t --mount type=bind,source=$(pwd),target=$(pwd) mdmc/mdmc:latest /bin/bash -c  "cd $(pwd) && apt-get update &&  apt-get install pandoc -y && pip3 install sphinx nbsphinx sphinx_rtd_theme docutils==0.16 . && make -d -C $(pwd)/doc html"
-  elif git diff diff remotes/origin/${TRAVIS_BRANCH} remotes/origin/${TRAVIS_PULL_REQUEST_BRANCH} -- requirements.txt | grep  '+ipython\|+ipykernel' # if there is a change to ipython which affects jupyter
+  elif git diff remotes/origin/${TRAVIS_BRANCH} remotes/origin/${TRAVIS_PULL_REQUEST_BRANCH} -- requirements.txt | grep  '+ipython\|+ipykernel' # if there is a change to ipython which affects jupyter
   then
     echo; echo "Documentation requires testing on new image"
 	docker pull mdmc/mdmc:travis
