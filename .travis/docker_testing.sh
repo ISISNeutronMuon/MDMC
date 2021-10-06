8b86381cab80d7ba7b394865ed22bb70f506f3e2
alexhroom <alex.room@btinternet.com>
2021-10-06 15:06:27 +0100

    expanded login [no ci]

--- .travis/docker_testing.sh
+++ .travis/docker_testing.sh
@@ -8,17 +8,18 @@
 # 2. if it does, rebuild the Docker container under the tag mdmc/mdmc:travis. Test the software inside this new container, then push the new container.
 # 3. if it doesn't, test with the original container (mdmc/mdmc:latest)
 
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin # this login circumvents the Docker IP rate limit for anonymous users
 if ! git diff remotes/origin/${TRAVIS_BRANCH} remotes/origin/${TRAVIS_PULL_REQUEST_BRANCH} --name-only -- requirements.txt | read REPLY && \
   ! git diff remotes/origin/${TRAVIS_BRANCH} remotes/origin/${TRAVIS_PULL_REQUEST_BRANCH} --name-only -- .dev_requirements.txt | read REPLY && \
   ! git diff remotes/origin/${TRAVIS_BRANCH} remotes/origin/${TRAVIS_PULL_REQUEST_BRANCH} --name-only -- ./build/Docker | read REPLY
 then
   echo; echo "Docker file does not require rebuilding." 
-  echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin # this login circumvents the Docker IP rate limit for anonymous users
   docker pull mdmc/mdmc:latest
-  docker logout
   docker run -t --mount type=bind,source=$(pwd),target=$(pwd) mdmc/mdmc:latest python3 -m pytest -s $(pwd)/tests/ --cov=$(pwd)/MDMC --cov-report xml
 else
   echo; echo "Docker file requires rebuilding."
+  echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin # this login circumvents the Docker IP rate limit for anonymous users
   docker build -t mdmc/mdmc:travis -f $(pwd)/build/Docker/Dockerfile . || exit 1 
   docker run -t --mount type=bind,source=$(pwd),target=$(pwd) mdmc/mdmc:travis python3 -m pytest -s $(pwd)/tests/ --cov=$(pwd)/MDMC --cov-report xml
 fi
+docker logout
