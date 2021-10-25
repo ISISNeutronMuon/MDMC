0b5d629b0a14a69d86e165d326ee18baf51bb269
alexhroom <alex.room@btinternet.com>
2021-10-25 18:30:15 +0100

    Fixed other uses of environmental variables.

--- .github/scripts/documentation_testing.sh
+++ .github/scripts/documentation_testing.sh
@@ -12,14 +12,14 @@
 # 2b. if the documentation test is successful, deploy it as a PR to our Github Pages repository. (lines 33-39)
 
 ### PR testing here
-if [ ${{ env.GITHUB_EVENT_NAME }} != schedule ]
-  if git diff remotes/origin/${{ env.GITHUB_BASE_REF }} remotes/origin/${{ env.GITHUB_HEAD_REF }} --name-only -- ./doc | read REPLY && \
-  ! git diff remotes/origin/${{ env.GITHUB_BASE_REF }} remotes/origin/${{ env.GITHUB_HEAD_REF }} -- requirements.txt | grep  '+ipython\|+ipykernel' # if there is a change to doc but not to ipython
+if [ "$EVENT_TYPE" != schedule ]
+  if git diff remotes/origin/master remotes/origin/"$BRANCH" --name-only -- ./doc | read REPLY && \
+  ! git diff remotes/origin/master remotes/origin/"$BRANCH" -- requirements.txt | grep  '+ipython\|+ipykernel' # if there is a change to doc but not to ipython
   then
     echo; echo "Documentation requires testing."
     docker pull mdmc/mdmc:latest
     docker run -t --mount type=bind,source=$(pwd),target=$(pwd) mdmc/mdmc:latest /bin/bash -c  "cd $(pwd) && apt-get update &&  apt-get install pandoc -y && pip3 install sphinx nbsphinx sphinx_rtd_theme docutils==0.16 . && make -d -C $(pwd)/doc html"
-  elif git diff remotes/origin/${{ env.GITHUB_BASE_REF }} remotes/origin/${{ env.GITHUB_HEAD_REF }} -- requirements.txt | grep  '+ipython\|+ipykernel' # if there is a change to ipython which affects jupyter
+  elif git diff remotes/origin/master remotes/origin/"$BRANCH" -- requirements.txt | grep  '+ipython\|+ipykernel' # if there is a change to ipython which affects jupyter
   then
     echo; echo "Documentation requires testing on new image"
     docker pull mdmc/mdmc:travis
