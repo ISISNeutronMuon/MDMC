9b0052400bae635fab644215de388ad1fc65c028
alexhroom <alex.room@btinternet.com>
2021-12-09 12:03:05 +0000

    Removed redundant tests [no ci]

--- tests/trajectory_analysis/test_SQw.py
+++ tests/trajectory_analysis/test_SQw.py
@@ -152,35 +152,6 @@ def test_trajectory_assertions(SQw_from_MD, trajectory, altered_trajectory):
         SQw_obj.calculate_from_MD(MD_input)
 
 
-@pytest.mark.parametrize('verbosity, output_type, prints',
-                         [(0, None, False), (1, float, True), (2, list, True), (3, list, True)])
-def test_verbose(SQw_from_MD, trajectory, verbosity, output_type, prints, capsys):
-
-    """
-    Test that we return nothing on verbose 0, a single timings float on verbose 1,
-    and a list of timings on verbose 3 and 4. Also test that print to stdout is done for verbosity >0,
-    and that the number of verbosity steps is correct.
-
-    The pytest parameter ``verbose_tuple`` has the value of ``verbose`` as the
-    first element, the expected type of timings as the second element, and
-    whether stdout should contain information as the third element.
-    """
-
-    SQw_obj = SQw_from_MD()
-    # record warnings to ensure no user warning over steps is given
-    timings = SQw_obj.calculate_from_MD(trajectory,
-                                        verbose=verbosity)
-
-    # workaround as Python doesn't just let you say 'NoneType'
-    if verbosity == 0:
-        output_type = type(None)
-
-    assert type(timings) == output_type
-
-    stdout = capsys.readouterr().out
-    assert (len(stdout) > 0) == prints
-
-
 def test_sqw_verbose_steps(SQw_from_MD):
     """Test that the number of verbosity steps for SQw.calculate_from_MD is correct."""
     SQw_obj = SQw_from_MD()
