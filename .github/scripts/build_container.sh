9de8bfec44305f1c307b4bed4553ceb9d0eb4921
alexhroom <alex.room@btinternet.com>
2022-01-07 15:52:31 +0000

    Separated container building out into its own script and workflow job

---
+++ .github/scripts/build_container.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# this script detects if a new docker container needs building, and if so, builds it.
+
+echo "$DOCKER_PASSWORD" | docker login -u "mdmc" --password-stdin # this login circumvents the Docker IP rate limit for anonymous users
+if ! git diff remotes/origin/master remotes/origin/"$BRANCH" --name-only | grep 'build/\|requirements.txt'
+then
+  echo "Docker file does not require rebuilding." 
+else
+  echo "Docker file requires rebuilding."
+  docker build -t mdmc/mdmc:ci-$BRANCH -f "$(pwd)"/build/Docker/Dockerfile . || exit 1 
+  docker push mdmc/mdmc:ci-$BRANCH
+fi
+docker logout
+exit 0
+
