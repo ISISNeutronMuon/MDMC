82920eef9963748bec3996fa5d1bfd98e4459adb
alexhroom <alex.room@btinternet.com>
2022-01-10 10:38:28 +0000

    Fixed pytest marks and singularity script

--- tests/system_tests/MD/LAMMPS/test_lammps_simulations.py
+++ tests/system_tests/MD/LAMMPS/test_lammps_simulations.py
@@ -8,12 +8,11 @@ difference is whether the LAMMPS simulation was run through MDMC."""
 
 import numpy as np
 import pytest
+pytestmark = [pytest.mark.mpi, pytest.mark.lammps]
 
 from MDMC.MD.simulation import Universe, Simulation, Shake, PPPM
 from MDMC.MD.structural_units import Atom, Molecule
 from MDMC.MD.interactions import Bond, BondAngle, Dispersion, Coulombic
-
-pytestmark = [pytest.mark.mpi, pytest.mark.lammps]
 
 # STDEV_FAC is the number of standard deviations within which the calculated
 # property must lie for it to be considered equivalent to the expected value
