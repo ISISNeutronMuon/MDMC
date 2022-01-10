b3a2f4112d9765240969136a635fde49e19c3c90
alexhroom <alex.room@btinternet.com>
2022-01-10 10:12:53 +0000

    Separated pull request and cron builds entirely, but added pull request to cron workflow for testing

--- .github/scripts/singularity_setup.sh
+++ .github/scripts/singularity_setup.sh
@@ -12,7 +12,7 @@ SINGULARITY_BASE="${GOPATH}/src/github.com/sylabs/singularity"
 export PATH="${GOPATH}/bin:${PATH}"
 export SVER="3.8.1" #SVER is Singularity VERsion
 
-mkdir -p "${GOPATH}/src/github.com/sylabs"
+sudo mkdir -p "${GOPATH}/src/github.com/sylabs"
 cd "${GOPATH}/src/github.com/sylabs"
 
 wget https://github.com/hpcng/singularity/releases/download/v${SVER}/singularity-${SVER}.tar.gz
