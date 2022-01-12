0844419a62772b7519b9626ce3e7b13606b9f7ef
alexhroom <alex.room@btinternet.com>
2022-01-12 10:05:00 +0000

    Edited various comments and fixed image tag

--- .github/scripts/documentation_testing.sh
+++ .github/scripts/documentation_testing.sh
@@ -2,17 +2,19 @@
 
 # build the documentation, which includes testing that the code in the tutorials runs
 
+# tests if there is a change to doc but not to ipython
 if git diff remotes/origin/master remotes/origin/"$BRANCH" --name-only -- ./doc | read REPLY && \
-! git diff remotes/origin/master remotes/origin/"$BRANCH" -- requirements.txt | grep  '+ipython\|+ipykernel' # if there is a change to doc but not to ipython
+! git diff remotes/origin/master remotes/origin/"$BRANCH" -- requirements.txt | grep  '+ipython\|+ipykernel' 
 then
   echo; echo "Documentation requires testing."
   docker pull mdmc/mdmc:latest
   docker run -t --mount type=bind,source="$(pwd)",target="$(pwd)" mdmc/mdmc:latest /bin/bash -c  "cd $(pwd) && apt-get update &&  apt-get install pandoc -y && pip3 install sphinx nbsphinx sphinx_rtd_theme docutils==0.16 . && make -d -C $(pwd)/doc html"
-elif git diff remotes/origin/master remotes/origin/"$BRANCH" -- requirements.txt | grep  '+ipython\|+ipykernel' # if there is a change to ipython which affects jupyter
+# tests if there is a change to ipython which affects jupyter
+elif git diff remotes/origin/master remotes/origin/"$BRANCH" -- requirements.txt | grep  '+ipython\|+ipykernel'
 then
   echo; echo "Documentation requires testing on new image"
-  docker pull mdmc/mdmc:travis
-  docker run -t --mount type=bind,source="$(pwd)",target="$(pwd)" mdmc/mdmc:travis /bin/bash -c  "cd $(pwd) && apt-get update &&  apt-get install pandoc -y && pip3 install sphinx nbsphinx sphinx_rtd_theme docutils==0.16 . && make -d -C $(pwd)/doc html"
+  docker pull mdmc/mdmc:ci-$BRANCH
+  docker run -t --mount type=bind,source="$(pwd)",target="$(pwd)" mdmc/mdmc:ci-$BRANCH /bin/bash -c  "cd $(pwd) && apt-get update &&  apt-get install pandoc -y && pip3 install sphinx nbsphinx sphinx_rtd_theme docutils==0.16 . && make -d -C $(pwd)/doc html"
 else
   echo; echo "Documentation does not require testing."
   exit 0;
