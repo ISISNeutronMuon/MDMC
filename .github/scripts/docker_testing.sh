b4af1638e067a823964fab7974b1735d83c63626
alexhroom <alex.room@btinternet.com>
2022-01-12 10:05:00 +0000

    Edited various comments and fixed image tag

--- .github/scripts/docker_testing.sh
+++ .github/scripts/docker_testing.sh
@@ -8,7 +8,7 @@
 # 2. if it does, test MDMC in mdmc/mdmc:latest.
 # 3. if it doesn't, exit.
 
-# $TESTSET is defined in CI so we only need one script for both lammps and non-lammps tests in parallel
+# $TESTSET is defined in the main CI .yml script
 
 echo "$DOCKER_PASSWORD" | docker login -u "mdmc" --password-stdin # this login circumvents the Docker IP rate limit for anonymous users
 if ! git diff remotes/origin/master remotes/origin/"$BRANCH" --name-only | grep 'MDMC/\|requirements.txt'
@@ -18,7 +18,9 @@ then
 else
   echo "Source code requires testing."
   # read this run block as 'attempt to pull the built docker container and test on it; if it fails (i.e. no container was built) then just test on latest instead'.
-  docker pull mdmc/mdmc:ci-$BRANCH && docker run -t --mount type=bind,source="$(pwd)",target="$(pwd)" mdmc/mdmc:ci-$BRANCH python3 -m pytest -m "$TESTSET" -s "$(pwd)"/tests/ --cov="$(pwd)"/MDMC --cov-report xml; [ $? -eq 0 ] || docker pull mdmc/mdmc:latest &&  docker run -t --mount type=bind,source="$(pwd)",target="$(pwd)" mdmc/mdmc:latest python3 -m pytest -m "$TESTSET" -s "$(pwd)"/tests/ --cov="$(pwd)"/MDMC --cov-report xml; [ $? -eq 0 ] || exit 1
+  docker pull mdmc/mdmc:ci-$BRANCH && docker run -t --mount type=bind,source="$(pwd)",target="$(pwd)" mdmc/mdmc:ci-$BRANCH python3 -m pytest -m "$TESTSET" -s "$(pwd)"/tests/ --cov="$(pwd)"/MDMC --cov-report xml; [ $? -eq 0 ] \
+  || docker pull mdmc/mdmc:latest &&  docker run -t --mount type=bind,source="$(pwd)",target="$(pwd)" mdmc/mdmc:latest python3 -m pytest -m "$TESTSET" -s "$(pwd)"/tests/ --cov="$(pwd)"/MDMC --cov-report xml; [ $? -eq 0 ] \ 
+  || exit 1
 fi
 docker logout
 exit 0
