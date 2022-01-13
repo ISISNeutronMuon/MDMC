6e94b35b35c6686ae3116613942f39b4ba14a94f
Alex Room <69592136+alexhroom@users.noreply.github.com>
2022-01-13 17:04:21 +0000

    doc testing hotfix so tests on correct image

--- .github/scripts/documentation_testing.sh
+++ .github/scripts/documentation_testing.sh
@@ -7,8 +7,7 @@ if ! git diff remotes/origin/master --name-only -- ./doc | read REPLY && \
 ! git diff remotes/originmaster -- requirements.txt | grep  '+ipython\|+ipykernel' 
 then
   echo; echo "Documentation does not require testing."
-  docker pull mdmc/mdmc:latest
-  docker run -t --mount type=bind,source="$(pwd)",target="$(pwd)" mdmc/mdmc:latest /bin/bash -c  "cd $(pwd) && apt-get update &&  apt-get install pandoc -y && pip3 install sphinx nbsphinx sphinx_rtd_theme docutils==0.16 . && make -d -C $(pwd)/doc html"
+  exit 0
 else
   echo; echo "Documentation requires testing."
   docker pull mdmc/mdmc:ci-$BRANCH && docker run -t --mount type=bind,source="$(pwd)",target="$(pwd)" mdmc/mdmc:latest /bin/bash -c  "cd $(pwd) && apt-get update &&  apt-get install pandoc -y && pip3 install sphinx nbsphinx sphinx_rtd_theme docutils==0.16 . && make -d -C $(pwd)/doc html" && exit 0 || \
