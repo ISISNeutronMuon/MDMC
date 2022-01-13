fd2d39fc9e5c8f5d5da5afe2dbfafee3dae8e30e
Alex Room <69592136+alexhroom@users.noreply.github.com>
2022-01-13 17:03:56 +0000

    doc testing hotfix so it tests on correct image

--- .github/scripts/documentation_testing.sh
+++ .github/scripts/documentation_testing.sh
@@ -2,20 +2,15 @@
 
 # build the documentation, which includes testing that the code in the tutorials runs
 
-# tests if there is a change to doc but not to ipython
-if git diff remotes/origin/master --name-only -- ./doc | read REPLY && \
+# tests if there is a change to doc or ipython
+if ! git diff remotes/origin/master --name-only -- ./doc | read REPLY && \
 ! git diff remotes/originmaster -- requirements.txt | grep  '+ipython\|+ipykernel' 
 then
-  echo; echo "Documentation requires testing."
+  echo; echo "Documentation does not require testing."
   docker pull mdmc/mdmc:latest
   docker run -t --mount type=bind,source="$(pwd)",target="$(pwd)" mdmc/mdmc:latest /bin/bash -c  "cd $(pwd) && apt-get update &&  apt-get install pandoc -y && pip3 install sphinx nbsphinx sphinx_rtd_theme docutils==0.16 . && make -d -C $(pwd)/doc html"
-# tests if there is a change to ipython which affects jupyter
-elif git diff remotes/origin/master -- requirements.txt | grep  '+ipython\|+ipykernel'
-then
-  echo; echo "Documentation requires testing on new image"
-  docker pull mdmc/mdmc:ci-$BRANCH
-  docker run -t --mount type=bind,source="$(pwd)",target="$(pwd)" mdmc/mdmc:ci-$BRANCH /bin/bash -c  "cd $(pwd) && apt-get update &&  apt-get install pandoc -y && pip3 install sphinx nbsphinx sphinx_rtd_theme docutils==0.16 . && make -d -C $(pwd)/doc html"
 else
-  echo; echo "Documentation does not require testing."
-  exit 0;
+  echo; echo "Documentation requires testing."
+  docker pull mdmc/mdmc:ci-$BRANCH && docker run -t --mount type=bind,source="$(pwd)",target="$(pwd)" mdmc/mdmc:latest /bin/bash -c  "cd $(pwd) && apt-get update &&  apt-get install pandoc -y && pip3 install sphinx nbsphinx sphinx_rtd_theme docutils==0.16 . && make -d -C $(pwd)/doc html" && exit 0 || \
+  docker pull mdmc/mdmc:latest && docker run -t --mount type=bind,source="$(pwd)",target="$(pwd)" mdmc/mdmc:latest /bin/bash -c  "cd $(pwd) && apt-get update &&  apt-get install pandoc -y && pip3 install sphinx nbsphinx sphinx_rtd_theme docutils==0.16 . && make -d -C $(pwd)/doc html" && exit 0 || exit 1
 fi
