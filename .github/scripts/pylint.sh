aa2f522e73bf1ed8accf1a8e69aabd277f481d99
alexhroom <alex.room@btinternet.com>
2022-02-15 13:37:05 +0000

    Fixed result printing

--- .github/scripts/pylint.sh
+++ .github/scripts/pylint.sh
@@ -3,7 +3,7 @@
 # This script lints a pull request, prints new linting notes that
 # aren't in master, and then passes or fails based on score.
 
-pylint MDMC > pylint.txt
+pylint MDMC > pylint_pr.txt
 # get differences report and score for both reports
 head -n -2 pylint_cron.txt > pylint_cron_notes.txt
 export CRON_SCORE=$(tail -n -2 pylint_cron.txt | sed -n 's/^Your code has been rated at \([-0-9.]*\)\/.*/\1/p')
@@ -21,5 +21,5 @@ echo "Master pylint score:"; echo "$CRON_SCORE";
 awk 'BEGIN {
     pr_score = ENVIRON["PR_SCORE"]
     cron_score = ENVIRON["CRON_SCORE"]
-    exit pr_score < cron_score}' || echo "Your score is lower than master - linting failed." && exit 1
-echo "Your score is higher than (or equal to) master - linting passed!" && exit 0
+    exit pr_score < cron_score}' && echo "Your score is higher than (or equal to) master - linting passed!" && exit 0 || echo "Your score is lower than master - linting failed." && exit 1
+
