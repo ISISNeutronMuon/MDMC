080a688f70b92d14d899bfb2bb9a14cbbb11372d
alexhroom <alex.room@btinternet.com>
2022-02-15 12:29:38 +0000

    Minor fixes to scripts

--- .github/scripts/pylint.sh
+++ .github/scripts/pylint.sh
@@ -12,14 +12,14 @@ head -n -2 pylint_pr.txt > pylint_pr_notes.txt
 export PR_SCORE=$(tail -n -2 pylint_pr.txt | sed -n 's/^Your code has been rated at \([-0-9.]*\)\/.*/\1/p')
 
 # print report if any differences
-echo "Differences between your linting and master:"
+echo "Linting issues in this PR's new code";
 diff pylint_cron_notes.txt pylint_pr_notes.txt
 
-# calculate if score is lower than master
-echo "Your pylint score:"; echo "$PR_SCORE"
-echo "Master score:"; echo "$CRON_SCORE"
+echo "This PR's pylint score:"; echo "$PR_SCORE";
+echo "Master pylint score:"; echo "$CRON_SCORE";
+# calculate if score is lower than master and exit accordingly
 awk 'BEGIN {
     pr_score = ENVIRON["PR_SCORE"]
     cron_score = ENVIRON["CRON_SCORE"]
     exit pr_score < cron_score}' || echo "Your score is lower than master - linting failed." && exit 1
-echo "Your score is higher than master - linting passed!" && exit 0
+echo "Your score is higher than (or equal to) master - linting passed!" && exit 0
