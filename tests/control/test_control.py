82fa846b560e58fbaa36612cd4bc94dbbb9c7a71
alexhroom <alex.room@btinternet.com>
2022-02-18 11:27:56 +0000

    Fixed control tests

--- tests/control/test_control.py
+++ tests/control/test_control.py
@@ -8,7 +8,6 @@ import re
 from typing import List
 
 from MDMC.control import control
-from MDMC.control.control import _is_data_uniform, _make_data_uniform
 from MDMC.trajectory_analysis.observables.sqw import SQw
 from MDMC.trajectory_analysis.observables.pdf import PairDistributionFunction
 from MDMC.MD.parameters import Parameter
@@ -603,7 +602,7 @@ def test_control_is_data_uniform(mock_observable):
     expected = mock_observable['exp']
     # create Control object without instantiating it to test one of its methods
     cont = control.Control
-    observed = _is_data_uniform(mock_observable['obs'])
+    observed = cont._is_data_uniform(mock_observable['obs'])
     assert expected == observed
 
 @pytest.mark.parametrize('mock_observable',
@@ -616,7 +615,7 @@ def test_control_make_data_uniform(mock_observable):
     expected = mock_observable['exp']
     # create Control object without instantiating it to test one of its methods
     cont = control.Control.__new__(control.Control)
-    observed = _make_data_uniform(mock_observable['obs'])
+    observed = cont._make_data_uniform(mock_observable['obs'])
     for var_key in observed.independent_variables:
         assert np.allclose(expected.independent_variables[var_key], observed.independent_variables[var_key], atol=1e-5)
     for var_key in observed.dependent_variables:
