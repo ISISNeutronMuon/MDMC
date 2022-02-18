99a6ae6cff1f258b3a89e1f756419c6eb1e2335a
alexhroom <alex.room@btinternet.com>
2022-02-18 11:42:32 +0000

    Reduced code duplication by adding SQwReader subclass

--- MDMC/readers/observables/MantidSQw.py
+++ MDMC/readers/observables/MantidSQw.py
@@ -2,12 +2,10 @@
 
 import numpy as np
 
-from MDMC.common import units
-from MDMC.common.decorators import unit_decorator
-from MDMC.readers.observables.obs_reader import ObservableReader
+from MDMC.readers.observables.obs_reader import SQwReader
 
 
-class MantidSQw(ObservableReader):
+class MantidSQw(SQwReader):
 
     """
     A class for reading SQw files from Mantid
@@ -66,88 +64,6 @@ class MantidSQw(ObservableReader):
         # Mantid sets errors to 0 if the corresponding datum is 0.  Change these to
         # inf so that error calculations can still be performed on them.
         self.SQw_err[np.where(self.SQw_err <= 0.)] = float('inf')
-
-    @property
-    def independent_variables(self):
-
-        """
-        Get the independent variables, Q (in ``Ang^-1``) and E (``meV``)
-
-        Returns
-        -------
-        dict
-            The independent variables Q and E
-        """
-
-        return {"Q":self.Q, "E":self.E}
-
-    @property
-    def dependent_variables(self):
-
-        """
-        Get the dependent variables, SQw (in ``arb``)
-
-        Returns
-        -------
-        dict
-            The dependent variables, SQw (in ``arb``)
-        """
-
-        return {"SQw": [self.SQw]}
-
-    @property
-    def errors(self):
-
-        """
-        Get the errors on the dependent variables
-
-        Returns
-        -------
-        dict
-            The error on SQw (in ``arb``)
-        """
-
-        return {"SQw": [self.SQw_err]}
-
-    @property
-    def E(self):
-
-        """
-        Get or set the energy transfer, E, in meV
-
-        Returns
-        -------
-        numpy.ndarray
-            Energy transfer, E, in ``meV``
-        """
-
-        return self._E
-
-    @E.setter
-    @unit_decorator(unit=units.ENERGY_TRANSFER)
-    def E(self, value):
-
-        self._E = value
-
-    @property
-    def Q(self):
-
-        """
-        Get or set the momentum transfer, Q, in ``Ang^-1``
-
-        Returns
-        -------
-        numpy.ndarray
-            Momentum transfer, Q, in ``Ang^-1``
-        """
-
-        return self._Q
-
-    @Q.setter
-    @unit_decorator(unit=units.LENGTH ** -1)
-    def Q(self, value):
-
-        self._Q = value
 
     def parse_variables(self, file):
 
