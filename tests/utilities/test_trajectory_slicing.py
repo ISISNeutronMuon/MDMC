2ac8c7264b483a15382902e4a16d1132d15c2b03
Franz Lang <franz.lang@stfc.ac.uk>
2022-03-03 11:26:05 +0000

    parametrize tests using pytest

--- tests/utilities/test_trajectory_slicing.py
+++ tests/utilities/test_trajectory_slicing.py
@@ -1,23 +1,24 @@
 """Tests the slice_trajectory method
 """
 import numpy as np
+import pytest
 
 from MDMC.utilities.trajectory_slicing import slice_trajectory
 from tests.system_tests.observables.data_manager import trajectory
 
-def test_slice_trajectory(trajectory):
-    # the loaded trajectory is a pytest fixture and as such cannot be used in pytest's
-    # parametrize method, so we have to parametrize the tests in a dumber way
-    trj = trajectory
-    tests_to_run = ((25, False, [trj[0:24], trj[25:49]]),
-                    (24, False, [trj[2:25], trj[26:49]]),
-                    (26, False, [trj[24:49]]),
-                    (48, True, [trj[0:47], trj[1:48], trj[2:49]]))
-    for subtrj_len, cont_slicing, expected_slices in tests_to_run:
-        actual_slices = slice_trajectory(trj, subtrj_len, cont_slicing)
-        for i, slice in enumerate(actual_slices):
-            assert np.all(expected_slices[i].times == slice.times)
-            assert np.all((expected_slices[i].positions == slice.positions))
-            assert np.all((expected_slices[i].velocities == slice.velocities))
-            assert np.all((expected_slices[i].data == slice.data))
 
+@pytest.mark.parametrize("subtrj_len, cont_slicing, expected_ranges",
+             [(25, False, [(0, 24), (25, 49)]),
+              (24, False, [(2, 25), (26, 49)]),
+              (26, False, [(24, 49)]),
+              (48, True, [(0, 47), (1, 48), (2, 49)])])
+def test_slice_trajectory(subtrj_len, cont_slicing, expected_ranges, trajectory):
+    actual_slices = slice_trajectory(trajectory, subtrj_len, cont_slicing)
+    expected_slices = []
+    for range in expected_ranges:
+        expected_slices.append(trajectory[range[0]:range[1]])
+    for i, slice in enumerate(actual_slices):
+        assert np.all(expected_slices[i].times == slice.times)
+        assert np.all((expected_slices[i].positions == slice.positions))
+        assert np.all((expected_slices[i].velocities == slice.velocities))
+        assert np.all((expected_slices[i].data == slice.data))
