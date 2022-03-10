86ca1c24b0200cc4708826b13f17da365e4e9e81
alexhroom <alex.room@btinternet.com>
2022-03-10 10:18:05 +0000

    Changed documentation packages to be contained in the Docker image

--- .github/scripts/documentation_testing.sh
+++ .github/scripts/documentation_testing.sh
@@ -10,6 +10,6 @@ then
   exit 0
 else
   echo; echo "Documentation requires testing."
-  docker pull mdmc/mdmc:ci-$BRANCH && docker run -t --mount type=bind,source="$(pwd)",target="$(pwd)" mdmc/mdmc:latest /bin/bash -c  "cd $(pwd) && apt-get update &&  apt-get install pandoc -y && pip3 install sphinx nbsphinx sphinx_rtd_theme docutils==0.16 . && make -d -C $(pwd)/doc html" && exit 0 || \
-  docker pull mdmc/mdmc:latest && docker run -t --mount type=bind,source="$(pwd)",target="$(pwd)" mdmc/mdmc:latest /bin/bash -c  "cd $(pwd) && apt-get update &&  apt-get install pandoc -y && pip3 install sphinx nbsphinx sphinx_rtd_theme docutils==0.16 . && make -d -C $(pwd)/doc html" && exit 0 || exit 1
+  docker pull mdmc/mdmc:ci-$BRANCH && docker run -t --mount type=bind,source="$(pwd)",target="$(pwd)" mdmc/mdmc:ci-$BRANCH /bin/bash -c  "cd $(pwd) && apt-get update && apt-get install pandoc -y && make -d -C $(pwd)/doc html" && exit 0 || \
+  docker pull mdmc/mdmc:latest && docker run -t --mount type=bind,source="$(pwd)",target="$(pwd)" mdmc/mdmc:latest /bin/bash -c  "cd $(pwd) && apt-get update && apt-get install pandoc -y && make -d -C $(pwd)/doc html" && exit 0 || exit 1
 fi
