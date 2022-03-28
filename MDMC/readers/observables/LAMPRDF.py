b1ad66b233db7ed5f24e73d59e3b01a8e82067b5
iermi <52323221+iermi@users.noreply.github.com>
2022-03-28 14:08:17 +0200

    Changed LAMPRDF.py
    Most of requested changes have been done. The code was also tested on some RDF-files and could reproduce the data successfully with the completed changes

--- MDMC/readers/observables/LAMPRDF.py
+++ MDMC/readers/observables/LAMPRDF.py
@@ -21,8 +21,6 @@ class LAMPRDF(ObservableReader):
         File containing the output from the calculation
     """
 
-
-
     def parse(self,**settings):
 
         """
@@ -32,109 +30,111 @@ class LAMPRDF(ObservableReader):
         """
 
         self.r = self.parse_indep_var(self.r)
-        self.rdf = self.parse_dep_var(self.rdff)
-
+        self.PDF = self.parse_dep_var(self.PDF)
+        self.PDF_err = np.zeros(self.PDF_err)
 
     @property
-    def Dist(self):
+    def r(self):
 
         """
-        Get or set the distance between pairs, Dist, in ``nm``
-
+        Get or set the distance between pairs, r, in ``Å``
         Returns
         -------
         numpy.ndarray
-            Distance between pairs, Dist, in ``nm``
+            Distance between pairs, r, in ``Å``
         """
 
-        return self._Dist
+        return self._r
 
-    @Dist.setter
+    @r.setter
     @unit_decorator(unit=units.LENGTH)
-    def Dist(self, value):
+    def r(self, value):
 
-        self._Dist = value
+        self._r = value
 
     @property
-    def rdf(self):
+    def PDF(self):
 
         """
         Get or set the RDF between pairs, in ``barn``
-
         Returns
         -------
         numpy.ndarray
             RDF between pairs, RDF, in ``barn``
         """
 
-        return self._rdf
+        return self._PDF
 
-    @rdf.setter
+    @PDF.setter
     @unit_decorator(unit=units.Unit('barn'))
-    def rdf(self, value):
+    def PDF(self, value):
         """ 
         Get or set the value of the radial distribution function (in ``barn``)
         """
 
-        self._rdf = value
+        self._PDF = value
+
+    @property
+    def errors(self):
+
+        """
+        Get the errors on the dependent variables
+
+        Returns
+        -------
+        dict
+            The error on SQw (in ``arb``)
+        """
+
+        return {"PDF": [self.PDF_err]}
 
 
     @property
     def independent_variables(self):
 
         """
-        Get the independent variable, Dist (in ``nm``)
-
+        Get the independent variable, r (in ``Å``)
         Returns
         -------
         dict
-            The independent variables Dist
+            The independent variables r
         """
 
-        return {"Dist":self.Dist}
+        return {"r":self.r}
 
     @property
     def dependent_variables(self):
 
         """
         Get the dependent variables, rdf (in ``barn``)
-
         Returns
         -------
         dict
             The dependent variables, rdf (in ``barn``)
         """
 
-        return {"rdf": [self.rdf]}
+        return {"PDF": [self.PDF]}
 
     def parse_indep_var(self, ffile):
 
         """
         Parses the independent variables
-
         Splits the file so that the data can be extracted into a ``array`` 
-   
-        Since the the output file contains both the independent and dependent variables,
-        
-        the parsing procedure for the same file is separated into two steps.
-     
-        The file is basically the same for both dependent and independent variables, 
-        
-        but the procedure is separated into 2 steps for the clarity.
-
+        Since the the output file contains both the independent and dependent variables,        
+        the parsing procedure for the same file is separated into two steps.     
+        The file is basically the same for both dependent and independent variables,         
+        but the procedure is separated into 2 steps for clarity.
         Parameters
         ----------
-        ffile : file
+        file : file
             Open file containing independent data
-            Generally, the file is the same fo rboth dependent and independent parameters
-
+            Generally, the file is the same for both dependent and independent parameters
         Returns
         -------
         array
             r -  distance
         """
         ffile1=open(ffile, 'r')  
-        k=0
         for i, line in enumerate(ffile1):
            if i==3:
              line=line.strip()
@@ -149,28 +149,24 @@ class LAMPRDF(ObservableReader):
              column=line.split()
              # c_number or the column[0] represents the counter for the number of rows,
              # this counter is not interesting for the data analysis.
-             c_number[k]=int(column[0])
-             # distance[k] or column[1] is the distance for which the rdfs were calculated.
-             distance[k]=float(column[1])
-           k=i-3
-        self.dist=distance
+             c_number[i-4]=int(column[0])
+             # distance[i-4] or column[1] is the distance for which the rdfs were calculated.
+             distance[i-4]=float(column[1])
+        self.r=distance
         ffile1.close()
-        return self.dist
+        return self.r
 
     def parse_dep_var(self, numrdf, ffile):
 
         """
         Parses the dependent variables (radial distribution functions)
-
-        Since the the output file contains both the independent and dependent variables,
-        
+        Since the the output file contains both the independent and dependent variables,        
         the parsing procedure for the same file is separated into two steps.
-
+        numrdf is the number of radial distribution functions in the file
         Parameters
         ----------
         file : file
             Open file containing independent data
-
         Returns
         -------
         numpy.ndarray
@@ -178,7 +174,6 @@ class LAMPRDF(ObservableReader):
         """
 
         # Give the number of radial distribution functions
-        num_rdf=numrdf
         ffile2=open(ffile,'r')
         for i, line in enumerate(ffile2):
         # The 3rd line is interesting since it is containing the important information for the initialization
@@ -189,36 +184,35 @@ class LAMPRDF(ObservableReader):
              column=line.split()
              time_step=int(column[0])
              num_rows=int(column[1]) 
-             NR=num_rows
         # Closing the file after obtaining the right number for the initialization of arrays.
         ffile2.close()
-        ar=(NR, num_rdf) 
-        # Initialization of the arrays by allocating the space and setting all numbers to zero.                         
-        rdf_ar=np.zeros(ar)
+        # Initialization of the arrays by allocating the space and setting all numbers to zero. 
+        #ar= (num_rows,numrdf)                       
+        rdf_ar=np.zeros((num_rows,numrdf))
+        rdf_err=np.zeros((num_rows,numrdf))
         c_number=np.zeros(num_rows)
         distance=np.zeros(num_rows) 
 
         ffile2=open(ffile, 'r')
-        k=0
         for i, line in enumerate(ffile2):
            # First 2 lines are comments on what rdfs were computed
            # The 3rd line contains the time-step which is not important for the current calculation
            # and the number of rows for rdf which is very important for the definition of the size of the array.
-           # The parameter is called as NR=num_rows
            if i>3:
              line=line.strip()
              column=line.split()
            # These numbers won't be used for the reader here. They kept for the clarity of the column count,
            # since the file is the same.
              c_number=int(column[0])
-             distance[k]=float(column[1])
+             distance[i-4]=float(column[1])
              l=0
          # Since the actual rdfs are starting from the 3rd column, labeled as number 2 in python,
          # and the counter starts from zero, we take that column as a column for rdf, which will be counted as l+2.
-             for l in range(num_rdf):
-               rdf_ar[k][l]=float(column[l+2])
-           k=i-3 
+             for l in range(numrdf):
+               rdf_ar[i-4][l]=float(column[l+2])      
         # RDF then will be an array with the size (number of rows x number of rdfs) 
-        self.rdf=rdf_ar
+        self.PDF=rdf_ar
+        self.PDF_err=rdf_err
         ffile2.close()
-        return self.rdf
+        return self.PDF
+
