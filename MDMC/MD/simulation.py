Moved all unit adders to setters rather than getters, also fixed most unintended unit conversion bugs

MDMC/MD/simulation.py
@@ -170,7 +170,7 @@ class Universe(AtomContainer):
             The dimensions of the ``Universe``
         """
 
-        return self._dimensions * UREG.angstrom
+        return self._dimensions
 
     @dimensions.setter
     def dimensions(self, dimensions):
@@ -183,7 +183,7 @@ class Universe(AtomContainer):
                              dimensions,
                              msg)
                 raise ValueError(msg)
-            self._dimensions = np.array([dimensions] * 3)
+            self._dimensions = np.array([dimensions] * 3) * UREG.angstrom
         elif isinstance(dimensions, (list, tuple, np.ndarray)):
             if len(dimensions) == 3:
                 if any(dim <= 0 for dim in np.array(dimensions)):
@@ -194,7 +194,7 @@ class Universe(AtomContainer):
                                  dimensions,
                                  msg)
                     raise ValueError(msg)
-                self._dimensions = np.array(dimensions)
+                self._dimensions = np.array(dimensions) * UREG.angstrom
             else:
                 msg = '3 dimensions must be specified'
                 LOGGER.error('%s: {dimensions: %s} %s',
@@ -338,7 +338,7 @@ class Universe(AtomContainer):
             Volume in ``Ang^3``
         """
 
-        return np.prod(self.dimensions) * (UREG.angstrom ** 3)
+        return np.prod(self.dimensions)
 
     @property
     def element_list(self):
@@ -509,8 +509,7 @@ class Universe(AtomContainer):
             The mass density of all of the atoms of the ``Universe`` in amu / Ang^3
         """
 
-        return (np.sum([atom.mass for atom in self.atoms]) / self.volume) \
-               * (UREG.amu / UREG.angstrom ** 3)
+        return np.sum([atom.mass for atom in self.atoms]) / self.volume
 
     @property
     def solvent_density(self):
@@ -1330,11 +1329,11 @@ class Simulation:
             Simulation time step in ``fs``
         """
 
-        return self._time_step * UREG.fs
+        return self._time_step
 
     @time_step.setter
     def time_step(self, value):
-        self._time_step = value
+        self._time_step = value * UREG.fs
 
     @property
     def traj_step(self):

MDMC/MD/solvents/_solvent_config.py
@@ -79,12 +79,12 @@ class SolventConfig(ABC):
             The dimensions of the SolventConfig box in Ang
         """
 
-        return self._box_dimensions * UREG.angstrom
+        return self._box_dimensions
 
     @box_dimensions.setter
     def box_dimensions(self, value):
 
-        self._box_dimensions = value
+        self._box_dimensions = value * UREG.angstrom
 
     @property
     def volume(self):
@@ -97,7 +97,7 @@ class SolventConfig(ABC):
             The volume of the SolventConfig in Ang^3
         """
 
-        return np.prod(self.box_dimensions) * (UREG.angstrom ** 3)
+        return np.prod(self.box_dimensions)
 
     @property
     def atom_types(self):
@@ -242,7 +242,7 @@ class SolventConfig(ABC):
             The mass density of the SolventConfig in amu / Ang^3
         """
 
-        return (self.mass * self.n_molecules / self.volume) * (UREG.amu / UREG.angstrom ** 3)
+        return (self.mass * self.n_molecules / self.volume)
 
     def reset_molecules(self):
         """

MDMC/MD/structural_units.py
@@ -90,12 +90,17 @@ class StructuralUnit(ABC):
         numpy.ndarray
         """
 
-        return self._position * UREG.angstrom
+        return self._position
 
     @position.setter
     def position(self, position):
-
-        self._position = position
+        # due to molecule construction methods,
+        # sometimes the molecules have units and sometimes they do not: check for this
+        # else we will sometimes give a molecule the units Angstrom ** 2
+        if isinstance(position, pint.Quantity):
+            self._position = position
+        else:
+            self._position = position * UREG.angstrom
 
     @property
     def velocity(self):
@@ -108,12 +113,12 @@ class StructuralUnit(ABC):
         numpy.ndarray
         """
 
-        return self._velocity * (UREG.angstrom / UREG.fs)
+        return self._velocity
 
     @velocity.setter
     def velocity(self, velocity):
 
-        self._velocity = velocity
+        self._velocity = velocity * (UREG.angstrom / UREG.fs)
 
     @property
     def atoms(self):
@@ -951,12 +956,12 @@ class Atom(StructuralUnit):
         float
             the atomic mass in ``amu``
         """
-        return self._mass * UREG.amu
+        return self._mass
 
     @mass.setter
     def mass(self, mass):
 
-        self._mass = mass
+        self._mass = mass * UREG.amu
 
     @property
     def atom_type(self):
@@ -1232,7 +1237,7 @@ class Molecule(CompositeStructuralUnit):
         numpy.ndarray
         """
 
-        return self._position * UREG.angstrom
+        return self._position
 
     @position.setter
     def position(self, position):
@@ -1308,7 +1313,7 @@ class Molecule(CompositeStructuralUnit):
         for atom in self.atoms:
             mass += atom.mass
 
-        return mass * UREG.amu
+        return mass
 
 
 @repr_decorator('min', 'max', 'volume')
@@ -1349,12 +1354,12 @@ class BoundingBox:
             The minimum extent in ``Ang``
         """
 
-        return self._min * UREG.angstrom
+        return self._min
 
     @min.setter
     def min(self, value):
 
-        self._min = value
+        self._min = value * UREG.angstrom
 
     @property
     def max(self):
@@ -1368,12 +1373,12 @@ class BoundingBox:
             The maximum extent in ``Ang``
         """
 
-        return self._max * UREG.angstrom
+        return self._max
 
     @max.setter
     def max(self, value):
 
-        self._max = value
+        self._max = value * UREG.angstrom
 
     @property
     def volume(self):
@@ -1387,7 +1392,7 @@ class BoundingBox:
             The volume of the bounding box
         """
 
-        return abs(np.prod(self.max - self.min)) * (UREG.angstrom ** 3)
+        return abs(np.prod(self.max - self.min))
 
 
 def filter_atoms(atoms, predicate):

MDMC/trajectory_analysis/observables/sqw.py
@@ -105,14 +105,14 @@ class SQwMixins:
             1D array of Q `float` (in ``Ang^-1``)
         """
         try:
-            return self.independent_variables['Q'] * (UREG.angstrom ** -1)
+            return self.independent_variables['Q']
         except KeyError:
             return None
 
     @Q.setter
     def Q(self, value):
 
-        self.independent_variables['Q'] = value
+        self.independent_variables['Q'] = value * (UREG.angstrom ** -1)
 
 
 class AbstractSQw(SQwMixins, Observable):
@@ -199,7 +199,7 @@ class AbstractSQw(SQwMixins, Observable):
 
         if self.independent_variables:
             try:
-                return self.independent_variables['E'] * UREG.meV
+                return self.independent_variables['E']
             except KeyError:
                 pass
         return None
@@ -390,9 +390,9 @@ class AbstractSQw(SQwMixins, Observable):
         if self.E is not None:
             self.validate_energy(dt)
         elif self.independent_variables:
-            self.independent_variables['E'] = self.calculate_E(len(t) - 1, dt)
+            self.independent_variables['E'] = self.calculate_E(len(t) - 1, dt) * UREG.meV
         else:
-            self.independent_variables = {'E': self.calculate_E(len(t) - 1, dt)}
+            self.independent_variables = {'E': self.calculate_E(len(t) - 1, dt) * UREG.meV}
         # Overwrite independent variable 'Q' if it already exists
         try:
             self.independent_variables['Q'] = np.array(settings['Q_values'])

tests/trajectory_analysis/test_SQw.py
@@ -7,6 +7,7 @@ import numpy as np
 from numpy.testing import assert_allclose
 import pytest
 from typing import Optional
+import pint
 
 from MDMC.common.constants import h
 import MDMC.trajectory_analysis.observables.obs_factory as of
@@ -18,6 +19,8 @@ from tests.test_data import data
 from tests.trajectory_analysis.test_histogram import trajectory
 from tests.MD.test_simulation import water_SPCE_universe, water_molecule, \
     atom, universe
+
+UREG = pint.UnitRegistry()
 
 @pytest.fixture
 def altered_trajectory(water_SPCE_universe):
@@ -99,7 +102,7 @@ def test_from_data(SQw_from_data):
     # Cannot use assert_allclose as our UnitNDArray fails comparison with a
     # normal numpy array
     for i, Q in enumerate(SQw_from_data.independent_variables['Q']):
-        assert np.isclose(Q, i * 0.05)
+        assert np.isclose(Q, (i * 0.05 * (UREG.angstrom ** -1)))
 
 
 def test_from_MD(SQw_from_MD):