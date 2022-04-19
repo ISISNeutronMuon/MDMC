Fixed more units and added incomplete definition file

MDMC/MD/structural_units.py
@@ -17,7 +17,7 @@ import weakref
 import numpy as np
 from scipy.spatial.transform import Rotation
 
-from MDMC.common.unit_registry import UREG, scrub_unit
+from MDMC.common.unit_registry import UREG, strip_unit
 from MDMC.common import atom_properties
 from MDMC.MD.interactions import Coulombic, BondedInteraction
 from MDMC.common.decorators import repr_decorator
@@ -47,9 +47,9 @@ class StructuralUnit(ABC):
     name : str
         The name of the structure.
 
-        Attributes
+    Attributes
     ----------
-        ID : int
+    ID : int
         A unique identifier for each ``StructuralUnit``.
     universe : Universe
         The ``Universe`` to which the ``StructuralUnit`` belongs.
@@ -96,7 +96,7 @@ class StructuralUnit(ABC):
         # due to molecule construction methods,
         # sometimes the molecules have units and sometimes they do not: check for this
         # else we will sometimes give a molecule the units Angstrom ** 2
-        self._position = scrub_unit(position) * UREG.angstrom
+        self._position = strip_unit(position) * UREG.angstrom
 
     @property
     def velocity(self):
@@ -1200,7 +1200,7 @@ class Molecule(CompositeStructuralUnit):
 
     @position.setter
     def position(self, position):
-        self._position = scrub_unit(position) * UREG.angstrom
+        self._position = strip_unit(position) * UREG.angstrom
         self._set_subunit_positions()
 
     @property
@@ -1312,7 +1312,7 @@ class BoundingBox:
 
     @min.setter
     def min(self, value):
-        self._min = scrub_unit(value) * UREG.angstrom
+        self._min = strip_unit(value) * UREG.angstrom
 
     @property
     def max(self):
@@ -1329,7 +1329,7 @@ class BoundingBox:
 
     @max.setter
     def max(self, value):
-        self._max = scrub_unit(value) * UREG.angstrom
+        self._max = strip_unit(value) * UREG.angstrom
 
     @property
     def volume(self):

MDMC/trajectory_analysis/observables/sqw.py
@@ -6,7 +6,7 @@ import numpy as np
 from numpy.testing import assert_allclose
 from scipy.interpolate import interp2d
 
-from MDMC.common.unit_registry import UREG, scrub_unit
+from MDMC.common.unit_registry import UREG, strip_unit
 from MDMC.common.constants import h, h_bar
 from MDMC.resolution.resolution_factory import ResolutionFactory
 from MDMC.trajectory_analysis.observables.obs import Observable
@@ -16,6 +16,7 @@ from MDMC.utilities.trajectory_slicing import slice_trajectory
 
 # pylint: disable=no-member
 # as it creates a false positive for Q magnitude
+
 
 class SQwMixins:
     """
@@ -582,7 +583,7 @@ class AbstractSQw(SQwMixins, Observable):
 
         # In general we do not have equal energy spacing, multiply the exponential factor by this
         # before transposing and dotting to sum over the energy domain
-        exp = np.exp(1j * np.outer(scrub_unit(t_array), scrub_unit(E_sorted)) /
+        exp = np.exp(1j * np.outer(strip_unit(t_array), strip_unit(E_sorted)) /
                      (h_bar.magnitude * 1e18)) * widths
         SQw_ift = np.dot(SQw_sorted, np.transpose(exp))
 