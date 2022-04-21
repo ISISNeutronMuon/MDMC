7acb1e2643c3198c09ccd25f1c9ce1176c1f1808
franzlang <17494620+franzlang@users.noreply.github.com>
2022-04-21 07:30:53 +0100

    Update MDMC/readers/observables/LAMPPDF.py
    clarifying expected file format in lamppdf reader docstring
    
    Co-authored-by: Anders Markvardsen <anders.markvardsen@stfc.ac.uk>

--- MDMC/readers/observables/LAMPPDF.py
+++ MDMC/readers/observables/LAMPPDF.py
@@ -12,8 +12,9 @@ class LAMPPDF(PDFReader):
     LAMP's ascii output uses a single file, with the expected file structure being:
     Row-Number  Distance  pdf-total  pdf1 pdf2  ...  pdfN
 
-    The `pdf_col` parameter can be used to select which data column contains the total PDF. The
-    remaining columns (if existent) are assumed to be partial PDFs.
+    The column file format above is the default with the total PDF data contained in the 3 column. When
+    initialising instances of this class it is possible to change this using the `pdf_col` parameter to select
+    which data column contains the total PDF. The remaining columns (if exist) are assumed to be partial PDFs.
 
     Parameters
     ----------
