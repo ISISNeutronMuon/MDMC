0fb127c785d47c3a6c987f5ca0e76310e7a4ae05
Alex Room <69592136+alexhroom@users.noreply.github.com>
2022-05-11 14:22:08 +0100

    fixed bugs

--- .github/scripts/build_container.sh
+++ .github/scripts/build_container.sh
@@ -6,7 +6,7 @@ echo "$DOCKER_PASSWORD" | docker login -u "mdmc" --password-stdin # this login c
 
 
 # full image rebuild if MD engines have changed
-if git diff remotes/origin/master --name-only | grep 'build/Dockerfile.engines'
+if git diff remotes/origin/master --name-only | grep 'build/Docker/Dockerfile.engines'
 then
   echo "Rebuilding Dockerfile including base."
   docker build -t mdmc/engines:ci-$BRANCH -f "$(pwd)"/build/Docker/Dockerfile.engines . || exit 1
@@ -24,7 +24,7 @@ then
   echo "Docker file does not require rebuilding." 
 else
   echo "Docker file requires rebuilding."
-  docker build -t mdmc/mdmc:ci-$BRANCH -f "$(pwd)"/build/Docker/Dockerfile . || exit 1 
+  docker build -t mdmc/mdmc:ci-$BRANCH -f "$(pwd)"/build/Docker/Dockerfile.mdmc . || exit 1 
   docker push mdmc/mdmc:ci-$BRANCH
 fi
 docker logout
