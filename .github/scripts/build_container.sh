b0fc1bf54eda08c53b7834b5cbce3130695da7d7
alexhroom <alex.room@btinternet.com>
2022-05-11 14:13:30 +0100

    Split Dockerfile into two files and updated scripts to match

--- .github/scripts/build_container.sh
+++ .github/scripts/build_container.sh
@@ -3,6 +3,22 @@
 # this script detects if a new docker container needs building, and if so, builds it.
 
 echo "$DOCKER_PASSWORD" | docker login -u "mdmc" --password-stdin # this login circumvents the Docker IP rate limit for anonymous users
+
+
+# full image rebuild if MD engines have changed
+if git diff remotes/origin/master --name-only | grep 'build/Dockerfile.engines'
+then
+  echo "Rebuilding Dockerfile including base."
+  docker build -t mdmc/engines:ci-$BRANCH -f "$(pwd)"/build/Docker/Dockerfile.engines . || exit 1
+  docker push mdmc/engines:ci-$BRANCH
+  docker build -t mdmc/mdmc:ci-$BRANCH -f "$(pwd)"/build/Docker/Dockerfile.mdmc --build-arg BASE_IMAGE=mdmc/engines:ci-$BRANCH . || exit 1
+  docker push mdmc/mdmc:ci-$BRANCH
+  docker logout
+  exit 0
+fi
+
+
+# mdmc/mdmc image rebuild without base image changes 
 if ! git diff remotes/origin/master --name-only | grep 'build/\|requirements.txt'
 then
   echo "Docker file does not require rebuilding." 
