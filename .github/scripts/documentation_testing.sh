bdb50d710d0482602abe3b05e86bbdebafcc1f44
Alex Room <69592136+alexhroom@users.noreply.github.com>
2022-05-15 15:39:31 +0100

    MDMC now installs properly

--- .github/scripts/documentation_testing.sh
+++ .github/scripts/documentation_testing.sh
@@ -11,6 +11,6 @@ then
   exit 0
 else
   echo; echo "Documentation requires testing."
-  docker pull mdmc/mdmc:ci-$BRANCH && docker run -t --mount type=bind,source="$(pwd)",target="$(pwd)" mdmc/mdmc:ci-$BRANCH /bin/bash -c  "cd $(pwd) && apt-get update && apt-get install pandoc -y && make -d -C $(pwd)/doc html" && exit 0 || \
-  docker pull mdmc/mdmc:latest && docker run -t --mount type=bind,source="$(pwd)",target="$(pwd)" mdmc/mdmc:latest /bin/bash -c  "cd $(pwd) && apt-get update && apt-get install pandoc -y && make -d -C $(pwd)/doc html" && exit 0 || exit 1
+  docker pull mdmc/mdmc:ci-$BRANCH && docker run -t --mount type=bind,source="$(pwd)",target="$(pwd)" mdmc/mdmc:ci-$BRANCH /bin/bash -c  "cd $(pwd) && apt-get update && apt-get install pandoc -y && pip3 install . && make -d -C $(pwd)/doc html" && exit 0 || \
+  docker pull mdmc/mdmc:latest && docker run -t --mount type=bind,source="$(pwd)",target="$(pwd)" mdmc/mdmc:latest /bin/bash -c  "cd $(pwd) && apt-get update && apt-get install pandoc -y && pip3 install . && make -d -C $(pwd)/doc html" && exit 0 || exit 1
 fi
