8d4c4bf8a891dbd703466d809d004ce2ee684599
alexhroom <alex.room@btinternet.com>
2022-05-26 11:53:22 +0100

    Made self-hosted tests always run

--- .github/scripts/docker_testing.sh
+++ .github/scripts/docker_testing.sh
@@ -1,25 +1,10 @@
 #!/bin/bash 
 
-####### This script tests whether the source code needs to be tested
-####### i.e. if there are changes to requirements.txt or the MDMC folder
+# OPTIONS are defined in the main CI script, BRANCH is the git branch
 
-# the workflow of this script is the following:
-# 1. check if the PR branch has a different requirements.txt or MDMC folder to the master branch
-# 2. if it does, test MDMC in mdmc/mdmc:ci-[branch] if it exists or mdmc/mdmc:latest.
-# 3. if it doesn't, exit.
-
-# $TESTSET is defined in the main CI .yml script
-
-echo "$DOCKER_PASSWORD" | docker login -u "mdmc" --password-stdin # this login circumvents the Docker IP rate limit for anonymous users
-if ! git diff remotes/origin/master --name-only | grep 'MDMC/\|requirements.txt'
-then
-  echo "Source code does not require testing."
-  exit 0 
-else
-  echo "Source code requires testing."
-  # read this run block as 'attempt to pull the built docker container and test on it; if it fails (i.e. no container was built) then just test on latest instead'.
-  docker pull mdmc/mdmc:ci-$BRANCH && docker run -t --mount type=bind,source="$(pwd)",target="$(pwd)" mdmc/mdmc:ci-$BRANCH python3 -m pytest "$OPTIONS"; [ $? -eq 0 ] && exit 0 || \
-  docker pull mdmc/mdmc:latest &&  docker run -t --mount type=bind,source="$(pwd)",target="$(pwd)" mdmc/mdmc:latest python3 -m pytest "$OPTIONS"; [ $? -eq 0 ] && exit 0 || exit 1
-fi
+echo "$DOCKER_PASSWORD" | docker login -u "mdmc" --password-stdin # this login 
+# read this run block as 'attempt to pull the built docker container and test on it; if it fails (i.e. no container was built) then just test on latest instead'.
+docker pull mdmc/mdmc:ci-$BRANCH && docker run -t --mount type=bind,source="$(pwd)",target="$(pwd)" mdmc/mdmc:ci-$BRANCH python3 -m pytest "$OPTIONS"; [ $? -eq 0 ] && exit 0 || \
+docker pull mdmc/mdmc:latest &&  docker run -t --mount type=bind,source="$(pwd)",target="$(pwd)" mdmc/mdmc:latest python3 -m pytest "$OPTIONS"; [ $? -eq 0 ] && exit 0 || exit 1
 docker logout
 exit 0
