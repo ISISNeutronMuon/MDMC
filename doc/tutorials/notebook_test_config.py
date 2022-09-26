3f2d23d5b0b0413a3bf7fd4595860ef2f9b334fd
Maks Sekula <maksymilian.sekula@hotmail.com>
2022-09-26 13:20:21 +0100

    Fix issue in config file

--- doc/tutorials/notebook_test_config.py
+++ doc/tutorials/notebook_test_config.py
@@ -1,15 +1,14 @@
 c = get_config()
-c.NbConvertApp.notebooks = [
-    "applying-a-forcefield.ipynb",
-    "Argon-a-to-z.ipynb"
-    "building-a-universe.ipynb"
-    "creating-an-observable.ipynb"
-    "running-a-refinement.ipynb"
-    "running-a-simulation.ipynb"
-    "selecting-fitting-parameters.ipynb"
-    "solvating-a-universe.ipynb"
-    "units.ipynb"
-]
-
 c.ExecutePreprocessor.enabled = True
 c.NbConvertApp.export_format = "notebook"
+c.NbConvertApp.notebooks = [
+    "applying-a-forcefield.ipynb",
+    "Argon-a-to-z.ipynb",
+    "building-a-universe.ipynb",
+    "creating-an-observable.ipynb",
+    "running-a-refinement.ipynb",
+    "running-a-simulation.ipynb",
+    "selecting-fitting-parameters.ipynb",
+    "solvating-a-universe.ipynb",
+    "units.ipynb"
+]
