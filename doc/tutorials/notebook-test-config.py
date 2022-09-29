da7879d34e605eb7ff3d36df6efa40b616ff5b07
maks <maksymilian.sekula@hotmail.com>
2022-09-29 17:06:46 +0100

    Modify paths in config file

--- doc/tutorials/notebook-test-config.py
+++ doc/tutorials/notebook-test-config.py
@@ -1,14 +1,14 @@
 c = get_config()
 c.NbConvertApp.export_format = "notebook"
 c.NbConvertApp.notebooks = [
-    "MDMC/doc/tutorials/applying-a-forcefield.ipynb",
-    "MDMC/doc/tutorials/Argon-a-to-z.ipynb",
-    "MDMC/doc/tutorials/building-a-universe.ipynb",
-    "MDMC/doc/tutorials/creating-an-observable.ipynb",
-    "MDMC/doc/tutorials/running-a-refinement.ipynb",
-    "MDMC/doc/tutorials/running-a-simulation.ipynb",
-    "MDMC/doc/tutorials/selecting-fitting-parameters.ipynb",
-    "MDMC/doc/tutorials/solvating-a-universe.ipynb",
-    "MDMC/doc/tutorials/units.ipynb"
+    "doc/tutorials/applying-a-forcefield.ipynb",
+    "doc/tutorials/Argon-a-to-z.ipynb",
+    "doc/tutorials/building-a-universe.ipynb",
+    "doc/tutorials/creating-an-observable.ipynb",
+    "doc/tutorials/running-a-refinement.ipynb",
+    "doc/tutorials/running-a-simulation.ipynb",
+    "doc/tutorials/selecting-fitting-parameters.ipynb",
+    "doc/tutorials/solvating-a-universe.ipynb",
+    "doc/tutorials/units.ipynb"
 ]
 c.ExecutePreprocessor.enabled = True
