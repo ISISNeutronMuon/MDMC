78f3aef912af40f24dd9335e51904f7c2dd7d25d
jfkcooper <jos.cooper@stfc.ac.uk>
2022-10-18 15:35:51 +0100

    print formatting

--- MDMC/control/control.py
+++ MDMC/control/control.py
@@ -593,8 +593,9 @@ class Control:
             _, _, y_random, coords = \
             self._expected_minimum_random_sampling(self.minimizer.optimizer, n_random_starts=points)
         except IndexError:
-            print("\n \n Your model has not been run for enough iterations to make a reasonable ")
-            print("guess at the best figure of merit. Please run for at least 20 steps. \n")
+            msg = ("\n \n Your model has not been run for enough iterations to make a reasonable "
+                   "guess at the best figure of merit. Please run for at least 20 steps. \n")
+            print(msg)
 
             return None
 
