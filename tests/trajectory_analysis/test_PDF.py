d3a3d5941379519a3057166f87b54ece4191b8ba
Maciej Bartkowiak <108934199+MBartkowiakSTFC@users.noreply.github.com>
2022-11-10 08:46:17 +0000

    Correct the element_set in test_PDF.py

--- tests/trajectory_analysis/test_PDF.py
+++ tests/trajectory_analysis/test_PDF.py
@@ -64,7 +64,7 @@ def trajectory(universe):
 
     trj = configurations_as_compact_trajectory(*[TemporalConfiguration(time, universe=universe)
                        for time in range(0, 1000, 1)])
-    # trj.configurations[0].element_set = ELEMENTS
+    trj.element_set = ELEMENTS  # it was already done this way for the old Trajectory
     return trj
 
 
