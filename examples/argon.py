3d75652946a4928927dabf0fb08219193d6a961c
Maciej Bartkowiak <108934199+MBartkowiakSTFC@users.noreply.github.com>
2022-11-10 16:23:01 +0000

    Remove unnecessary changes

--- examples/argon.py
+++ examples/argon.py
@@ -71,4 +71,4 @@ control = Control(simulation=simulation,
 
 # Run the refinement, i.e. refine the FF parameters against the data.
 # n_steps = 3 is too small, but a good choice to first test this script
-control.refine(n_steps=30)
+control.refine(n_steps=3)
