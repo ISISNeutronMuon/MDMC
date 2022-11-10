17963c5efe626a5ded762315b660e348b9e8aee0
jfkcooper <jos.cooper@stfc.ac.uk>
2022-11-10 16:05:28 +0000

    simulation module silenced for tests and some test refectoring started

--- MDMC/MD/simulation.py
+++ MDMC/MD/simulation.py
@@ -67,6 +67,8 @@ class Universe(AtomContainer):
         ``constraint_algorithm`` (`ConstraintAlgorithm`)
             The constraint algorithm which will be applied to constrained
             ``BondedInteractions``.
+        ``verbose`` (`str`)
+            If the output of the class instantiation should be reported, default to True.
 
     Attributes
     ----------
@@ -134,6 +136,8 @@ class Universe(AtomContainer):
         self.kspace_solver = settings.get('kspace_solver')
         self.electrostatic_solver = settings.get('electrostatic_solver')
         self.dispersive_solver = settings.get('dispersive_solver')
+
+        self.verbose = settings.get('verbose', True)
         # kspace_solver is mutually exclusive with the other two solver
         # attributes
         if self.kspace_solver and (self.electrostatic_solver or
@@ -159,8 +163,8 @@ class Universe(AtomContainer):
                                           '  Force field',
                                           '  Number of atoms'])
 
-        print(
-            f'Universe created with:\n{setup_frame.to_string(index=True, header=False)}\n')
+        if self.verbose:
+            print(f'Universe created with:\n{setup_frame.to_string(index=True, header=False)}\n')
 
     def __str__(self) -> str:
 
@@ -1131,7 +1135,8 @@ class Universe(AtomContainer):
                 self.constraint_algorithm = settings.get('constraint_algorithm',
                                                          Shake(1e-4, 100))
 
-            print(f'Force field created by solvent {solvent}')
+            if self.verbose:
+                    print(f'Force field created by solvent {solvent}')
 
         except ImportError:
             pass
@@ -1381,6 +1386,8 @@ class Simulation:
         ``pressure`` (`float`)
             Simulation pressure in ``Pa``. This is required if a barostat is
             passed.
+        ``verbose`` (`str`)
+            If the output of the class instantiation should be reported, default to True.
 
     Attributes
     ----------
@@ -1410,6 +1417,7 @@ class Simulation:
         self.settings = settings
         self.engine = MDEngineFacadeFactory.create_facade(engine)
         self.engine.parent_simulation = self
+        self.verbose = settings.get('verbose', True)
         self._setup()
 
         setup_msg = f'Simulation created with {engine} engine'
@@ -1419,7 +1427,8 @@ class Simulation:
             setup_frame = pd.DataFrame(setup_values, index=setup_keys)
             setup_msg += f' and settings:\n{setup_frame.to_string(index=True, header=False)}\n'
 
-        print(setup_msg)
+        if self.verbose:
+            print(setup_msg)
 
     @property
     def time_step(self) -> float:
