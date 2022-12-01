1744c6a89a0ac60e8378391d2972d0285b1ffa90
Maks Sekula <maksymilian.sekula@hotmail.com>
2022-12-01 16:40:45 +0000

    Add appropriate space between functions/tests

--- tests/system_tests/observables/test_PDF.py
+++ tests/system_tests/observables/test_PDF.py
@@ -21,6 +21,7 @@ ATOL = 1e-10
 RTOL = 5e-4
 CLOSER_RTOL = 1.e-8
 
+
 @pytest.fixture(scope="module")
 def PDF_file():
 
@@ -33,6 +34,7 @@ def PDF_file():
     """
 
     return Dataset(data.OBS_DATA['netcdf_PDF'], 'r')
+
 
 @pytest.fixture(scope="module")
 def PDF(trajectory, PDF_file):
@@ -50,6 +52,7 @@ def PDF(trajectory, PDF_file):
     pdf = ObservableFactory.create_observable('PDF')
     pdf.calculate_from_MD(trajectory, n_frames=5, r=r, dimensions=[39.4221067] * 3)
     return pdf
+
 
 @pytest.fixture(scope="module")
 def averaged_PDF(trajectory, PDF_file):
@@ -69,6 +72,8 @@ def averaged_PDF(trajectory, PDF_file):
     pdf.calculate_from_MD(trajectory, n_frames=10, r=r, use_average=True,
                           dimensions=[39.4221067] * 3)
     return pdf
+
+
 @pytest.fixture(scope="module")
 def expected_peak_r_values():
     """
@@ -79,6 +84,7 @@ def expected_peak_r_values():
         its peak as the corresponding value
     """
     return {('H', 'H'): 1.65, ('H', 'O'): 1.05, ('O', 'O'): 2.65}
+
 
 # Non-averaged PDF tests
 @pytest.mark.parametrize("partial_str", [('H', 'H'), ('H', 'O'), ('O', 'O')])
@@ -103,14 +109,18 @@ def test_total_PDF_peaks(PDF, expected_peak_r_values):
     peak_actual_r_values = [PDF.r[i] for i in peak_indexes]
     np.isin(peak_r_values, peak_actual_r_values)
 
+
 def test_total_PDF_starts_with_0(PDF):
     """Tests that the total PDF begins with 0"""
     beginning_values = PDF.PDF[:8]
     assert np.all(np.equal(beginning_values, 0.))
+
+
 def test_total_PDF_converges_on_1(PDF):
     """Tests that the total PDF converges on 1"""
     end_values = PDF.PDF[-1:-7]
     assert np.allclose(end_values, 1.)
+
 
 # Averaged PDF Tests
 @pytest.mark.parametrize("partial_str", [('H', 'H'), ('H', 'O'), ('O', 'O')])
@@ -119,21 +129,25 @@ def test_partial_PDF_starts_with_0(averaged_PDF, partial_str):
     beginning_values = averaged_PDF.partial_pdfs[partial_str][:8]
     assert np.all(np.equal(beginning_values, 0.))
 
+
 def test_total_PDFs_start_with_0(averaged_PDF):
     """Tests that the beginning values of the total PDF begin with 0"""
     beginning_values = averaged_PDF.PDF[:8]
     assert np.all(np.equal(beginning_values, 0.))
+
 
 def test_total_PDFs_converge_to_1(averaged_PDF):
     """Tests that the values of the total PDF converge on 1"""
     end_values = averaged_PDF.PDF[-1:-7]
     assert np.allclose(end_values, 1.)
 
+
 @pytest.mark.parametrize("partial_str", [('H', 'H'), ('H', 'O'), ('O', 'O')])
 def test_partial_PDFs_converge_to_1(averaged_PDF, partial_str):
     """Tests that the values of the partial PDFs converge on 1"""
     end_values = averaged_PDF.partial_pdfs[partial_str][-1:-7]
     assert np.allclose(end_values,  1.)
+
 
 def test_total_avg_PDF_peaks(averaged_PDF, expected_peak_r_values):
     """Tests that the averaged total PDF contains peaks in the correct known places"""
@@ -144,6 +158,7 @@ def test_total_avg_PDF_peaks(averaged_PDF, expected_peak_r_values):
     peak_indexes, properties = find_peaks(abs_pdf, height=2)
     peak_actual_r_values = [averaged_PDF.r[i] for i in peak_indexes]
     np.isin(peak_r_values, peak_actual_r_values)
+
 
 @pytest.mark.parametrize("partial_str", [('H', 'H'), ('H', 'O'), ('O', 'O')])
 def test_partial_PDF_peaks(averaged_PDF, partial_str, expected_peak_r_values):
