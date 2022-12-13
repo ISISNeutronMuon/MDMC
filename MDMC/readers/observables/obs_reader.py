5ffd6b65953871aa7abcbf1a02742ff284f1bbc7
maks <maksymilian.sekula@hotmail.com>
2022-12-13 13:57:13 +0000

    Remove partial pdfs as a property for now

--- MDMC/readers/observables/obs_reader.py
+++ MDMC/readers/observables/obs_reader.py
@@ -183,7 +183,6 @@ class SQwReader(ObservableReader, ABC):
     @w.setter
     @unit_decorator(unit=units.Unit('ps') ** -1)
     def w(self, value: float) -> None:
-
         self._w = value
 
     @property
@@ -202,7 +201,6 @@ class SQwReader(ObservableReader, ABC):
     @E.setter
     @unit_decorator(unit=units.ENERGY_TRANSFER)
     def E(self, value: float) -> None:
-
         self._E = value
 
     @property
@@ -232,6 +230,7 @@ class PDFReader(ObservableReader, ABC):
         super().__init__(file_name)
         self.PDF = None
         self.PDF_err = None
+        self.partial_pdfs = None
 
     @property
     def independent_variables(self) -> dict:
@@ -301,7 +300,6 @@ class PDFReader(ObservableReader, ABC):
     @PDF.setter
     @unit_decorator(unit=units.Unit('barn'))
     def PDF(self, value: float) -> None:
-
         self._PDF = value
 
     @property
@@ -313,30 +311,9 @@ class PDFReader(ObservableReader, ABC):
         numpy.ndarray
             error on the total pair distribution function (in ``barn``)
         """
-
         return self._PDF_err
 
     @PDF_err.setter
     @unit_decorator(unit=units.Unit('barn'))
     def PDF_err(self, value: float) -> None:
-
         self._PDF_err = value
-
-    @property
-    def partial_pdfs(self) -> dict:
-        """
-        Get or set the partial pair distribution functions between pairs (in ``barn``)
-
-        Returns
-        -------
-        dict
-            A dictionary containing the partial pair distribution functions.
-            The keys are the name of the partial
-            The values are arrays containing the values of the partial PDF (in ``barn``)
-        """
-
-        return self.partial_pdfs
-
-    @partial_pdfs.setter
-    def partial_pdfs(self, value: dict) -> None:
-        self.partial_pdfs = value
