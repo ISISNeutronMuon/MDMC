66d295587bbf6033ee543096025b1ec17afafc6b
Maciej Bartkowiak <108934199+MBartkowiakSTFC@users.noreply.github.com>
2022-12-16 12:45:51 +0000

    Correct the style of mathematics.py

--- MDMC/common/mathematics.py
+++ MDMC/common/mathematics.py
@@ -41,14 +41,14 @@ def correlation(input1, input2=None, normalise=False) -> np.ndarray:
         ``input1`` if ``input2`` is `None`)
     """
 
-    N = len(input1)
+    num_steps = len(input1)
 
-    fft1 = fft(input1, n=(N * 2), axis=0)
+    fft1 = fft(input1, n=(num_steps * 2), axis=0)
 
     if input2 is None:
         fft2 = fft1
     else:
-        fft2 = fft(input2, n=(N * 2), axis=0)
+        fft2 = fft(input2, n=(num_steps * 2), axis=0)
 
     # Calculate the cyclic correlation function
     cyclic_corr = ifft(np.conjugate(fft1) * fft2, axis=0)
@@ -57,13 +57,13 @@ def correlation(input1, input2=None, normalise=False) -> np.ndarray:
     # 1 / (N - m)
     # where m is the number of each individual step
     if normalise:
-        prefactor = 1. / (N - np.arange(N))
+        prefactor = 1. / (num_steps - np.arange(num_steps))
         if len(np.shape(cyclic_corr)) > 1:
             cyclic_corr = np.sum(cyclic_corr, axis=1)
     else:
         prefactor = 1.
 
-    corr = prefactor * np.real(cyclic_corr[0:N])
+    corr = prefactor * np.real(cyclic_corr[0:num_steps])
 
     return corr
 
@@ -90,11 +90,11 @@ def faster_correlation(input1, input2) -> np.ndarray:
         ``input1`` if ``input2`` is `None`)
     """
 
-    N = len(input1)
+    num_steps = len(input1)
 
-    fft1 = fft(input1, n=(N * 2), axis=0)
+    fft1 = fft(input1, n=(num_steps * 2), axis=0)
 
-    fft2 = fft(input2, n=(N * 2), axis=0)
+    fft2 = fft(input2, n=(num_steps * 2), axis=0)
 
     # Calculate the cyclic correlation function
     cyclic_corr = ifft(np.conjugate(fft1) * fft2, axis=0)
@@ -102,11 +102,11 @@ def faster_correlation(input1, input2) -> np.ndarray:
     # Normalise for variable number of contributions to each correlation:
     # 1 / (N - m)
     # where m is the number of each individual step
-    prefactor = 1. / (N - np.arange(N))
+    prefactor = 1. / (num_steps - np.arange(num_steps))
     # I have to guarantee that the array is a 2D array on input
     cyclic_corr = np.sum(cyclic_corr, axis=1)
 
-    corr = prefactor * np.real(cyclic_corr[0:N])
+    corr = prefactor * np.real(cyclic_corr[0:num_steps])
 
     return corr
 
@@ -130,32 +130,32 @@ def faster_autocorrelation(input1, weights = None) -> np.ndarray:
         ``input1`` if ``input2`` is `None`)
     """
 
-    N = len(input1)
+    num_steps = len(input1)
 
     # print(f"Input shape: {input1.shape}")
 
-    fft1 = fft(input1, n=(N * 2), axis=0)
+    fft1 = fft(input1, n=(num_steps * 2), axis=0)
 
     # Calculate the cyclic correlation function
     cyclic_corr = ifft(np.conjugate(fft1) * fft1, axis=0)
 
     # print(f"cyclic_corr shape: {cyclic_corr.shape}")
     # Normalise for variable number of contributions to each correlation:
-    # 1 / (N - m)
+    # 1 / (num_steps - m)
     # where m is the number of each individual step
-    prefactor = 1. / (N - np.arange(N))
+    prefactor = 1. / (num_steps - np.arange(num_steps))
     # I have to guarantee that the array is a 2D array on input
     if weights is not None:
         try:
             temp_weights = weights.reshape((1, len(weights)))
         except AttributeError:
             temp_weights = weights
-        cyclic_corr *= weights
+        cyclic_corr *= temp_weights
     cyclic_corr = np.sum(cyclic_corr, axis=1)
     # print(f"cyclic_corr shape after summing: {cyclic_corr.shape}")
     # print(f"prefactor shape: {prefactor.shape}")
 
-    corr = prefactor * np.real(cyclic_corr[0:N])
+    corr = prefactor * np.real(cyclic_corr[0:num_steps])
 
     return corr
 
