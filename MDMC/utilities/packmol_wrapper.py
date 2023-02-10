db010df2d5a356ae3dce8f0d8a449974f56d8dca
maks <maksymilian.sekula@hotmail.com>
2023-02-10 16:35:17 +0000

    Remove fftool from packmol wrapper

--- MDMC/utilities/packmol_wrapper.py
+++ MDMC/utilities/packmol_wrapper.py
@@ -1,82 +1,29 @@
+import shutil
 import subprocess
 import os
+import sys
+
 from MDMC.MD.simulation import Universe
 
 #TODO: create an overaching interface function to call all others
 #TODO: make distinction between building an input file for packmol or some other MD simulation
 
-def fill_with_packmol(universe: Universe, molecule_dict: dict, density: float=None):
-    _call_fftool(universe, molecule_dict, density)
-    _call_packmol()
-
-def _call_fftool(universe: Universe, molecule_dict: dict, density: float=None, md_eng: str=None) -> None:
-    """
-    A wrapper function to call fftool to create the input file needed for packmol (or an MD software
-    if specified)
-
-    Parameters
-    ----------
-    universe: Universe
-        The universe object in which to add the molecules to
-    molecule_dict: dict
-        A dictionary containing the path of the atomic coordinates file as the key,
-        with the (integer) number of molecules as the value
-        Current formats accepted are the same as that of fftool
-        (see: https://github.com/paduagroup/fftool)
-    density: optional, float
-        The target density (in mol/L) for the system. If not supplied, the side length(s)
-        of the universe will be used
-    md_eng: optional, str
-        If provided, the function will use fftool to create a file for a specified MD software
-        instead of for packmol (currently - LAMMPS, DL_POLY, GROMACS and OpenMM are supported
-        by fftool)
-
-    """
-    command_list = ["fftool"]
-    dimensions = universe.dimensions
-
-    # Add molecules & number
-    for molecule_tup in molecule_dict.items():
-        command_list.append(str(molecule_tup[1]))
-        command_list.append(str(molecule_tup[0]))
-
-    # Input density/boundary conditions to aim for
-    if density is not None:
-        command_list.append("-r")
-        command_list.append(str(density))
-    else:
-        command_list.append("-b")
-        if type(dimensions) == float:
-            command_list.append(str(dimensions))
-        else:
-            for dimension in dimensions:
-                command_list.append(str(dimension))
-
-    # MD engine flag for output (if provided)
-    if md_eng is not None:
-        if md_eng == "LAMMPS":
-            command_list.append("-l")
-        elif md_eng == "OpenMM":
-            command_list.append("-x")
-        elif md_eng == "GROMACS":
-            command_list.append("-g")
-        elif md_eng == "DL_POLY":
-            command_list.append("-d")
-
-    _call_external_program(command_list, "/root/")
-
-def _call_packmol(path_to_inp: str="pack.inp") -> None:
+def fill_with_packmol(inp_file_name: str, desired_cwd: str) -> None:
     """
     Parameters
     ----------
-    path_to_inp: str
-        A string path to the .inp file used to input into packmol. Defaults to "pack.inp" - the
-        fftool default under the assumption it was called in the current directory.
+    inp_file_name: str
+        The name the .inp file used to input into packmol.
+    desired_cwd: str
+        A string path to the working directory from which you want to call packmol (i.e. where your
+        input files are)
 
     """
-    command_list = ["packmol", "<"]
-    command_list.append(path_to_inp)
-    _call_external_program(command_list, "/root/")
+    path_var = ":".join(sys.path)
+    packmol_path = shutil.which("packmol", path=path_var)
+    command_list = [packmol_path, "<"]
+    command_list.append(desired_cwd+inp_file_name)
+    _call_external_program(command_list, desired_cwd)
 
 
 def _call_external_program(command_list: list[str], work_dir: str):
@@ -92,8 +39,9 @@ def _call_external_program(command_list: list[str], work_dir: str):
         The desired working directory for the program to run in
 
     """
+    command_list = " ".join(command_list)
     try:
-        subprocess.run(args=command_list, cwd=work_dir)
+        subprocess.run(args=command_list, cwd=work_dir, shell=True)
     except:
         wd = os.getcwd()
-        subprocess.run(args=command_list, cwd=wd)
+        subprocess.run(args=command_list, cwd=wd, shell=True)
