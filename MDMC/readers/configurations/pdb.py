9a59fb4b3de7a1217744d981027593ed8b0572f2
Maks Sekula <maksymilian.sekula@hotmail.com>
2023-02-13 20:40:55 +0000

    Create pdb reader

--- MDMC/readers/configurations/pdb.py
+++ MDMC/readers/configurations/pdb.py
@@ -1,22 +1,16 @@
 """Module for reading pdb files"""
-from typing import TYPE_CHECKING
-
-from MDMC.common.decorators import set_docstring
 from MDMC.readers.configurations.conf_reader import ConfigurationReader
-
-if TYPE_CHECKING:
-    from MDMC.MD.structures import Atom
+from MDMC.MD.structures import Atom, Molecule
 
 
 class ProteinDataBankReader(ConfigurationReader):
-
     """
     A class for reading pdb configuration files
 
     Examples
     --------
-    To use a pdb reader to read a file called 'paracetamol.pdb' and create a
-    ``Molecule`` from it (assuming ``Molecule`` has been imported from
+    To use a pdb reader to read a file called 'paracetamol.pdb' and create a set of
+    ``Molecule``s from it (assuming ``Molecule`` has been imported from
     ``MDMC.MD``):
 
     .. highlight:: python
@@ -25,42 +19,50 @@ class ProteinDataBankReader(ConfigurationReader):
         file = 'paracetamol.pdb'
         pdb = pdb()
         pdb.open(file)
-        # See parse docstring for description of ``names`` parameter
-        # Lines are oxygen, nitrogen, carbon and hydrogen atoms
-        pdb.parse(names=['109', '177', # Oxygens
-                         '207', # Nitrogen
-                         '208', '108', '90', '178', '90', '90', '90', '185',
-                         '85', '85', '85', '91', '91', '91', '91', '183', '110']
-                 ) # Hydrogens)
-        paracetamol = Molecule(atoms=pdb.atoms)
+        pdb.parse()
+        paracetamol = pdb.molecules[0]
     """
-
-    extension = 'pdb'
 
     def __init__(self, file_name: str):
         super().__init__(file_name)
-        with open(file_name, "r") as f:
-            file_contents = f.readlines()
-
-        molecules = {}
-        prev_id = ""
-        for i in file_contents:
-            line = i.split()
-            if line[0] == "ATOM" or line[0] == "HETATM":
-                current_id = "".join(line[3:6])
-                if prev_id == current_id:
-                    molecules[prev_id].append(line[-4:-1])
-                else:
-                    prev_id = current_id
-                    molecules[prev_id] = [line[-4:-1], ]
-        self._atoms = None
-
+        self._atoms = []
+        self._molecules = []
 
     def parse(self, **settings: dict) -> None:
+        super().__enter__()
+        prev_id = ""
+        molecules = {}
+        for i in self.file:
+            line = i.split()
+            if line[0] == "ATOM" or line[0] == "HETATM":
+                if len(line) == 9:
+                    current_id = "".join(line[3:6])
+                else:
+                    current_id = "".join(line[3:5])
 
+                element = line[2]
+                current_pos = [float(i) for i in line[-3:]]
+                current_atom = Atom(element, current_pos)
+                self._atoms.append(current_atom)
 
-
+                if prev_id == current_id:
+                    molecules[prev_id].append(current_atom)
+                else:
+                    if prev_id:
+                        full_molecule = Molecule(atoms=molecules[prev_id])
+                        self._molecules.append(full_molecule)
+                    prev_id = current_id
+                    molecules[prev_id] = [current_atom, ]
+        super().__exit__(None, 0, None)
     @property
     def atoms(self) -> 'list[Atom]':
-
         return self._atoms
+
+    @property
+    def extension(self) -> str:
+        return "pdb"
+
+    @property
+    def molecules(self) -> 'list[Molecule]':
+        """Returns a list of ``Molecule`` objects from the data read from the file"""
+        return self._molecules
