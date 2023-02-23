Correct the wrong calculation in pdf.py

MDMC/trajectory_analysis/observables/pdf.py
@@ -1,6 +1,6 @@
 """Module for calculating the total pair distribution function (PDF)"""
 from collections import defaultdict
-from itertools import (chain, combinations_with_replacement, islice,
+from itertools import (chain, combinations_with_replacement,
                        product)
 from typing import Optional
 import warnings
@@ -565,6 +565,7 @@ class PairDistributionFunction(Observable):
         r_max = np.max(self.r) + self.r_step / 2
         _, bin_edges = np.histogram([], len(self.r), range=(r_min, r_max))
         bin_edges_squared = bin_edges**2
+        # the squared bin edges are calculated so we can bin squared distances directly
 
         part_comps = np.array(list(map(get_component_lengths,
                                        self.universe_dimensions)))
@@ -575,42 +576,65 @@ class PairDistributionFunction(Observable):
         # neighbors for which atom pairs must be calculated.
         partition_indexes = list(self._calculate_partition_indexes(part_comps))
         partition_pair_indexes = self._get_partition_pairs(part_comps)
+        # The partition_pair_indexes do NOT include pairs of identical elements.
+        # However, we need those in the current version of the PDF code.
 
         # Now we iterate over all the possible combinations of chemical elements
         for partial_string in self.partial_strings:
-            elem1, elem2 = partial_string
+            elem_A, elem_B = partial_string
 
             # And here we iterate over possible pairs of neighbouring partitions
             for part1, part2 in partition_pair_indexes + [(x,x) for x in partition_indexes]:
+                distance_squared = self._calculate_squared_distances(elem_A, elem_B,
+                                                  part1, part2, partitions)
+                if distance_squared is not None:
+                    histogram, _ = np.histogram(distance_squared, bins=bin_edges_squared)
+                    self.partial_pdfs[partial_string] += histogram
 
-                array1 = partitions[elem1][part1]
-                array2 = partitions[elem2][part2]
+            if elem_A != elem_B:
+                for part1, part2 in partition_pair_indexes:
+                    distance_squared = self._calculate_squared_distances(elem_B, elem_A,
+                                                    part1, part2, partitions)
+                    if distance_squared is not None:
+                        histogram, _ = np.histogram(distance_squared, bins=bin_edges_squared)
+                        self.partial_pdfs[partial_string] += histogram
 
-                n_at_array1 = len(array1)
-                n_at_array2 = len(array2)
-                array1 = array1.reshape(1,n_at_array1,3)
-                array2 = array2.reshape(n_at_array2,1,3)
-                difference = array1 - array2
-                for dim in range(3):
-                    temp_array = difference[:,:,dim]
-                    box_side = abs(self.universe_dimensions[dim])
-                    # Correct for periodic boundary conditions
-                    crit1 = np.where(temp_array > 0.5*box_side)
-                    crit2 = np.where(temp_array < -0.5*box_side)
-                    temp_array[crit1] -= box_side
-                    temp_array[crit2] += box_side
-                    difference[:,:,dim] = temp_array
+    def _calculate_squared_distances(self, elem1: str, elem2: str,
+                                     part1: tuple, part2: tuple, partitions: dict):
+        distance_squared = None
+        array1 = partitions[elem1][part1]
+        array2 = partitions[elem2][part2]
+        # Each array should contain all the atoms of the chemical element elem
+        # in the partition part.
 
-                if elem1==elem2 and np.all(part1 == part2):
-                    temp = np.sum(difference**2, axis = 2)
-                    temp = np.triu(temp, k=1)
-                    distance_squared = temp.ravel()
-                else:
-                    distance_squared = np.sum(difference**2, axis = 2).ravel()
+        n_at_array1 = len(array1)
+        n_at_array2 = len(array2)
+        array1 = array1.reshape(1,n_at_array1,3)
+        array2 = array2.reshape(n_at_array2,1,3)
+        difference = array1 - array2
+        # this calculated the difference in coordinates between every atom
+        # in array1 and every element in array2
+        for dim in range(3):
+            temp_array = difference[:,:,dim]
+            box_side = abs(self.universe_dimensions[dim])
+            # Correct for periodic boundary conditions
+            crit1 = np.where(temp_array > 0.5*box_side)
+            crit2 = np.where(temp_array < -0.5*box_side)
+            temp_array[crit1] -= box_side
+            temp_array[crit2] += box_side
+            difference[:,:,dim] = temp_array
 
-                histogram, _ = np.histogram(distance_squared, bins=bin_edges_squared)
+        if elem1==elem2 and np.all(part1 == part2):
+            temp = np.sum(difference**2, axis = 2)
+            temp = np.triu(temp, k=1)
+            # now the array contains only the part above the diagonal
+            # and this way we avoid double counting.
+            distance_squared = temp.ravel()
+        else:
+            distance_squared = np.sum(difference**2, axis = 2).ravel()
 
-                self.partial_pdfs[partial_string] += histogram
+        return distance_squared
+
 
     def _partition(self, trajectory: CompactTrajectory, part_comps: np.ndarray) -> dict:
         """
@@ -710,68 +734,6 @@ class PairDistributionFunction(Observable):
         return product(*map(np.arange, (self.universe_dimensions
                                         / partition_components).astype('int32'))
                        )
-
-    def _calculate_histogram_from_position_pairs(self, position_pairs: iter) -> np.ndarray:
-        """
-        Returns a histogram of pair separations calculated from
-        ``position_pairs``
-
-        Parameters
-        ----------
-        position_pairs : iter
-            An iterator returning 2 element `tuples`, where each element is a 3
-            element vector specifying a position.
-
-        Returns
-        -------
-        numpy.ndarray
-            An histogram of length equal to the length of ``r`` in
-            ``independent_variables``, where each count is an atomic separation
-            (between the elements in a `tuple` in ``position_pairs``)
-        """
-        # Use np.histogram to get empty array of correct size and bin edges
-        # Assumes constant r step size
-        r_min = np.min(self.r) - self.r_step / 2
-        r_max = np.max(self.r) + self.r_step / 2
-        hist, bin_edges = np.histogram([], len(self.r), range=(r_min, r_max))
-        bin_edges_squared = bin_edges**2
-
-        def jit_histogram(separations, bin_edges):
-            jit_hist, _ = np.histogram(separations, bins=bin_edges)
-            return jit_hist
-
-        # Calculate histograms over blocks. This is mainly for memory management.
-        # Numpy also uses blocks, with a BLOCK size of 65536 (256**2),
-        # so will use this block size and pad with zeros
-        # if position_pairs has been exhausted.
-        # pylint: disable=invalid-name
-        BLOCK = 65536
-        exhausted = False
-        while not exhausted:
-            # Get next block from iterator
-            pair_block = np.fromiter(islice(position_pairs, BLOCK), dtype=object)
-            # return histogram if no more exist in the iterator
-            if len(pair_block) == 0:
-                return hist
-
-            # Unpack & subtract positions from each other
-            pos_1 = np.array([tup[0] for tup in pair_block])
-            pos_2 = np.array([tup[1] for tup in pair_block])
-            difference = np.subtract(pos_1, pos_2)
-
-            if len(pair_block) < BLOCK:
-                # Toggle exhausted so that the while loop will stop.
-                exhausted = True
-                # The calculated differences will be filled with np.array(['inf'])
-                # if the iterator ended before reaching the element at position BLOCK (65536).
-                padding = np.array([np.array([float('inf'), float('inf'), float('inf')])
-                           for _ in range(BLOCK - len(pair_block))])
-                difference = np.append(difference, padding, axis=0)
-
-            separations = self._calculate_euclidean_norm_squared(difference)
-            hist += jit_histogram(separations, bin_edges_squared)
-
-        return hist
 
     @staticmethod
     def _calculate_euclidean_norm(vectors: np.ndarray) -> np.ndarray: