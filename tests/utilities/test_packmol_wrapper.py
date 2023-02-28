d927bb2373a4b656059a32e0e44b485aca6997e0
maks <maksymilian.sekula@hotmail.com>
2023-02-28 09:38:33 +0000

    Create packmol tests

---
+++ tests/utilities/test_packmol_wrapper.py
@@ -0,0 +1,25 @@
+"""Tests for the packmol wrapper utility of MDMC"""
+import MDMC.utilities.packmol_wrapper as packmol
+
+def test_get_packmol_path():
+    """Tests that the packmol path is correct within the docker container"""
+    actual_path = packmol.get_packmol_path()
+    pass
+
+def test_correct_packmol_run():
+    pass
+
+def test_incorrect_packmol_path():
+    pass
+
+def test_incorrect_packmol_filename():
+    pass
+
+def test_get_packmol_output_name():
+    pass
+
+def test_get_packmol_universe_dimensions():
+    pass
+
+def test_call_external_program():
+    pass
