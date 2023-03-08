8de5949fac33cf84d951bece96050eafc0e3d805
maks <maksymilian.sekula@hotmail.com>
2023-03-08 15:24:41 +0000

    restructue pdb reader to read a single molecule config

--- MDMC/readers/configurations/pdb.py
+++ MDMC/readers/configurations/pdb.py
@@ -1,6 +1,11 @@
 """Module for reading pdb files"""
+from itertools import pairwise
+
+import numpy as np
+
+from MDMC.MD.interactions import Bond
 from MDMC.readers.configurations.conf_reader import ConfigurationReader
-from MDMC.MD.structures import Atom, Molecule
+from MDMC.MD.structures import Atom
 
 
 class ProteinDataBankReader(ConfigurationReader):
@@ -16,7 +21,7 @@ class ProteinDataBankReader(ConfigurationReader):
     .. highlight:: python
     .. code-block:: python
 
-        file = 'paracetamol.pdb'
+        file = 'water.pdb'
         pdb = pdb()
         pdb.open(file)
         pdb.parse()
@@ -27,11 +32,11 @@ class ProteinDataBankReader(ConfigurationReader):
         super().__init__(file_name)
         self._atoms = []
         self._molecules = []
+        self._bonds = []
 
     def parse(self, **settings: dict) -> None:
-        super().__enter__()
         prev_id = ""
-        molecules = {}
+        self._molecule = {}
         for i in self.file:
             line = i.split()
             if line[0] == "ATOM" or line[0] == "HETATM":
@@ -44,25 +49,36 @@ class ProteinDataBankReader(ConfigurationReader):
                 current_pos = [float(i) for i in line[-3:]]
                 current_atom = Atom(element, current_pos)
                 self._atoms.append(current_atom)
+                self._molecule[line[1]] = current_atom
 
-                if prev_id == current_id:
-                    molecules[prev_id].append(current_atom)
-                else:
-                    if prev_id:
-                        full_molecule = Molecule(atoms=molecules[prev_id])
-                        self._molecules.append(full_molecule)
-                    prev_id = current_id
-                    molecules[prev_id] = [current_atom, ]
-        super().__exit__(None, 0, None)
+            elif line[0] == "CONECT":
+                atoms_to_connect = line[1:]
+                for atom1_id, atom2_id in pairwise(atoms_to_connect):
+                    self.create_bond(self._molecule[atom1_id], self._molecule[atom2_id])
+
+    def create_bond(self, atom1: Atom, atom2: Atom) -> None:
+        """
+        Checks the bond lengths of the atoms in the molecule and
+        creates a bond if it is below a certain threshold
+
+        This is needed because PDB files are able to include H-bonds (which MDMC does not support)
+        alongside other types of bonds, which are undistinguishable from each other in a pdb file.
+        Therefore, cutting off the bond length at a reasonable distance prevents an extremely long
+        bond being introduced into a molecule structure
+        """
+        cutoff = 2.5
+        difference = np.subtract(atom1.position, atom2.position)
+        bond_length = np.linalg.norm(difference)
+        if bond_length < cutoff:
+            self._bonds += Bond((atom1, atom2))
+
     @property
     def atoms(self) -> 'list[Atom]':
         return self._atoms
 
+    def bonds(self) -> 'list[Bond]':
+        return self._bonds
+
     @property
     def extension(self) -> str:
         return "pdb"
-
-    @property
-    def molecules(self) -> 'list[Molecule]':
-        """Returns a list of ``Molecule`` objects from the data read from the file"""
-        return self._molecules
