49116aedd1d4d318f19c0d8cfee8fd26e32ce394
Franz Lang <franz.lang@stfc.ac.uk>
2023-03-09 16:22:30 +0000

    updating test_minimizer with mocked Control [no ci]

--- tests/refinement/test_minimizer.py
+++ tests/refinement/test_minimizer.py
@@ -9,9 +9,21 @@ import pytest
 from MDMC.MD.parameters import Parameter, Parameters
 from MDMC.refinement.minimizers.minimizer_factory import MinimizerFactory
 from MDMC.refinement.minimizers.minimizer_abs import Minimizer
+from MDMC.control import Control
 
 pytestmark = pytest.mark.mpi
 
+
+class MockControl:
+
+    def __init__(self):
+        self.n_steps = 20
+
+@pytest.fixture(scope="module")
+def mockcontrol():
+
+    _mockcontrol = MockControl()
+    return _mockcontrol
 
 @pytest.fixture(scope="module")
 def parameters():
@@ -42,7 +54,7 @@ def remove_fixed_parameter(params_obj):
             break
 
 @patch.multiple(Minimizer, __abstractmethods__=set())
-def test_minimizer_init(parameters):
+def test_minimizer_init(mockcontrol, parameters):
     """Test initializing ``Minimizer``"""
     # it's not worth parametrising a fixture just for this, so we use a loop
     cases = [parameters, list(parameters.values())]
@@ -50,12 +62,12 @@ def test_minimizer_init(parameters):
     # Ignore pylint error as abstract class is mocked
     # pylint: disable=abstract-class-instantiated
     for parms in cases:
-        minim = Minimizer(parms)
+        minim = Minimizer(mockcontrol, parms)
         assert np.all(minim.parameters == parameters)
 
 
 @patch.multiple(Minimizer, __abstractmethods__=set())
-def test_minimizer_init_invalid_parameters(parameters):
+def test_minimizer_init_invalid_parameters(mockcontrol, parameters):
     """Test initializing ``Minimizer`` with fixed parameters, which should raise a `ValueError`"""
 
     # Ignore pylint error as abstract class is mocked
@@ -65,11 +77,11 @@ def test_minimizer_init_invalid_parameters(parameters):
     parameters.append(fixed_parameter)
 
     with pytest.raises(ValueError):
-        Minimizer(parameters)
+        Minimizer(mockcontrol, parameters)
 
 
 @patch.multiple(Minimizer, __abstractmethods__=set())
-def test_minimizer_write_history(parameters):
+def test_minimizer_write_history(mockcontrol, parameters):
     """Test history csv output of ``Minimizer``"""
     class MockMinimizer(Minimizer):
 
@@ -82,7 +94,7 @@ def test_minimizer_write_history(parameters):
 
     remove_fixed_parameter(parameters)
 
-    minim = MockMinimizer(parameters)
+    minim = MockMinimizer(mockcontrol, parameters)
     minim._history = [[10., 20., 30.],
                       ['Accepted', 'Rejected', 'Accepted'],
                       [3., 4., 5.874958734958]]
@@ -101,7 +113,7 @@ def test_minimizer_write_history(parameters):
                           ([0, 9, 2],
                            ['A', 'C', 'charge', 'equilibrium_state', 'sigma'])
                           ])
-def test_minimizer_history_columns(parameters, p_slice, columns):
+def test_minimizer_history_columns(mockcontrol, parameters, p_slice, columns):
     """
     Tests that the history columns for the` minimizer are as expected,
     including the names of the ``Parameter`` objects which are refined
@@ -109,7 +121,7 @@ def test_minimizer_history_columns(parameters, p_slice, columns):
     parameter_slice = Parameters(list(parameters.values())[slice(*p_slice)])
 
     for minimizer_name in MinimizerFactory.get_minimizer_names():
-        minim = MinimizerFactory.create_minimizer(minimizer_name, parameter_slice)
+        minim = MinimizerFactory.create_minimizer(minimizer_name, mockcontrol, parameter_slice)
         expected_columns = columns
         expected_columns.extend(['FoM', 'Change state'])
 
@@ -124,7 +136,7 @@ def test_minimizer_fixed_parameter():
     parameters = [Parameter(name='fixed', value=1., fixed=True)]
     with pytest.raises(ValueError):
         for minimizer_name in MinimizerFactory.get_minimizer_names():
-            _ = MinimizerFactory.create_minimizer(minimizer_name, parameters)
+            _ = MinimizerFactory.create_minimizer(minimizer_name, mockcontrol, parameters)
 
 
 def test_minimizer_tied_parameter():
@@ -135,4 +147,4 @@ def test_minimizer_tied_parameter():
     parameters = [tied_parameter]
     with pytest.raises(ValueError):
         for minimizer_name in MinimizerFactory.get_minimizer_names():
-            minim = MinimizerFactory.create_minimizer(minimizer_name, parameters)
+            minim = MinimizerFactory.create_minimizer(minimizer_name, mockcontrol, parameters)
