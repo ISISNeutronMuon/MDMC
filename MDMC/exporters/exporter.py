e4d50f97ec0f60e49cd596b03aa5d195474ff959
maks <maksymilian.sekula@hotmail.com>
2023-04-18 14:34:33 +0100

    Add exporters [no ci]

---
+++ MDMC/exporters/exporter.py
@@ -0,0 +1,58 @@
+"""Module for exporter abstract class"""
+
+from abc import ABC, abstractmethod
+
+from MDMC.common.decorators import repr_decorator
+
+
+@repr_decorator('file')
+class Exporter(ABC):
+
+    """
+    Abstract class that defines methods common to all exporters
+
+    Parameters
+    ----------
+    file_name: str
+        name of file to export
+    """
+
+    def __init__(self, file_name: str):
+
+        self.file = None
+        self.file_name = file_name
+
+    def __enter__(self) -> None:
+        """
+        Provides a generic implementation of file opening using inbuilt python
+        open
+
+        Should be overridden if necessary for specific file types.
+        """
+        # pylint: disable=consider-using-with
+        # as this is an abstracted open method
+
+        self.file = open(self.file_name, 'r', encoding='UTF-8')
+
+    def __exit__(self, exception_type, exception_value, traceback) -> None:
+        """Closes the open file after parsing"""
+
+        self.file.close()
+
+    @abstractmethod
+    def parse(self, **settings: dict) -> None:
+        """
+        Parses the file data so that it is in a format expected by the class
+        calling the data exporter
+
+        For exporters which are not specific to one data type, the calling class
+        must be determined so that the file data can be parsed into
+        the appropriate data type.
+
+        Parameters
+        ----------
+        **settings: dict
+            dictionary of settings for exporter
+        """
+
+        raise NotImplementedError
