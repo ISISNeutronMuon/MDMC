c2ff40fec8a3e43ee240f0166a527973c96e4adf
maks <maksymilian.sekula@hotmail.com>
2023-05-02 19:01:41 +0100

    Create initial pdb exporter

--- MDMC/exporters/configurations/pdb.py
+++ MDMC/exporters/configurations/pdb.py
@@ -1,11 +1,18 @@
-"""An """
+"""A module to export configuations as pdb files"""
 from MDMC.exporters.configurations.conf_exporter import ConfigurationExporter
 from MDMC.MD import Structure
-
+from ase import Atoms
+from ase.io import proteindatabank
 class ProteinDataBankExporter(ConfigurationExporter):
 
     def __init__(self, file_name: str):
         super().__init__(file_name)
 
     def write(self, structure: Structure, **settings: dict) -> None:
-        pass
+        # Convert into ASE format
+        atom_list = [atom.element for atom in structure.atoms]
+        atom_symbols = "".join(atom_list)
+        atom_positions = [atom.position for atom in structure.atoms]
+        ase_atoms = Atoms(symbols=atom_symbols, positions=atom_positions)
+        # Write to pdb file
+        proteindatabank.write_proteindatabank(self.file, ase_atoms)
