2a74ade861a8d3894a7b12d1d25acb3225e7ab96
maks <maksymilian.sekula@hotmail.com>
2023-05-11 14:43:55 +0100

    flesh out packmol input exporter

--- MDMC/exporters/packmol_input.py
+++ MDMC/exporters/packmol_input.py
@@ -1,9 +1,9 @@
 """A module of a class to export a packmol input file"""
 from MDMC.MD.packmol.packmol_setup import PackmolSetup
-from MDMC.exporters.configurations.conf_exporter import ConfigurationExporter
+from MDMC.exporters.exporter import Exporter
 
-
-class PackmolInputExporter(ConfigurationExporter):
+class PackmolInputExporter(Exporter):
+    """A class to export `PackmolSetup` objects into packmol input files"""
     def __init__(self, file_name: str):
         super().__init__(file_name)
 
@@ -14,13 +14,16 @@ class PackmolInputExporter(ConfigurationExporter):
     def extension() -> str:
         return ".inp"
 
-    def write(self, setup: PackmolSetup, **settings: dict) -> None:
+    def write(self, setup: PackmolSetup, molecule_file_names: dict, **settings: dict) -> None:
         """
         Write the data contained in a `PackmolSetup` object out to a packmol input file
+
         Parameters
         ----------
-        setup
+        setup: PackmolSetup
             A `PackmolSetup` object which contains the molecules and constraints for the
+        molecule_file_names: dict
+            A dictionary mapping molecules in the system to corresponding file names
         """
         system_settings, mol_settings = setup.get_settings()
         tol = system_settings["tolerance"]
@@ -30,7 +33,8 @@ class PackmolInputExporter(ConfigurationExporter):
         self.file.writeline("output output-universe.pdb")
         for molecule in mol_settings:
             # Get structure file name
-            struct_file_name = ""
+            struct_file_name = molecule_file_names[molecule]
+            struct_file_name if struct_file_name.endswith(".pdb") else struct_file_name += ".pdb"
             self.file.writeline(f"structure {struct_file_name}")
             for setting in mol_settings[molecule].keys():
                 self.file.writeline(self.INDENT+f"{setting}")
