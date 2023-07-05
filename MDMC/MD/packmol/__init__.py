a8b8d4f06dfe97867a99fbce3f6cf0f5ef51d63f
alexhroom <alex.room@btinternet.com>
2023-07-05 12:51:04 +0100

    improved imports and docs

--- MDMC/MD/packmol/__init__.py
+++ MDMC/MD/packmol/__init__.py
@@ -1,3 +1,3 @@
 """Interface with Packmol (https://m3g.github.io/packmol/)"""
-from . import packmol_setup
-from . import packmol_wrapper
+from .packmol_setup import PackmolSetup
+from .packmol_wrapper import PackmolFiller
