cd41446f971d0fb0ac8e8079918a8bd7a69b5afd
alexhroom <alex.room@btinternet.com>
2023-07-13 10:58:39 +0100

    changed CI to match new doc structure

---
+++ doc/notebook-test-config.py
@@ -0,0 +1,12 @@
+c = get_config()
+c.NbConvertApp.export_format = "notebook"
+c.NbConvertApp.notebooks = [
+    "doc/tutorials/Argon-a-to-z.ipynb",
+    "doc/how-to/use-MDMC/notebooks/applying-a-forcefield.ipynb",
+    "doc/how-to/use-MDMC/notebooks/building-a-universe.ipynb",
+    "doc/how-to/use-MDMC/notebooks/creating-an-observable.ipynb",
+    "doc/how-to/use-MDMC/notebooks/running-a-simulation.ipynb",
+    "doc/how-to/use-MDMC/notebooks/selecting-fitting-parameters.ipynb",
+    "doc/how-to/use-MDMC/notebooks/solvating-a-universe.ipynb",
+    "doc/how-to/use-MDMC/notebooks/units.ipynb"
+]
