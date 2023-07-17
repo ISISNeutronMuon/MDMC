62a401347aeb8f48e33ec64f22b99a12cdc8d5dc
alexhroom <alex.room@btinternet.com>
2023-07-17 10:46:05 +0100

    added packmol tutorial to testing and fixed inputs

--- doc/tutorials/notebook-test-config.py
+++ doc/tutorials/notebook-test-config.py
@@ -8,5 +8,6 @@ c.NbConvertApp.notebooks = [
     "doc/tutorials/running-a-simulation.ipynb",
     "doc/tutorials/selecting-fitting-parameters.ipynb",
     "doc/tutorials/solvating-a-universe.ipynb",
+    "doc/tutorials/filling-with-packmol.ipynb",
     "doc/tutorials/units.ipynb"
 ]
