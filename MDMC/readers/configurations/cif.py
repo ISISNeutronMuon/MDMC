b30a7e85e6b987640e99da5a74586afc9585d43b
alexhroom <alex.room@btinternet.com>
2023-07-27 15:07:33 +0100

    re-added cif reader and fixed test

---
+++ MDMC/readers/configurations/cif.py
@@ -0,0 +1,66 @@
+"""
+Reads a .cif file into an MDMC molecule, optionally ignoring symmetry data.
+Adapted from https://wiki.fysik.dtu.dk/ase/_modules/ase/io/cif.html#read_cif.
+"""
+from typing import TYPE_CHECKING
+
+from ase.io.cif import parse_cif
+
+from MDMC.MD.ase.convert import ASE_to_MDMC
+from MDMC.readers.configurations.conf_reader import ConfigurationReader
+from MDMC.readers.configurations.ase import ASEReader
+
+if TYPE_CHECKING:
+    from MDMC.MD import Atom
+
+class CIFReader(ConfigurationReader):
+    """
+    Reads a .cif file into an MDMC molecule.
+    """
+
+    def parse(self, **settings: dict) -> None:
+        """
+        Parse a .cif file into an MDMC molecule,
+        optionally ignoring symmetry data.
+
+        Parameters
+        ----------
+        settings:
+            ignore_symmetry: bool, default True
+                Whether to read or ignore symmetry data.
+        """
+        if settings.get('ignore_symmetry', True):
+            images = []
+            for block in parse_cif(self.file):
+                if not block.has_structure():
+                    continue
+
+                atoms = block.get_unsymmetrized_structure()
+                images.append(atoms)
+
+            atoms = list(images)[0]
+            self._atoms = ASE_to_MDMC(atoms)
+        else:  # else, this is just the standard ASE cif parsing
+            reader = ASEReader(self.file_name)
+            with reader:
+                reader.parse()
+                self._atoms = reader.atoms
+
+    @property
+    @staticmethod
+    def extension() -> str:
+        """
+        The expected file extension for the ``ConfigurationReader``
+        """
+
+        return ".cif"
+
+
+    @property
+    def atoms(self) -> list['Atom']:
+        """
+        All subclasses must implement atoms, which returns a list of ``Atom``
+        objects from the data read from the file
+        """
+
+        return self._atoms
