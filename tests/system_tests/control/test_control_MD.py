68340774d8db2a35a3f902e05c323eb2d3f7d862
Alex H. Room <69592136+alexhroom@users.noreply.github.com>
2024-07-11 14:17:47 +0100

    removed erroneous `np.product`

--- tests/system_tests/control/test_control_MD.py
+++ tests/system_tests/control/test_control_MD.py
@@ -32,7 +32,7 @@ def argon_control(exp_datasets) -> callable:
         universe = Universe(dimensions=23.0668)
         Ar = Atom('Ar', charge=0., mass=36.0)
 
-        n_ar_atoms = int(density * np.product(universe.dimensions))
+        n_ar_atoms = int(density * np.prod(universe.dimensions))
         print(f'Number of argon atoms = {n_ar_atoms}')
         universe.fill(Ar, num_struc_units=(n_ar_atoms))
 
