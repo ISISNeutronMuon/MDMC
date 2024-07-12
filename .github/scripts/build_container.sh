f25d004de5e5a5a9aa8bd8eea07f995eb2e719eb
Jacob Wilkins <jacob.wilkins@stfc.ac.uk>
2024-07-12 11:48:30 +0100

    Fix docker updating with outdated versions

--- .github/scripts/build_container.sh
+++ .github/scripts/build_container.sh
@@ -4,6 +4,8 @@
 
 echo "$DOCKER_PASSWORD" | docker login -u "mdmc" --password-stdin # this login circumvents the Docker IP rate limit for anonymous users
 
+TOKEN=$(curl -s -H "Content-Type: application/json" -X POST -d '{"username": "'${UNAME}'", "password": "'${DOCKER_PASSWORD}'"}' https://hub.docker.com/v2/users/login/ | jq -r .token)
+
 
 # full image rebuild if MD engines have changed
 if git diff remotes/origin/master --name-only | grep 'build/Docker/Dockerfile.engines'
@@ -22,6 +24,13 @@ fi
 if ! git diff remotes/origin/master --name-only | grep 'build/Docker/Dockerfile.mdmc\|requirements.txt\|pyproject.toml'
 then
   echo "Docker file does not require rebuilding."
+
+  # Delete docker tags if not needed.
+  if $(docker manifest inspect mdmc/mdmc:ci-$BRANCH > /dev/null)
+  then
+      curl -s -X DELETE -H "Authorization: JWT ${TOKEN}" https://hub.docker.com/v2/repositories/mdmc/engines/tags/ci-${BRANCH}/
+      curl -s -X DELETE -H "Authorization: JWT ${TOKEN}" https://hub.docker.com/v2/repositories/mdmc/mdmc/tags/ci-${BRANCH}/
+  fi
 else
   echo "Docker file requires rebuilding."
   docker build -t mdmc/mdmc:ci-$BRANCH -f "$(pwd)"/build/Docker/Dockerfile.mdmc . || exit 1
