e3e2bb982229e38a2f22d501e599d16aa7f304c8
Alex H. Room <69592136+alexhroom@users.noreply.github.com>
2024-09-20 15:52:38 +0100

    Validate docs in GUI (#1427)
    * fixed docs in gui
    
    * review fixes

--- MDMC/gui/__init__.py
+++ MDMC/gui/__init__.py
@@ -1,4 +1,6 @@
-"""Functions for viewing MDMC configurations via ASE."""
+"""
+Functions for viewing MDMC configurations via ASE.
+"""
 from typing import Union
 
 import ase.visualize
@@ -12,20 +14,21 @@ def view(obj: Union[Structure, Universe],
          max_atoms: int = 2000) -> Union[HTML, None]:
     """
     View an MDMC Structure or Universe.
+
     Wrapper around the ASE viewer.
 
     Parameters
     ----------
-    obj: Structure or AtomContainer
+    obj : ~MDMC.MD.Structure or ~MDMC.MD.AtomContainer
         The MDMC molecular object to be viewed.
-    viewer: str
+    viewer : str
         The viewer.
-    max_atoms: int, default 2000
+    max_atoms : int, default 2000
         The maximum number of atoms to be displayed.
 
     Returns
     -------
-    HTML or None
+    ~IPython.display.HTML or None
         Either opens the relevant GUI, or returns a HTML object
         (in the case of HTML viewers like X3D).
     """
