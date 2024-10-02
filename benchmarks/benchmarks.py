67e830fc98e58df3189802af9dae916ba8c4ea0b
Adam Greenbank <adam.greenbank@stfc.ac.uk>
2024-10-02 13:53:51 +0000

    Added refine benchmarks with mocked FoM calculation

--- benchmarks/benchmarks.py
+++ benchmarks/benchmarks.py
@@ -1,3 +1,5 @@
+from unittest.mock import patch
+
 from MDMC.control import Control
 from MDMC.MD import Atom, LennardJones, Simulation, Universe
 from MDMC.MD.interactions import Dispersion
@@ -57,7 +59,9 @@ class MinimizerSuite:
             exp_datasets=self.exp_datasets,
             fit_parameters=self.universe.parameters,
             MD_steps=570,
-            minimizer_type="MMC")
+            minimizer_type="MMC",
+            n_steps=100
+        )
         
         self.control_GPR = Control(
             simulation=self.simulation,
@@ -65,7 +69,8 @@ class MinimizerSuite:
             fit_parameters=self.universe.parameters,
             MD_steps=570,
             minimizer_type="GPR",
-            n_steps=100)
+            n_steps=100
+        )
         
         self.control_GPO = Control(
             simulation=self.simulation,
@@ -73,8 +78,9 @@ class MinimizerSuite:
             fit_parameters=self.universe.parameters,
             MD_steps=570,
             minimizer_type="GPO",
-            n_steps=100)
-        
+            n_steps=100
+        )
+    
     def time_minimiser_MMC(self):
        fom = self.control_MMC.max_FoM
        self.control_MMC.minimizer.step(fom)
@@ -87,3 +93,96 @@ class MinimizerSuite:
        fom = self.control_GPO.max_FoM
        self.control_GPO.minimizer.step(fom)
 
+
+class RefineSuite:
+    timeout = 10000
+
+    def setup(self):
+        density = 0.0176
+    
+        self.universe = Universe(dimensions=50.)
+        
+        Ar = Atom('Ar', charge=0.)
+
+        n_ar_atoms = int(density * np.prod(self.universe.dimensions))
+
+        self.universe.fill(Ar, num_struc_units=(n_ar_atoms))
+
+        self.simulation = Simulation(self.universe,
+                        engine="lammps",
+                        time_step=10.18893,
+                        temperature=120.,
+                        traj_step=15)
+        
+        Ar_dispersion = Dispersion(self.universe,
+                           (Ar.atom_type, Ar.atom_type),
+                           cutoff=8.,
+                           vdw_tail_correction=True,
+                           function=LennardJones(1.0243, 3.36))
+
+
+        self.simulation = Simulation(self.universe,
+                        engine="lammps",
+                        time_step=10.18893,
+                        temperature=120.,
+                        traj_step=15)
+        
+        data_path = Path(__file__).with_name("data")
+        input_file_path = data_path.joinpath("Well_s_q_omega_Ar_data.xml")
+        
+        self.exp_datasets = [
+            {
+                'file_name':input_file_path.absolute(),
+                'type':'SQw',
+                'reader':'xml_SQw',
+                'weight':1.,
+                'resolution':None
+            }
+            ]
+    
+        self.control_MMC = Control(
+            simulation=self.simulation,
+            exp_datasets=self.exp_datasets,
+            fit_parameters=self.universe.parameters,
+            MD_steps=570,
+            minimizer_type="MMC",
+            n_steps=100
+        )
+        
+        self.control_GPR = Control(
+            simulation=self.simulation,
+            exp_datasets=self.exp_datasets,
+            fit_parameters=self.universe.parameters,
+            MD_steps=570,
+            minimizer_type="GPR",
+            n_steps=100
+        )
+        
+        self.control_GPO = Control(
+            simulation=self.simulation,
+            exp_datasets=self.exp_datasets,
+            fit_parameters=self.universe.parameters,
+            MD_steps=570,
+            minimizer_type="GPO",
+            n_steps=100
+        )
+
+    def mock_FoM(self):
+        fom = 0
+
+        for i, v in enumerate(self.fit_parameters.values()):
+            fom += v.value ** (i + 1)
+
+        return fom
+
+    @patch.object(Control, "_generate_FoM", mock_FoM)
+    def time_refine_MMC(self):
+        self.control_MMC.refine(n_steps=10)
+
+    @patch.object(Control, "_generate_FoM", mock_FoM)
+    def time_refine_GPO(self):
+        self.control_GPO.refine(n_steps=10)
+
+    @patch.object(Control, "_generate_FoM", mock_FoM)
+    def time_refine_GPR(self):
+        self.control_GPR.refine(n_steps=10)
