66625797f4ec9bd710a2fff993b1668d109857d5
Adam Greenbank <adam.greenbank@stfc.ac.uk>
2024-10-09 16:56:58 +0000

    Added results parser Makes viewing results easier

--- benchmarks/scripts/results_parser.py
+++ benchmarks/scripts/results_parser.py
@@ -1,63 +1,34 @@
 import json
 import pandas as pd
+import numpy as np
 from itertools import product
-from pathlib import Path
-import argparse
+from pprint import pprint
 
-parser = argparse.ArgumentParser(
-    prog='MDMC ASV results parser',
-    description='Parse the results of an MDMC benchmarking run to make it more human readable'
-)
+filename = "results/a23cc70a78d3/e3e2bb98-virtualenv-py3.12.json"
 
-parser.add_argument("benchmark_commit")
-args = parser.parse_args()
-
-filename = f"{args.benchmark_commit}-virtualenv-py3.10.json"
-
-results_dir = Path(__file__).parent.parent.absolute() / "results/"
-
-#Assume we only have one machine
-machine_dir = [x.name for x in results_dir.iterdir() if x.is_dir()][0]
-results_dir = results_dir / machine_dir
-
-filename = results_dir / filename
-
-with filename.open("r", encoding="utf-8") as json_file:
+with open(filename) as json_file:
     data = json.load(json_file)
-    
-results = {k.rsplit(".", 1)[1]: v[0] for k, v in data["results"].items()}
+
+
+results = {k.split(".")[-1]:v[0] for k, v in data["results"].items()}
 
 #Take params from first benchmark's result
-#Tuple of (number of parameters, number of refinement steps)
 results_vals = list(data["results"].values())
-params = list(product(*results_vals[0][1]))
+params = product(*results_vals[0][1])
+
+params = [f"{p[0]} parameters, {p[1]} steps" for p in params]
 
 tuple_results = {}
 
-for k in results:
+for k in results.keys():
     result_type, result_name = k.split("_")
     if result_type == "time":
-        tuple_results[(result_name, "Time")] = results[k]
-        
-        time_per_step = [r / int(p[1]) for r, p in zip(results[k], params)]
-        tuple_results[(result_name, "Time per step")] = time_per_step
+        tuple_results[("time", result_name)] = results[k]
     elif result_type == "track":
-        tuple_results[(result_name, "FoM")] = results[k]
-
+        tuple_results[("FoM", result_name)] = results[k]
 
 cols = pd.MultiIndex.from_tuples(tuple_results.keys())
-rows = pd.MultiIndex.from_tuples(params)
 
-df = pd.DataFrame(tuple_results,  columns=cols, index=rows)
-
-df.insert(3, ("Peak Memory (GB)", "refineGPR"), df.pop(("Peak Memory (GB)", "refineGPR")))
-df.insert(6, ("FoM", "refineGPR"), df.pop(("FoM", "refineGPR")))
-
-df.index.names = ["Parameters", "Steps"]
-
-cols = df.columns.to_list()
-
-#Quick and dirty way to group columns by benchmark
-df.insert(2, ("refineGPO", "FoM"), df.pop(("refineGPO", "FoM")))
+df = pd.DataFrame(tuple_results,  columns=cols, index=params)
 
 print(df)
