bcc007ae3a87be80cdd2b1985e804314ae3b597d
Adam Greenbank <adam.greenbank@stfc.ac.uk>
2024-10-10 10:16:19 +0100

    Small fixes

--- benchmarks/scripts/results_parser.py
+++ benchmarks/scripts/results_parser.py
@@ -1,22 +1,30 @@
 import json
 import pandas as pd
-import numpy as np
 from itertools import product
-from pprint import pprint
+from pathlib import Path
+import glob
+import os
 
-filename = "results/a23cc70a78d3/e3e2bb98-virtualenv-py3.12.json"
+results_dir = Path(__file__).parent.parent.absolute() / "results/"
+
+#Assume we only have one machine
+machine_dir = [x.name for x in os.scandir(results_dir) if x.is_dir()][0]
+results_dir = results_dir / machine_dir
+
+filenames = glob.glob("*.json", root_dir=results_dir)
+filenames.remove("machine.json")
+
+#Assumes we only have one results file
+filename = results_dir / filenames[0]
 
 with open(filename) as json_file:
     data = json.load(json_file)
-
 
 results = {k.split(".")[-1]:v[0] for k, v in data["results"].items()}
 
 #Take params from first benchmark's result
 results_vals = list(data["results"].values())
 params = product(*results_vals[0][1])
-
-params = [f"{p[0]} parameters, {p[1]} steps" for p in params]
 
 tuple_results = {}
 
@@ -28,7 +36,10 @@ for k in results.keys():
         tuple_results[("FoM", result_name)] = results[k]
 
 cols = pd.MultiIndex.from_tuples(tuple_results.keys())
+rows = pd.MultiIndex.from_tuples(params)
 
-df = pd.DataFrame(tuple_results,  columns=cols, index=params)
+df = pd.DataFrame(tuple_results,  columns=cols, index=rows)
+
+df.index.names = ["Parameters", "Steps"]
 
 print(df)
