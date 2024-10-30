593e6ea47746f1c70fdc072fedf8956e9a6291a8
Adam Greenbank <adam.greenbank@stfc.ac.uk>
2024-10-30 11:02:08 +0000

    Removed minimizer suite

--- benchmarks/benchmarks.py
+++ benchmarks/benchmarks.py
@@ -61,93 +61,6 @@ class MockSimulation(Simulation):
         self.auto_equilibrated = True
         self.engine.run()
 
-
-class MinimizerSuite:
-    timeout = 10000
-
-    def setup(self):
-        density = 0.0176
-    
-        self.universe = Universe(dimensions=38.4441)
-        
-        Ar = Atom('Ar', charge=0.)
-
-        n_ar_atoms = int(density * np.prod(self.universe.dimensions))
-
-        self.universe.fill(Ar, num_struc_units=(n_ar_atoms))
-
-        self.simulation = Simulation(self.universe,
-                        engine="lammps",
-                        time_step=10.18893,
-                        temperature=120.,
-                        traj_step=15)
-        
-        Ar_dispersion = Dispersion(self.universe,
-                           (Ar.atom_type, Ar.atom_type),
-                           cutoff=8.,
-                           vdw_tail_correction=True,
-                           function=LennardJones(1.0243, 3.36))
-
-
-        self.simulation = Simulation(self.universe,
-                        engine="lammps",
-                        time_step=10.18893,
-                        temperature=120.,
-                        traj_step=15)
-        
-        data_path = Path(__file__).with_name("data")
-        input_file_path = data_path.joinpath("Well_s_q_omega_Ar_data.xml")
-        
-        self.exp_datasets = [
-            {
-                'file_name':input_file_path.absolute(),
-                'type':'SQw',
-                'reader':'xml_SQw',
-                'weight':1.,
-                'resolution':None
-            }
-            ]
-    
-        self.control_MMC = Control(
-            simulation=self.simulation,
-            exp_datasets=self.exp_datasets,
-            fit_parameters=self.universe.parameters,
-            MD_steps=570,
-            minimizer_type="MMC",
-            n_steps=100
-        )
-        
-        self.control_GPR = Control(
-            simulation=self.simulation,
-            exp_datasets=self.exp_datasets,
-            fit_parameters=self.universe.parameters,
-            MD_steps=570,
-            minimizer_type="GPR",
-            n_steps=100
-        )
-        
-        self.control_GPO = Control(
-            simulation=self.simulation,
-            exp_datasets=self.exp_datasets,
-            fit_parameters=self.universe.parameters,
-            MD_steps=570,
-            minimizer_type="GPO",
-            n_steps=100
-        )
-    
-    # def time_minimiser_MMC(self):
-    #    fom = self.control_MMC.max_FoM
-    #    self.control_MMC.minimizer.step(fom)
-
-    # def time_minimiser_GPR(self):
-    #    fom = self.control_GPR.max_FoM
-    #    self.control_GPR.minimizer.step(fom)
-
-    # def time_minimiser_GPO(self):
-    #    fom = self.control_GPO.max_FoM
-    #    self.control_GPO.minimizer.step(fom)
-
-
 class RefineSuite:
     timeout = 10000
     n_params = [1, 3, 5, 10]
@@ -244,53 +157,53 @@ class RefineSuite:
         return self.fom
 
     @patch.object(Control, "_generate_FoM", mock_FoM)
-    def time_refineMMC(self, n_params, n_steps):
+    def time_MMC(self, n_params, n_steps):
         self.control_MMC.refine(n_steps=n_steps)
 
     @patch.object(Control, "_generate_FoM", mock_FoM)
-    def peakmem_refineMMC(self, n_params, n_steps):
+    def peakmem_MMC(self, n_params, n_steps):
         self.control_MMC.refine(n_steps=n_steps)
 
     @patch.object(Control, "_generate_FoM", mock_FoM)
-    def track_refineMMC(self, n_params, n_steps):
+    def track_MMC(self, n_params, n_steps):
         self.control_MMC.refine(n_steps=n_steps)
         return self.control_MMC.fom
 
     @patch.object(Control, "_generate_FoM", mock_FoM)
-    def time_refineGPO(self, n_params, n_steps):
+    def time_GPO(self, n_params, n_steps):
         self.control_GPO.refine(n_steps=n_steps)
 
     @patch.object(Control, "_generate_FoM", mock_FoM)
-    def peakmem_refineGPO(self, n_params, n_steps):
+    def peakmem_GPO(self, n_params, n_steps):
         self.control_GPO.refine(n_steps=n_steps)
 
     @patch.object(Control, "_generate_FoM", mock_FoM)
-    def track_refineGPO(self, n_params, n_steps):
+    def track_GPO(self, n_params, n_steps):
         self.control_GPO.refine(n_steps=n_steps)
         return float(self.control_GPO.fom)
 
     @patch.object(Control, "_generate_FoM", mock_FoM)
-    def time_refineGPR(self, num_params, n_steps):
+    def time_GPR(self, num_params, n_steps):
         self.control_GPR.refine(n_steps=n_steps)
 
     @patch.object(Control, "_generate_FoM", mock_FoM)
-    def track_refineGPR(self, n_params, n_steps):
+    def track_GPR(self, n_params, n_steps):
         self.control_GPR.refine(n_steps=n_steps)
         return float(self.control_GPR.fom)
 
     @patch.object(Control, "_generate_FoM", mock_FoM)
-    def peakmem_refineGPR(self, num_params, n_steps):
+    def peakmem_GPR(self, num_params, n_steps):
         self.control_GPR.refine(n_steps=n_steps)
              
     @patch.object(Control, "_generate_FoM", mock_FoM)
-    def time_refineOGPR(self, num_params, n_steps):
+    def time_OGPR(self, num_params, n_steps):
         self.control_OGPR.refine(n_steps=n_steps)
 
     @patch.object(Control, "_generate_FoM", mock_FoM)
-    def track_refineOGPR(self, n_params, n_steps):
+    def track_OGPR(self, n_params, n_steps):
         self.control_OGPR.refine(n_steps=n_steps)
         return float(self.control_OGPR.fom)
 
     @patch.object(Control, "_generate_FoM", mock_FoM)
-    def peakmem_refineOGPR(self, num_params, n_steps):
+    def peakmem_OGPR(self, num_params, n_steps):
         self.control_OGPR.refine(n_steps=n_steps)
