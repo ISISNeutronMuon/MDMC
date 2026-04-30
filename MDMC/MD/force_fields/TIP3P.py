84feffe924acd3f9fd86c6cd09f4a4fd34bbbc85
chi cheng <chi-yang.cheng@proton.me>
2026-04-30 16:17:03 +0100

    add docstrings

--- MDMC/MD/force_fields/TIP3P.py
+++ MDMC/MD/force_fields/TIP3P.py
@@ -38,6 +38,9 @@ from MDMC.MD.force_fields.ff import WaterModel
 from MDMC.MD.interaction_functions import Coulomb, HarmonicPotential, LennardJones
 from MDMC.MD.interactions import Bond, BondAngle, Coulombic, Dispersion
 
+if TYPE_CHECKING:
+    from MDMC.MD import Universe
+
 
 class TIP3P(WaterModel):
     """
@@ -53,26 +56,18 @@ def add_tip3p_ff(universe, cutoff, ewald, constrained=True):
     sigma = 3.151  # Ang
     epsilon = 0.6363  # kJ mol^-1
 
-        # Charge Parameters
-        q_O = -0.834  # e
-        q_H = abs(q_O / 2)  # e
+    # Bond Parameters
+    r_OH = 0.9572  # Ang
+    f_OH = 1882.8  # kJ mol^-1 Ang^-2
 
-        # LJ Parameters
-        sigma = 3.151  # Ang
-        epsilon = 0.6363  # kJ mol^-1
+    # Bond Angle Parameters
+    a_HOH = 104.52  # deg
+    f_HOH = 230.12  # kJ mol^-1 rad^-2
 
-        # Bond Parameters
-        r_OH = 0.9572  # Ang
-        f_OH = 1882.8  # kJ mol^-1 Ang^-2
-
-        # Bond Angle Parameters
-        a_HOH = 104.52  # deg
-        f_HOH = 230.12  # kJ mol^-1 rad^-2
-
-        return {
-            (Coulombic, ("O",)): Coulomb(q_O),
-            (Coulombic, ("H",)): Coulomb(q_H),
-            (Dispersion, ("O", "O")): LennardJones(epsilon, sigma),
-            (Bond, ("H", "O")): HarmonicPotential(r_OH, f_OH, interaction_type="bond"),
-            (BondAngle, ("H", "O", "H")): HarmonicPotential(a_HOH, f_HOH, interaction_type="angle"),
-        }
+    return {
+        (Coulombic, ("O",)): Coulomb(q_O),
+        (Coulombic, ("H",)): Coulomb(q_H),
+        (Dispersion, ("O", "O")): LennardJones(epsilon, sigma),
+        (Bond, ("H", "O")): HarmonicPotential(r_OH, f_OH, interaction_type="bond"),
+        (BondAngle, ("H", "O", "H")): HarmonicPotential(a_HOH, f_HOH, interaction_type="angle"),
+    }
