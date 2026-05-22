471298cd33c0c23dc8d9c25fffac6c2ce0e396e2
chi <chi-yang.cheng@proton.me>
2026-05-22 11:49:41 +0100

    add bond and angle forces from opls

--- MDMC/MD/force_fields/OPLSAA.py
+++ MDMC/MD/force_fields/OPLSAA.py
@@ -18,6 +18,8 @@
 corresponding TINKER file."""
 
 from MDMC.MD.force_fields.ff import FileForceField
+from MDMC.MD.interaction_functions import HarmonicPotential, NonBonded
+from MDMC.MD.interactions import Bond, BondAngle, DihedralAngle, NonBondedForce
 
 
 class OPLSAA(FileForceField):
@@ -26,3 +28,87 @@ class OPLSAA(FileForceField):
     """
 
     file_name = "oplsaa.dat"
+
+
+def add_opls_force_field(universe, cutoff: float, ewald: float):
+    opls_aa_file = OPLSAA()
+
+    atom_types = set()
+    for atom in universe.atoms:
+        atom_types.add(atom.atom_type)
+
+    atoms_df = opls_aa_file.atoms
+    disp_df = opls_aa_file.dispersions
+    for atom_type in atom_types:
+        charge = atoms_df[atoms_df["atom_type"] == int(atom_type)].iloc[0]["charge"]
+        disp_row = disp_df[disp_df["atom_type"] == int(atom_type)].iloc[0]
+        nonbonded = NonBondedForce(
+            universe,
+            atom_type,
+            cutoff=cutoff,
+            ewald=ewald,
+            function=NonBonded(charge=charge, epsilon=disp_row["epsilon"], sigma=disp_row["sigma"]),
+        )
+        nonbonded.function.charge.parameter_name = f"OPLS-{atom_type}-nonbonded_charge"
+        nonbonded.function.epsilon.parameter_name = f"OPLS-{atom_type}-nonbonded_epsilon"
+        nonbonded.function.sigma.parameter_name = f"OPLS-{atom_type}-nonbonded_sigma"
+
+    bonds_df = opls_aa_file.bonds
+    angles_df = opls_aa_file.bond_angles
+
+    for interaction in universe.interactions:
+        if isinstance(interaction, Bond):
+            atm_i, atm_j = interaction.atoms[0]
+
+            s_i = atoms_df["atom_type"] == int(atm_i.atom_type)
+            s_j = atoms_df["atom_type"] == int(atm_j.atom_type)
+            grp_i = atoms_df[s_i].iloc[0]["atom_group"]
+            grp_j = atoms_df[s_j].iloc[0]["atom_group"]
+            grp_i, grp_j = sorted([grp_i, grp_j])
+
+            s_i = bonds_df["atom_group1"] == grp_i
+            s_j = bonds_df["atom_group2"] == grp_j
+            bond_row = bonds_df[s_i][s_j].iloc[0]
+
+            harmonicbond = HarmonicPotential(
+                equilibrium_state=bond_row["equilibrium_state"],
+                potential_strength=bond_row["potential_strength"],
+                interaction_type="bond",
+            )
+            harmonicbond.equilibrium_state.parameter_name = (
+                f"OPLS-{grp_i}-{grp_j}-harmonicbond_equilibrium_state"
+            )
+            harmonicbond.potential_strength.parameter_name = (
+                f"OPLS-{grp_i}-{grp_j}-harmonicbond_potential_strength"
+            )
+            interaction.function = harmonicbond
+
+        elif isinstance(interaction, BondAngle):
+            atm_i, atm_j, atm_k = interaction.atoms[0]
+
+            s_i = atoms_df["atom_type"] == int(atm_i.atom_type)
+            s_j = atoms_df["atom_type"] == int(atm_j.atom_type)
+            s_k = atoms_df["atom_type"] == int(atm_k.atom_type)
+            grp_i = atoms_df[s_i].iloc[0]["atom_group"]
+            grp_j = atoms_df[s_j].iloc[0]["atom_group"]
+            grp_k = atoms_df[s_k].iloc[0]["atom_group"]
+            if grp_i > grp_k:
+                grp_i, grp_j, grp_k = reversed([grp_i, grp_j, grp_k])
+
+            s_i = angles_df["atom_group1"] == grp_i
+            s_j = angles_df["atom_group2"] == grp_j
+            s_k = angles_df["atom_group3"] == grp_k
+            angle_row = angles_df[s_i][s_j][s_k].iloc[0]
+
+            harmonicangle = HarmonicPotential(
+                equilibrium_state=angle_row["equilibrium_state"],
+                potential_strength=angle_row["potential_strength"],
+                interaction_type="angle",
+            )
+            harmonicangle.equilibrium_state.parameter_name = (
+                f"OPLS-{grp_i}-{grp_j}-{grp_k}-harmonicangle_equilibrium_state"
+            )
+            harmonicangle.potential_strength.parameter_name = (
+                f"OPLS-{grp_i}-{grp_j}-{grp_k}-harmonicangle_potential_strength"
+            )
+            interaction.function = harmonicangle
--- examples/methanol-openmm.py
+++ examples/methanol-openmm.py
@@ -1,4 +1,11 @@
+import os
+
 from MDMC.MD import *
+from MDMC.MD.force_fields.OPLSAA import add_opls_force_field
+
+# Currently MDMC uses OMP_NUM_THREADS to control the number of processes
+# in the sqw calculation
+os.environ["OMP_NUM_THREADS"] = "4"
 
 # Define the unique atoms using the ForceField atom_type
 # These can be seen in the oplsaa.dat file (MDMC/MD/force_fields/data/oplsaa.dat)
@@ -13,13 +20,12 @@ HO = Atom("H", position=[ 1.3606,  0.7699,  0.0000], name="97", atom_type="97")
 methanol = Molecule(
     atoms=[HC1, HC2, HC3, C, O, HO],
     interactions=[
-        Bond((C, HC1), (C, HC2), (C, HC3), (O, HO)),
+        Bond((C, HC1), (C, HC2), (C, HC3)),
+        Bond((O, HO)),
         Bond((C, O), constrained=True),
-        BondAngle(
-            (HC1, C, O), (HC2, C, O), (HC3, C, O),
-            (HC1, C, HC2), (HC2, C, HC3), (HC3, C, HC1),
-            (HO, O, C)
-        ),
+        BondAngle((HC1, C, O), (HC2, C, O), (HC3, C, O)),
+        BondAngle((HC1, C, HC2), (HC2, C, HC3), (HC3, C, HC1)),
+        BondAngle((HO, O, C)),
         DihedralAngle((HC1, C, O, HO), (HC2, C, O, HO), (HC3, C, O, HO))
     ]
 )
@@ -27,3 +33,17 @@ methanol = Molecule(
 # Create a universe and add the methanol
 universe = Universe(dimensions=15.0, constraint_algorithm=Shake(1e-5, 100), electrostatic_solver=PPPM(accuracy=1e-4))
 universe.fill(methanol, num_density=0.01)
+add_opls_force_field(universe, cutoff=6.0, ewald=1e-4)
+
+simulation = Simulation(
+    universe,
+    engine="openmm",
+    time_step=1.0,
+    temperature=300,
+    traj_step=10,
+    openmm_platform="OpenCL",
+    # default precision on CUDA and OpenCL is single
+    openmm_properties={"Precision": "single"},
+)
+
+simulation.run(n_steps=30000, equilibration=True)
