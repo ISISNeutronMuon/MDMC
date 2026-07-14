913524a0a6cbb49d55a05d618d5e67c63884ac78
Maciej Bartkowiak <maciej.bartkowiak@stfc.ac.uk>
2026-07-14 11:17:44 +0100

    Avoid parsing the data file twice

--- MDMC/trajectory_analysis/observables/mdanse_observable.py
+++ MDMC/trajectory_analysis/observables/mdanse_observable.py
@@ -375,8 +375,9 @@ class MDANSEObservable(Observable):
     def read_from_file(self, reader: csv_reader):
         """Load the data from a file."""
         self._origin = "experiment"
-        with reader:
-            reader.parse()
+        if not reader._dependent_variables:
+            with reader:
+                reader.parse()
         self._dependent_variables = reader.dependent_variables
         self._independent_variables = reader.independent_variables
         self._errors = reader.errors
